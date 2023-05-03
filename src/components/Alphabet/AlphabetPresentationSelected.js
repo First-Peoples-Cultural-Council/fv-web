b648c10f98ce606854a38feb17520ea4719b6c3f
Cara Barter <carabarter@gmail.com>
2023-05-03 16:09:51 -0700

    fix ES lint errors

--- src/components/Alphabet/AlphabetPresentationSelected.js
+++ src/components/Alphabet/AlphabetPresentationSelected.js
@@ -86,7 +86,7 @@ function AlphabetPresentationSelected({
               const zebraStripe = index % 2 === 0 ? 'bg-gray-100' : ''
               return (
                 <div
-                  key={index}
+                  key={wordId}
                   className={`grid grid-cols-5 w-full py-2 px-4 ${zebraStripe}`}
                 >
                   <div className="col-span-2 sm:flex-col sm:place-content-center md:flex-row md:text-center p-2">
@@ -155,10 +155,7 @@ function AlphabetPresentationSelected({
       {/* Modal */}
       {videoIsOpen && (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            onClick={() => onVideoClick()}
-          >
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/* content */}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
