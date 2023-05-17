f6258e03446b0b8c3d5771764df1d0a5051b699f
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-05-17 11:50:54 -0700

    Add Auth header to apiBase config

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,27 @@
 import ky from 'ky'
+import { CognitoUserPool } from 'amazon-cognito-identity-js'
+
+// FPCC
 import GlobalConfiguration from 'src/GlobalConfiguration'
+
+const poolData = {
+  UserPoolId: GlobalConfiguration.AWS_USER_POOL_ID,
+  ClientId: GlobalConfiguration.AWS_CLIENT_ID,
+}
+const userPool = new CognitoUserPool(poolData)
+const cognitoUser = userPool.getCurrentUser()
+
+let headers = {}
+
+if (cognitoUser != null) {
+  cognitoUser.getSession((err, session) => {
+    if (session?.isValid()) {
+      headers = {
+        Authorization: `Bearer ${session.getAccessToken().getJwtToken()}`,
+      }
+    }
+  })
+}
 
 export const apiV1 = ky.create({
   prefixUrl: GlobalConfiguration.V1_API_URL,
@@ -9,6 +31,7 @@ export const apiV1 = ky.create({
 export const apiBase = ky.create({
   prefixUrl: GlobalConfiguration.API_URL,
   timeout: 60000,
+  headers,
 })
 
 export const externalApi = ky.create({
