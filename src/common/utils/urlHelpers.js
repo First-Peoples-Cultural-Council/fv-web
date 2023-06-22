85476d5ce59585dee46a4b349026b40feeb1d76e
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-06-22 11:39:42 -0700

    Add new getMediaPath helper

--- src/common/utils/urlHelpers.js
+++ src/common/utils/urlHelpers.js
@@ -1,3 +1,13 @@
+import {
+  AUDIO,
+  VIDEO,
+  IMAGE,
+  ORIGINAL,
+  SMALL,
+  MEDIUM,
+  THUMBNAIL,
+} from 'common/constants'
+
 // size only applies to images
 // Values for size can be: 'Thumbnail', 'Small', 'Medium', 'FullHD', 'OriginalJpeg',
 export const getMediaUrl = ({ id, type, viewName = 'Medium' }) => {
@@ -19,6 +29,27 @@ export const getMediaUrl = ({ id, type, viewName = 'Medium' }) => {
 
     default:
       return 'The media type supplied is not recognised by the url helper'
+  }
+}
+
+// NB ALL sizes supplied for VIDEO or images of mime-type 'gif' will return a static image src except for ORIGINAL
+export const getMediaPath = ({ mediaObject, type, size = ORIGINAL }) => {
+  if (!mediaObject?.original) {
+    return "A media object with the property of 'originl' must be supplied to retrieve a src path."
+  }
+  if (![ORIGINAL, SMALL, MEDIUM, THUMBNAIL].includes(size)) {
+    return 'Only ORIGINAL, SMALL, MEDIUM, or THUMBNAIL are accepted as sizes for media.'
+  }
+  switch (type) {
+    case AUDIO:
+      return mediaObject?.original?.path
+
+    case VIDEO:
+    case IMAGE:
+      return mediaObject?.[size]?.path
+
+    default:
+      return 'The media type supplied is not recognised by the getMediaPath helper'
   }
 }
 
