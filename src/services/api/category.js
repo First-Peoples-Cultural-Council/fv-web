3e88b6919d589d2375eb74b75b4ccbf195082ec6
Cara Barter <carabarter@gmail.com>
2023-06-29 14:51:11 -0700

    remove unneeded function

--- src/services/api/category.js
+++ src/services/api/category.js
@@ -1,17 +1,9 @@
-import { apiV1, apiBase } from 'services/config'
+import { apiBase } from 'services/config'
 import { SITES, CATEGORIES } from 'common/constants'
 
 const category = {
   get: async ({ sitename }) =>
     apiBase.get(`${SITES}/${sitename}/${CATEGORIES}/`).json(),
-
-  updateParent: async ({ categoryId, parentCategoryId }) => {
-    const body = {
-      categoryId,
-      parentCategoryId,
-    }
-    return apiV1.post('category/updateParent', { json: body }).json()
-  },
 }
 
 export default category
