14549c9103d11aeddc2b3ce2cf2dd66cabd09201
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-07-07 11:22:15 -0700

    Addition of Search param constants

--- src/common/constants/searchParams.js
+++ src/common/constants/searchParams.js
@@ -1,3 +1,6 @@
+// NB: For the source of truth for this list one must refer to the API code/documents
+
+/* Param Keys for Search API */
 export const CATEGORY = 'category'
 export const DOMAIN = 'domain'
 export const GAMES = 'games'
@@ -6,3 +9,22 @@ export const PAGE = 'page'
 export const PAGE_SIZE = 'pageSize'
 export const STARTS_WITH_CHAR = 'startsWithChar'
 export const TYPES = 'types'
+
+/* Recognized values for params */
+
+// For DOMAIN
+export const DOMAIN_TRANSLATION = 'translation'
+export const DOMAIN_LANGUAGE = 'language'
+export const DOMAIN_BOTH = 'both'
+
+// For  GAMES and KIDS
+export const TRUE = 'True'
+export const FALSE = 'False'
+
+// For TYPES
+export const TYPE_PHRASE = 'phrases'
+export const TYPE_SONG = 'songs'
+export const TYPE_STORY = 'stories'
+export const TYPE_WORD = 'words'
+export const TYPES_ENTRIES = 'words,phrases,songs,stories'
+export const TYPES_DICTIONARY = 'words,phrases'
