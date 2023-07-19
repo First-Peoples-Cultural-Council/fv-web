be096990ef6ebf341dc6a6495ad4e607e7afb715
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-07-19 16:30:38 -0700

    Add paths for media

--- src/common/constants/paths.js
+++ src/common/constants/paths.js
@@ -7,3 +7,8 @@ export const MY_SITES = 'my-sites'
 export const SEARCH = 'search'
 export const SITES = 'sites'
 export const WIDGETS = 'widgets'
+
+// MEDIA
+export const AUDIO_PATH = 'audio'
+export const IMAGE_PATH = 'images'
+export const VIDEO_PATH = 'videos'
