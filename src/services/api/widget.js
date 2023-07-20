d7939ac3029cdf3962c084c6104dc8577eaee580
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-07-20 11:29:17 -0700

    Remove deprecated page service

--- src/services/api/widget.js
+++ src/services/api/widget.js
@@ -1,5 +1,6 @@
 import { apiV1 } from 'services/config'
 
+// THIS FILE IS DEPRECATED - please use services/api/widgets for new fv-be widget apis
 const widget = {
   getWordOfTheDay: async ({ siteId }) =>
     apiV1.get(`word_of_the_day/${siteId}`).json(),
