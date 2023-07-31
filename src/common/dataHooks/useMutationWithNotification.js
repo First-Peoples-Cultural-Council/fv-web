0b4a44e04cf26adff7f1cf4a695c94514cdacec9
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-07-31 15:27:12 -0700

    Create custom useMutation hook

---
+++ src/common/dataHooks/useMutationWithNotification.js
@@ -0,0 +1,43 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+// FPCC
+import { ERROR, SUCCESS } from 'common/constants'
+import { useNotification } from 'context/NotificationContext'
+
+export default function useMutationWithNotification({
+  mutationFn,
+  redirectTo,
+  queryKeyToInvalidate,
+  type,
+}) {
+  const queryClient = useQueryClient()
+  const { setNotification } = useNotification()
+
+  const mutation = useMutation({
+    mutationFn,
+    onSuccess: () => {
+      setNotification({
+        type: SUCCESS,
+        message: `Success! The ${type} has been saved.`,
+      })
+      if (redirectTo) {
+        setTimeout(() => {
+          window.location.href = redirectTo
+        }, 1000)
+      }
+    },
+    onError: () => {
+      setNotification({
+        type: ERROR,
+        message: `ERROR: There was a problem saving your ${type}. Please try again. If the error persists please contact FirstVoices Support.`,
+      })
+    },
+    onSettled: () => {
+      if (queryKeyToInvalidate) {
+        queryClient.invalidateQueries({ queryKey: queryKeyToInvalidate })
+      }
+    },
+  })
+
+  return mutation
+}
