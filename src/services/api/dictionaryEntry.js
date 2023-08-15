c8ecdd13d7fa80ced3a1194f9b6f57fe7e7e323f
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-08-15 10:29:39 -0700

    Add api services

--- src/services/api/dictionaryEntry.js
+++
@@ -1,9 +0,0 @@
-import { apiBase } from 'services/config'
-import { DICTIONARY, SITES } from 'common/constants'
-
-const dictionaryEntry = {
-  get: async ({ sitename, id }) =>
-    apiBase.get(`${SITES}/${sitename}/${DICTIONARY}/${id}`).json(),
-}
-
-export default dictionaryEntry
