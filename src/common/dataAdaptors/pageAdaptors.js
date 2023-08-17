ec5ff76c488e86d70320acbd552e580b51ac913d
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-08-17 12:22:51 -0700

    fw-4511, merge main
    - re-add the selectOneMediaFormHelper in the pageAdaptors

--- src/common/dataAdaptors/pageAdaptors.js
+++ src/common/dataAdaptors/pageAdaptors.js
@@ -1,16 +1,16 @@
+import { selectOneMediaFormHelper } from 'common/utils/mediaHelpers'
+
 export function pageInfoAdaptor({ formData, sitename }) {
   // Returns appropriate request body for updating page info (not widget list)
+  const mediaObject = selectOneMediaFormHelper(formData, 'banner')
+
   return {
     title: formData?.title || null,
     visibility: formData?.visibility,
     subtitle: formData?.subtitle || null,
     slug: formData?.slug || null,
-    bannerImage:
-      formData?.banner && formData?.banner?.docType !== 'video'
-        ? formData?.banner.docId
-        : null,
-    bannerVideo:
-      formData?.banner?.docType === 'video' ? formData?.banner.docId : null,
+    bannerImage: mediaObject?.imageId,
+    bannerVideo: mediaObject?.videoId,
     sitename,
   }
 }
