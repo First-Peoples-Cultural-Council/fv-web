101a257f7d2466bdf054db5367e8fee17c733586
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-08-22 15:44:13 -0700

    Remove old widget browser

--- src/components/WidgetAddToDoc/index.js
+++
@@ -1,9 +0,0 @@
-import WidgetAddToDocContainer from 'components/WidgetAddToDoc/WidgetAddToDocContainer'
-import WidgetAddToDocPresentation from 'components/WidgetAddToDoc/WidgetAddToDocPresentation'
-import WidgetAddToDocData from 'components/WidgetAddToDoc/WidgetAddToDocData'
-
-export default {
-  Container: WidgetAddToDocContainer,
-  Presentation: WidgetAddToDocPresentation,
-  Data: WidgetAddToDocData,
-}
