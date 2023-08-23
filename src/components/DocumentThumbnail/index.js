174a8e5f1f2b3a9bd4d55e5e71d2a6807581cefc
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-08-23 14:21:51 -0700

    FW-4760, FW-4736 private site link (#97)
    * Remove old DocumentThumbnail component
    
    * Ensure members can access their Private sites
    
    * Update Lock icon
    
    * FW-4736 Remove onClick text from mobile menu

--- src/components/DocumentThumbnail/index.js
+++
@@ -1,9 +0,0 @@
-import DocumentThumbnailContainer from 'components/DocumentThumbnail/DocumentThumbnailContainer'
-import DocumentThumbnailData from 'components/DocumentThumbnail/DocumentThumbnailData'
-import DocumentThumbnailPresentation from 'components/DocumentThumbnail/DocumentThumbnailPresentation'
-
-export default {
-  Container: DocumentThumbnailContainer,
-  Data: DocumentThumbnailData,
-  Presentation: DocumentThumbnailPresentation,
-}
