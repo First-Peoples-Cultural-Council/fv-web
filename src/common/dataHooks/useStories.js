22cc9e1b6b609b9f09e57e6f34dca09737f0c3be
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-08-28 16:32:03 -0700

    Retrieve sitename from params

--- src/common/dataHooks/useStories.js
+++ src/common/dataHooks/useStories.js
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
 
 // FPCC
 import { STORIES } from 'common/constants'
 import api from 'services/api'
 import { storySummaryAdaptor, storyDetailAdaptor } from 'common/dataAdaptors'
 
-export function useStories({ sitename }) {
+export function useStories() {
+  const { sitename } = useParams()
+
   const response = useQuery(
     [STORIES, sitename],
     () => api.stories.getAll({ sitename }),
@@ -19,7 +22,9 @@ export function useStories({ sitename }) {
   return { ...response, data: { ...response?.data, results: formatted } }
 }
 
-export function useStory({ sitename, id }) {
+export function useStory({ id }) {
+  const { sitename } = useParams()
+
   const response = useQuery(
     [STORIES, sitename, id],
     () => api.stories.get({ sitename, id }),
