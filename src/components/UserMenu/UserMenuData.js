9dae5e17bf457e70123931444e9209f2803b087b
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-09-01 12:31:10 -0700

    Generate dashboard link based on sites and roles

--- src/components/UserMenu/UserMenuData.js
+++ src/components/UserMenu/UserMenuData.js
@@ -2,14 +2,35 @@
 import { useSiteStore } from 'context/SiteContext'
 import { useUserStore } from 'context/UserContext'
 import useLoginLogout from 'common/hooks/useLoginLogout'
+import { atLeastAssistant } from 'common/constants/roles'
 
 function UserMenuData() {
   const { site } = useSiteStore()
   const { user } = useUserStore()
   const { login, logout } = useLoginLogout()
 
+  const getDashboardLink = () => {
+    if (!user?.isTeam) return null
+    const userSiteRole = user?.roles?.[site?.sitename] || ''
+    if (userSiteRole.match(atLeastAssistant) || user?.isStaff) {
+      return `/${site?.sitename}/dashboard`
+    }
+    const siteToUse = getFirstSiteWIthTeamAccess(user?.roles)
+    return `/${siteToUse}/dashboard`
+  }
+
+  const getFirstSiteWIthTeamAccess = (roles) => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of Object.entries(roles)) {
+      if (value.match(atLeastAssistant)) return key
+    }
+    return ''
+  }
+
+  const dashboardLink = getDashboardLink()
+
   return {
-    currentUser: user,
+    currentUser: { ...user, dashboardLink },
     hasImmersion: site?.features?.includes('immersion'),
     login,
     logout,
