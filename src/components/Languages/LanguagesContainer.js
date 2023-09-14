3d8406ea08031a8493fe0098c2cf10ab9db664a0
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-09-14 09:09:21 -0700

    FW-4193 Add Loading to Languages Page

--- src/components/Languages/LanguagesContainer.js
+++ src/components/Languages/LanguagesContainer.js
@@ -3,15 +3,19 @@ import React from 'react'
 // FPCC
 import LanguagesPresentation from 'components/Languages/LanguagesPresentation'
 import LanguagesData from 'components/Languages/LanguagesData'
+import Loading from 'components/Loading'
 
 function LanguagesContainer() {
-  const { allSitesList, userSitesList, parentLanguagesData } = LanguagesData()
+  const { allSitesList, userSitesList, parentLanguagesData, isLoading } =
+    LanguagesData()
   return (
-    <LanguagesPresentation
-      allSitesList={allSitesList}
-      userSitesList={userSitesList}
-      parentLanguagesData={parentLanguagesData}
-    />
+    <Loading.Container isLoading={isLoading}>
+      <LanguagesPresentation
+        allSitesList={allSitesList}
+        userSitesList={userSitesList}
+        parentLanguagesData={parentLanguagesData}
+      />
+    </Loading.Container>
   )
 }
 
