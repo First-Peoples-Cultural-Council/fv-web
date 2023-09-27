76f2082c977b1c03e8c71a71ede3edb622a58fa6
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-09-27 13:23:17 -0700

    Add fields to song form except lyrics

--- src/components/SongCrud/SongCrudPresentation.js
+++ src/components/SongCrud/SongCrudPresentation.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import * as yup from 'yup'
+import { EditorState } from 'draft-js'
 
 // FPCC
 import Form from 'components/Form'
 import DeleteButton from 'components/DeleteButton'
 import { definitions } from 'common/utils/validationHelpers'
 import useEditForm from 'common/hooks/useEditForm'
+import { AUDIO, IMAGE, VIDEO } from 'common/constants'
 
 function SongCrudPresentation({
   backHandler,
@@ -17,18 +19,35 @@ function SongCrudPresentation({
   const validator = yup.object().shape({
     title: definitions.title().required('A title is required'),
     titleTranslation: definitions.paragraph(),
+    intro: definitions.wysiwyg({ charCount: 1200 }),
+    introTranslation: definitions.wysiwyg({ charCount: 1200 }),
+    acknowledgments: definitions.textArray({ charCount: 500 }),
+    notes: definitions.textArray({ charCount: 500 }),
+    relatedAudio: definitions.idArray(),
+    relatedImages: definitions.idArray(),
+    relatedVideos: definitions.idArray(),
   })
 
   const defaultValues = {
     title: '',
     titleTranslation: '',
+    intro: EditorState.createEmpty(),
+    introTranslation: EditorState.createEmpty(),
+    acknowledgements: [],
+    notes: [],
+    relatedAudio: [],
+    relatedImages: [],
+    relatedVideos: [],
+    includeInKids: 'true',
+    includeInGames: 'true',
   }
 
-  const { errors, handleSubmit, isCreateMode, register, reset } = useEditForm({
-    defaultValues,
-    validator,
-    dataToEdit,
-  })
+  const { control, errors, handleSubmit, isCreateMode, register, reset } =
+    useEditForm({
+      defaultValues,
+      validator,
+      dataToEdit,
+    })
 
   return (
     <div id="SongCrudPresentation" className="max-w-5xl p-8">
@@ -64,6 +83,110 @@ function SongCrudPresentation({
                 {errors?.titleTranslation?.message}
               </div>
             )}
+          </div>
+          <div className="col-span-12">
+            <Form.WysiwygField
+              label="Introduction"
+              nameId="intro"
+              control={control}
+              toolbar="none"
+            />
+            {errors?.intro && (
+              <div className="text-red-500">{errors?.intro?.message}</div>
+            )}
+          </div>
+          <div className="col-span-12">
+            <Form.WysiwygField
+              label="Introduction Translation"
+              nameId="introTranslation"
+              control={control}
+              toolbar="none"
+            />
+            {errors?.introTranslation && (
+              <div className="text-red-500">
+                {errors?.introTranslation?.message}
+              </div>
+            )}
+          </div>
+          <div className="col-span-12">
+            <Form.MediaArrayField
+              label="Audio"
+              nameId="relatedAudio"
+              control={control}
+              register={register}
+              type={AUDIO}
+              maxItems={10}
+            />
+            {errors?.relatedAudio && (
+              <div className="text-red-500">
+                {errors?.relatedAudio?.message}
+              </div>
+            )}
+          </div>
+          <div className="col-span-12">
+            <Form.MediaArrayField
+              label="Images"
+              nameId="relatedImages"
+              control={control}
+              register={register}
+              type={IMAGE}
+              maxItems={10}
+            />
+            {errors?.relatedImages && (
+              <div className="text-red-500">
+                {errors?.relatedImages?.message}
+              </div>
+            )}
+          </div>
+          <div className="col-span-12">
+            <Form.MediaArrayField
+              label="Videos"
+              nameId="relatedVideos"
+              control={control}
+              register={register}
+              type={VIDEO}
+              maxItems={10}
+            />
+            {errors?.relatedVideos && (
+              <div className="text-red-500">
+                {errors?.relatedVideos?.message}
+              </div>
+            )}
+          </div>
+          <div className="col-span-12">
+            <Form.TextArrayField
+              label="Notes"
+              nameId="notes"
+              register={register}
+              control={control}
+              errors={errors}
+              maxItems={6}
+            />
+          </div>
+          <div className="col-span-12">
+            <Form.TextArrayField
+              label="Acknowledgements"
+              nameId="acknowledgements"
+              register={register}
+              control={control}
+              errors={errors}
+              maxItems={6}
+            />
+          </div>
+          <div className="col-span-12">
+            <Form.Visibility control={control} errors={errors} />
+          </div>
+          <div className="col-span-12">
+            <Form.RadioButtons
+              label="Include on the Kids site?"
+              control={control}
+              errors={errors}
+              nameId="includeInKids"
+              options={[
+                { label: 'Yes', value: 'true' },
+                { label: 'No', value: 'false' },
+              ]}
+            />
           </div>
           <div className="col-span-12 flex justify-end mt-6 px-6">
             <Form.SubmitButtons
