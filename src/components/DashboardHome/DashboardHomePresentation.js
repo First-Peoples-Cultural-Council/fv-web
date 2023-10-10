f4c151f9621a9e0c94fd5687c4bacf7917e445c8
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-10-10 13:21:44 -0700

    Undo quick links

--- src/components/DashboardHome/DashboardHomePresentation.js
+++ src/components/DashboardHome/DashboardHomePresentation.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 
 // FPCC
-import getIcon from 'common/utils/getIcon'
-import RequireAuth from 'common/RequireAuth'
-import { MEMBER } from 'common/constants/roles'
+import DashboardTiles from 'components/DashboardTiles'
 import DashboardLocator from 'components/DashboardLocator'
 
 function DashboardHomePresentation({ site, tiles, currentUser }) {
@@ -44,59 +41,7 @@ function DashboardHomePresentation({ site, tiles, currentUser }) {
             </div>
           </div>
         </section>
-        <section className="mx-auto max-w-3xl">
-          <h2 className="text-sm font-medium text-gray-500">Quick Links</h2>
-          <ul className="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
-            {tiles.map((tile) => (
-              <RequireAuth
-                key={tile.name}
-                siteMembership={tile?.auth ? tile?.auth : MEMBER}
-              >
-                <li
-                  key={tile?.name}
-                  className="relative group col-span-1 flex rounded-md shadow-lg"
-                >
-                  <div
-                    className={`bg-gray-100 text-${tile.iconColor} flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-l-md font-medium`}
-                  >
-                    {getIcon(tile.icon, 'h-8 w-8 fill-current')}
-                  </div>
-                  <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
-                    <div className="flex-1 truncate px-4 py-2 text-sm">
-                      {tile?.externalLink ? (
-                        <a
-                          href={tile?.href}
-                          className="text-lg font-medium text-fv-charcoal group-hover:text-gray-600"
-                          target="_blank"
-                          rel="noreferrer noopener"
-                        >
-                          {/* Extend touch target to entire panel */}
-                          <span
-                            className="absolute inset-0"
-                            aria-hidden="true"
-                          />
-                          {tile?.name}
-                        </a>
-                      ) : (
-                        <Link
-                          to={tile?.href}
-                          className="text-lg font-medium text-fv-charcoal group-hover:text-gray-600"
-                        >
-                          {/* Extend touch target to entire panel */}
-                          <span
-                            className="absolute inset-0"
-                            aria-hidden="true"
-                          />
-                          {tile?.name}
-                        </Link>
-                      )}
-                    </div>
-                  </div>
-                </li>
-              </RequireAuth>
-            ))}
-          </ul>
-        </section>
+        <DashboardTiles.Presentation tileContent={tiles} />
       </div>
     </main>
   )
