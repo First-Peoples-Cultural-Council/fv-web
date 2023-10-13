55ff137cd6f58fe7b9b3a6d474fb3fd6667fe6ab
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-10-13 15:20:42 -0700

    Show all media in story preview

--- src/components/StoryCrud/StoryCrudPreview.js
+++ src/components/StoryCrud/StoryCrudPreview.js
@@ -22,38 +22,89 @@ function StoryCrudPreview({ storyData }) {
     includeInKids,
   } = storyData
 
-  const [translate, setTranslate] = useState(true)
-  const [currentPageId, setCurrentPageId] = useState()
+  const [translate, setTranslate] = useState(false)
+  const [currentPage, setCurrentPage] = useState()
 
   useEffect(() => {
-    if (!currentPageId) {
-      setCurrentPageId(pages?.[0])
+    if (!currentPage) {
+      setCurrentPage(pagesData?.[pages?.[0]])
     }
-  }, [currentPageId, pages])
+  }, [currentPage, pages, pagesData])
+
+  const labelStyle = 'text-fv-charcoal-light font-semibold'
+  const headingStyle = 'font-bold pb-8 text-xl'
+  const detailStyle = 'space-y-2'
+
+  const translateButton = () => (
+    <button
+      type="button"
+      className="text-primary-light text-sm font-semibold"
+      onClick={() => setTranslate(!translate)}
+    >
+      View {translate ? 'Language' : 'Translation'}
+    </button>
+  )
+
+  const relatedVisualMediaThumbnails = ({ images, videos }) => {
+    if (!images?.length > 0 && !videos?.length > 0) {
+      return null
+    }
+    return (
+      <>
+        {images?.length > 0 &&
+          images?.map((imageId) => (
+            <MediaThumbnail.Image
+              key={imageId}
+              id={imageId}
+              containerStyles="w-40 h-40 mr-2"
+              imageStyles="object-cover"
+            />
+          ))}
+        {videos?.length > 0 &&
+          videos?.map((videoId) => (
+            <MediaThumbnail.Video
+              key={videoId}
+              id={videoId}
+              containerStyles="w-40 h-40 mr-2"
+            />
+          ))}
+      </>
+    )
+  }
+
+  const audioThumbnails = (audio) => {
+    if (audio?.length > 0) {
+      return audio?.map((audioId) => (
+        <MediaThumbnail.Audio key={audioId} id={audioId} />
+      ))
+    }
+    return null
+  }
 
   return (
-    <div id="StoryCrudPreview">
-      <div className="bg-white p-10 rounded">
-        <h1 className="font-bold pb-8 text-xl">Story Info</h1>
-        <div className="text-fv-charcoal-light">
-          <h2 className="font-semibold">Story Title</h2>
-          <p>{translate ? title : titleTranslation}</p>
-          <h2 className="pt-8 font-semibold">Author</h2>
-          <p>{author}</p>
-          <h2 className="pt-8 font-semibold">Story Introduction</h2>
-          <div>
-            <WysiwygBlock jsonString={translate ? intro : introTranslation} />
+    <div id="StoryCrudPreview" className="space-y-4">
+      <section className="bg-white p-8 rounded">
+        <h2 className={headingStyle}>Story Info</h2>
+        <div className="text-fv-charcoal space-y-6">
+          <div className={detailStyle}>
+            <h3 className={labelStyle}>Story Title</h3>
+            <p>{translate ? titleTranslation : title}</p>
+            {translateButton()}
           </div>
-          <button
-            type="button"
-            className="text-primary-light font-semibold"
-            onClick={() => setTranslate(!translate)}
-          >
-            View Translation
-          </button>
+          <div className={detailStyle}>
+            <h3 className={labelStyle}>Author</h3>
+            <p>{author}</p>
+          </div>
+          <div className={detailStyle}>
+            <h3 className={`${labelStyle} -mb-3`}>Story Introduction</h3>
+
+            <WysiwygBlock jsonString={translate ? introTranslation : intro} />
+            {translateButton()}
+          </div>
+
           {acknowledgements?.length > 0 && (
-            <section>
-              <h2 className="pt-8 font-semibold">Acknowledgements</h2>
+            <div className={detailStyle}>
+              <h3 className={labelStyle}>Acknowledgements</h3>
               <ul>
                 {acknowledgements?.map((ack) => (
                   <li key={ack?.id} className="text-sm">
@@ -61,11 +112,11 @@ function StoryCrudPreview({ storyData }) {
                   </li>
                 ))}
               </ul>
-            </section>
+            </div>
           )}
-          {acknowledgements?.length > 0 && (
-            <section>
-              <h2 className="pt-8 font-semibold">Notes</h2>
+          {notes?.length > 0 && (
+            <div className={detailStyle}>
+              <h3 className={labelStyle}>Notes</h3>
               <ul>
                 {notes?.map((note) => (
                   <li key={note?.id} className="text-sm">
@@ -73,10 +124,21 @@ function StoryCrudPreview({ storyData }) {
                   </li>
                 ))}
               </ul>
-            </section>
+            </div>
           )}
-
-          <h2 className="pt-8 pb-2 font-semibold">Featured Image</h2>
+          <div className={detailStyle}>
+            <h3 className={labelStyle}>Audio</h3>
+            {audioThumbnails(currentPage?.relatedAudio)}
+          </div>
+          <div className={detailStyle}>
+            <h3 className={labelStyle}>Visual Media</h3>
+            <div className="inline-flex">
+              {relatedVisualMediaThumbnails({
+                images: storyData?.relatedImages,
+                videos: storyData?.relatedVideos,
+              })}
+            </div>
+          </div>
         </div>
         <div className="flex justify-end">
           <Link
@@ -85,86 +147,71 @@ function StoryCrudPreview({ storyData }) {
             EDIT
           </Link>
         </div>
-      </div>
-      {pagesData?.[currentPageId] && (
-        <div className="relative">
-          <div className="bg-white mt-10 p-10 rounded">
-            <h1 className="font-bold pb-8 text-xl">Story Pages</h1>
-            <div className="text-fv-charcoal-light flex">
-              {pagesData?.[currentPageId]?.relatedImages?.length > 0 && (
-                <div className="w-1/2 pr-10">
-                  <MediaThumbnail.Image
-                    id={pagesData?.[currentPageId]?.relatedImages?.[0]}
-                    imageStyles="object-cover rounded-md"
+      </section>
+      {currentPage && (
+        <section className="relative bg-white p-8 rounded">
+          <h2 className={headingStyle}>Story Pages</h2>
+          <div className="text-fv-charcoal-light grid grid-cols-2">
+            <div className="col-span-1">
+              {relatedVisualMediaThumbnails({
+                images: currentPage?.relatedImages,
+                videos: currentPage?.relatedVideos,
+              })}
+            </div>
+            <div className="col-span-1 space-y-4">
+              {currentPage?.text && (
+                <div className={detailStyle}>
+                  <h3 className={`${labelStyle} -mb-3`}>Page Text</h3>
+                  <WysiwygBlock
+                    jsonString={
+                      translate
+                        ? currentPage?.text
+                        : currentPage?.textTranslation
+                    }
                   />
+                  {translateButton()}
                 </div>
               )}
-              {pagesData?.[currentPageId]?.relatedVideos?.length > 0 && (
-                <div className="w-1/2 pr-10">
-                  <MediaThumbnail.Video
-                    id={pagesData?.[currentPageId]?.relatedVideos?.[0]}
-                  />
-                </div>
-              )}
-              <div>
-                {pagesData?.[currentPageId].text && (
-                  <div>
-                    <WysiwygBlock
-                      jsonString={
-                        translate
-                          ? pagesData?.[currentPageId]?.text
-                          : pagesData?.[currentPageId]?.textTranslation
-                      }
-                    />
-                  </div>
-                )}
-                <button
-                  type="button"
-                  className="text-primary-light font-semibold"
-                  onClick={() => setTranslate(!translate)}
-                >
-                  View Translation
-                </button>
-              </div>
-            </div>
-            <div className="flex top-4 justify-center py-2 gap-x-2 text-fv-charcoal-light">
-              Pages
-              {pages?.map((pageId, pageIndex) => (
-                <button
-                  type="button"
-                  key={pageId}
-                  onClick={() => setCurrentPageId(pageId)}
-                  className={`cursor-pointer ${
-                    pageId === currentPageId
-                      ? 'text-fv-charcoal-dark font-bold'
-                      : 'text-fv-charcoal-light'
-                  }`}
-                >
-                  {pageIndex + 1}
-                </button>
-              ))}
-            </div>
-            <div className="flex justify-end">
-              <Link
-                to={`/${sitename}/dashboard/edit/story?step=1&id=${storyData?.id}`}
-              >
-                EDIT
-              </Link>
+              {audioThumbnails(currentPage?.relatedAudio)}
             </div>
           </div>
-        </div>
+          <div className="flex top-4 justify-center py-2 gap-x-2 text-fv-charcoal-light">
+            Pages
+            {pages?.map((pageId, pageIndex) => (
+              <button
+                type="button"
+                key={pageId}
+                onClick={() => setCurrentPage(pagesData?.[pageId])}
+                className={`cursor-pointer ${
+                  pageId === currentPage?.id
+                    ? 'text-fv-charcoal-dark font-bold'
+                    : 'text-fv-charcoal-light'
+                }`}
+              >
+                {pageIndex + 1}
+              </button>
+            ))}
+          </div>
+          <div className="flex justify-end">
+            <Link
+              to={`/${sitename}/dashboard/edit/story?step=1&id=${storyData?.id}`}
+            >
+              EDIT
+            </Link>
+          </div>
+        </section>
       )}
-      <div className="bg-white mt-10 p-10 space-y-4">
-        <h1 className="font-bold text-xl">Privacy</h1>
-        <div className="text-fv-charcoal-light">
-          <p className="font-semibold">Who can view your Content?</p>
-          <p>{visibility}</p>
-        </div>
-        <div className="text-fv-charcoal-light">
-          <h2 className="font-semibold">
-            Want this included in the Kids site?
-          </h2>
-          <p>{includeInKids === 'true' ? 'Yes' : 'No'}</p>
+      <section className="bg-white p-8 rounded">
+        <h2 className={headingStyle}>Privacy</h2>
+        <div className="text-fv-charcoal space-y-6">
+          <div className={detailStyle}>
+            <h3 className={labelStyle}>Who can view your Content?</h3>
+            <p>{visibility}</p>
+          </div>
+          <div className={detailStyle}>
+            <h3 className={labelStyle}>Want this included in the Kids site?</h3>
+            <p>{includeInKids === 'true' ? 'Yes' : 'No'}</p>
+          </div>
         </div>
         <div className="flex justify-end">
           <Link
@@ -173,11 +220,11 @@ function StoryCrudPreview({ storyData }) {
             EDIT
           </Link>
         </div>
-      </div>
+      </section>
       <div className="flex w-full justify-end">
         <Link
-          to=".."
-          className="bg-primary hover:bg-primary-light text-white border border-transparent rounded-lg shadow-sm my-1 py-2 px-4 mt-6 inline-flex justify-center text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-light"
+          to={`/${sitename}/dashboard/edit/entries?types=story`}
+          className="bg-secondary hover:bg-secondary-light text-white border border-transparent rounded-lg shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-light"
         >
           <span>FINISH</span>
         </Link>
