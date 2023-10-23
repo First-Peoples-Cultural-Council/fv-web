c64eb7af672b7b11c6cec1cb46e103ff5cbf4c1e
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2023-10-23 14:35:39 -0700

    Add Join request service

---
+++ src/services/api/joinRequests.js
@@ -0,0 +1,31 @@
+import { apiBase } from 'services/config'
+import { SITES, JOIN_REQUESTS } from 'common/constants'
+
+const joinRequests = {
+  getJoinRequests: async ({ sitename, pageParam, perPage = 48 }) =>
+    apiBase()
+      .get(
+        `${SITES}/${sitename}/${JOIN_REQUESTS}/?page=${pageParam}&pageSize=${perPage}`,
+      )
+      .json(),
+  get: async ({ sitename, id }) =>
+    apiBase().get(`${SITES}/${sitename}/${JOIN_REQUESTS}/${id}`).json(),
+  create: async ({ sitename, properties }) =>
+    apiBase()
+      .post(`${SITES}/${sitename}/${JOIN_REQUESTS}/`, { json: properties })
+      .json(),
+  approve: async ({ sitename, id, properties }) =>
+    apiBase()
+      .put(`${SITES}/${sitename}/${JOIN_REQUESTS}/${id}/approve/`, {
+        json: properties,
+      })
+      .json(),
+  ignore: async ({ sitename, id }) =>
+    apiBase().put(`${SITES}/${sitename}/${JOIN_REQUESTS}/${id}/ignore/`).json(),
+  reject: async ({ sitename, id }) =>
+    apiBase().put(`${SITES}/${sitename}/${JOIN_REQUESTS}/${id}/reject/`).json(),
+  delete: async ({ sitename, id }) =>
+    apiBase().delete(`${SITES}/${sitename}/${JOIN_REQUESTS}/${id}`).json(),
+}
+
+export default joinRequests
