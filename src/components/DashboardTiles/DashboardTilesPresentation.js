a6fb33e7eda84e2a54abb2d3b5ce0807349c3fb3
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2023-10-25 09:05:34 -0700

    fw-5045, fix dashboard tiling (#176)
    * fw-5045, fix dashboard tiling
    
    dynamic column styles were not being deployed for specific sizes; fixed by removing unused size qualifier
    
    * Remove console.log
    
    ---------
    
    Co-authored-by: Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>

--- src/components/DashboardTiles/DashboardTilesPresentation.js
+++ src/components/DashboardTiles/DashboardTilesPresentation.js
@@ -39,7 +39,7 @@ function DashboardTilesPresentation({ tileContent }) {
   const gridClass = () =>
     tileCount === 1
       ? 'max-w-lg rounded-lg bg-gray-200 overflow-hidden shadow'
-      : `divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-${rowSize} sm:gap-px rounded-lg bg-gray-200 overflow-hidden shadow`
+      : `divide-y divide-gray-200 divide-y-0 grid grid-cols-${rowSize} gap-px rounded-lg bg-gray-200 overflow-hidden shadow`
   return (
     <section id="DashboardTilesPresentation">
       <div className={gridClass()}>
