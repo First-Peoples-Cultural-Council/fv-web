01828d4f39d28f64b2fca944f24a9f8dd5f12d87
Bronson Chapman <bronsonchapman01@gmail.com>
2023-11-21 12:33:02 -0700

    constant change

--- src/components/WidgetStats/WidgetStatsData.js
+++ src/components/WidgetStats/WidgetStatsData.js
@@ -2,9 +2,8 @@
 import { useStats } from '../../common/dataHooks/useStats'
 
 function WidgetStatsData() {
-  const stats = useStats()
   return {
-    data: stats.data,
+    data: useStats().data,
   }
 }
 
