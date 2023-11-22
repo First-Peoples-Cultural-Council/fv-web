34746078fabad83b2e4347b48d0b0b74d41e1bb5
Bronson Chapman <64934597+bronson-c@users.noreply.github.com>
2023-11-22 12:03:42 -0700

    FW-4520 Use Stats API (#201)
    * use stats API
    
    * re-add superadmin req
    
    * constant change
    
    * use correct week key
    
    * fix import

--- src/components/WidgetStats/WidgetStatsData.js
+++ src/components/WidgetStats/WidgetStatsData.js
@@ -1,70 +1,9 @@
-import { useQuery } from '@tanstack/react-query'
-
 // FPCC
-import { useSiteStore } from 'context/SiteContext'
-import api from 'services/api'
+import { useStats } from 'common/dataHooks/useStats'
 
 function WidgetStatsData() {
-  const { site } = useSiteStore()
-  const { data } = useQuery(
-    ['stats', site?.id],
-    () => api.widgets.getStats({ siteId: site?.id }),
-    {
-      // The query will not execute until the site?.id exists
-      enabled: !!site?.id,
-    },
-  )
-
-  const types = ['words', 'phrases', 'songs', 'stories']
-  const temporal = data?.temporal
-  const aggreggate = data?.aggregate
-
-  const getPeriodTotalForType = (period, docType) =>
-    temporal?.[docType]?.[period]?.created
-
-  const getPeriodTotal = (period) => {
-    let total = 0
-    types.forEach((type) => {
-      total += getPeriodTotalForType(period, type)
-    })
-    return total
-  }
-
-  const getPeriodTotalsForAllTypes = (period) => {
-    const totals = {}
-    types.forEach((type) => {
-      totals[type] = getPeriodTotalForType(period, type)
-    })
-    return totals
-  }
-
-  const statsDataAdaptor = () => {
-    const weekTotal = getPeriodTotal('last_7_days')
-    const monthTotal = getPeriodTotal('last_month')
-
-    if (weekTotal > 4) {
-      return {
-        ...getPeriodTotalsForAllTypes('last_7_days'),
-        header: 'NEW THIS WEEK',
-      }
-    }
-    if (monthTotal > 4) {
-      return {
-        ...getPeriodTotalsForAllTypes('last_month'),
-        header: 'NEW THIS MONTH',
-      }
-    }
-    return {
-      words: aggreggate?.words?.total,
-      phrases: aggreggate?.phrases?.total,
-      songs: aggreggate?.songs?.total,
-      stories: aggreggate?.stories?.total,
-      header: 'ON THIS SITE',
-    }
-  }
-
   return {
-    data: statsDataAdaptor(),
+    data: useStats().data,
   }
 }
 
