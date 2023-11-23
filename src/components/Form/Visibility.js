379836ac5bac58a1e45cce97eefbafa231fe42df
Nolan Van Hell <nolan@fpcc.ca>
2023-11-23 15:48:44 -0800

    FW-4809 Reduce visibility options for assistant create (#205)

--- src/components/Form/Visibility.js
+++ src/components/Form/Visibility.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 // FPCC
 import Select from 'components/Form/Select'
 import { useSiteStore } from 'context/SiteContext'
+import { useUserStore } from 'context/UserContext'
+import { ASSISTANT } from '../../common/constants/roles'
 
 function Visibility({
   control,
@@ -11,6 +13,22 @@ function Visibility({
   label = 'Who can see this entry?',
 } = {}) {
   const { site } = useSiteStore()
+  const { user } = useUserStore()
+  const userRoles = user?.roles || {}
+  const userSiteRole = userRoles?.[site?.sitename] || ''
+  const isAssistant = userSiteRole === ASSISTANT
+
+  const options = (function _() {
+    if (isAssistant) {
+      const optionsLen = site?.visibilityOptions.length
+      for (let i = 0; i < optionsLen; i += 1) {
+        if (site?.visibilityOptions[i]?.value !== 'team') {
+          site?.visibilityOptions.splice(i, 1)
+        }
+      }
+    }
+    return site?.visibilityOptions
+  })()
 
   return (
     site?.visibilityOptions && (
@@ -19,7 +37,7 @@ function Visibility({
           label={label}
           control={control}
           nameId="visibility"
-          options={site?.visibilityOptions}
+          options={options}
         />
         {errors?.visibility && (
           <div className="text-red-500">{errors?.visibility?.message}</div>
