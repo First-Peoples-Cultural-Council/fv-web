d70741bd6e99467720ce4e6b882247ea49e2c335
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-11-27 15:09:41 -0800

    - Added a method to shuffle words - Added data layer and moved all logic from presentation layer to data layer - Previous functionality of wordscramble recreated

--- src/components/Game/WordScramble/WordScrambleContainer.js
+++ src/components/Game/WordScramble/WordScrambleContainer.js
@@ -3,11 +3,32 @@ import React from 'react'
 // FPCC
 import Loading from 'components/Loading'
 import WordScramblePresentation from 'components/Game/WordScramble/WordScramblePresentation'
+import WordScrambleData from 'components/Game/WordScramble/WordScrambleData'
 
 function WordScrambleContainer() {
+  const {
+    translation,
+    jumbledWords,
+    selectedWords,
+    gameCompleted,
+    validAnswer,
+    wordClicked,
+    checkAnswer,
+    resetGame,
+  } = WordScrambleData()
+
   return (
     <Loading.Container isLoading={false}>
-      <WordScramblePresentation />
+      <WordScramblePresentation
+        translation={translation}
+        jumbledWords={jumbledWords}
+        selectedWords={selectedWords}
+        gameCompleted={gameCompleted}
+        validAnswer={validAnswer}
+        wordClicked={wordClicked}
+        checkAnswer={checkAnswer}
+        resetGame={resetGame}
+      />
     </Loading.Container>
   )
 }
--- src/components/Game/WordScramble/index.js
+++ src/components/Game/WordScramble/index.js
@@ -1,7 +1,9 @@
-import WordScrambleContainer from 'components/Game/WordScramble/WordScrambleContainer/'
+import WordScrambleContainer from 'components/Game/WordScramble/WordScrambleContainer'
+import WordScrambleData from 'components/Game/WordScramble/WordScrambleData'
 import WordScramblePresentation from 'components/Game/WordScramble/WordScramblePresentation'
 
 export default {
   Container: WordScrambleContainer,
+  Data: WordScrambleData,
   Presentation: WordScramblePresentation,
 }
