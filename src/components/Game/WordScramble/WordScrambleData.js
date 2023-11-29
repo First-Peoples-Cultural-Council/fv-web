709aade77516d26768caa030183e4130e3cd9b02
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-11-29 12:16:36 -0800

    Fw 5237 word scramble game structure with basic functionality (#219)
    * added base components for WordScramble game
    
    * - Added a method to shuffle words
    - Added data layer and moved all logic from presentation layer to data layer
    - Previous functionality of wordscramble recreated
    
    * fixed a typo in setter for validAnswer in wordScramble game

---
+++ src/components/Game/WordScramble/WordScrambleData.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+
+function shuffleWords(array) {
+  const shuffledArray = array.slice()
+  for (let current = shuffledArray.length - 1; current > 0; current -= 1) {
+    const randomIndex = Math.floor(Math.random() * (current + 1))
+    const temp = shuffledArray[current]
+    shuffledArray[current] = shuffledArray[randomIndex]
+    shuffledArray[randomIndex] = temp
+  }
+  return shuffledArray
+}
+
+function WordScrambleData() {
+  // Mock data
+  const inputData = {
+    translation: 'A B C D',
+    title: 'A B C D',
+  }
+
+  const [jumbledWords, setJumbledWords] = useState([])
+  const [selectedWords, setSelectedWords] = useState([])
+  const [gameCompleted, setGameCompleted] = useState(false)
+  const [validAnswer, setValidAnswer] = useState(false)
+
+  useEffect(() => {
+    const correctAnswer = inputData?.title.split(' ')
+    setJumbledWords(shuffleWords([...correctAnswer]))
+  }, [inputData?.title])
+
+  const wordClicked = (word) => {
+    if (gameCompleted && !validAnswer) {
+      setGameCompleted(false)
+    }
+
+    if (selectedWords.includes(word)) {
+      // Removing word from selected words list
+      setSelectedWords(
+        selectedWords.filter((selectedWord) => selectedWord !== word),
+      )
+    } else {
+      // Adding word to the selected words list
+      setSelectedWords([...selectedWords, word])
+    }
+  }
+
+  const checkAnswer = () => {
+    const selectedAnswer = selectedWords.join(' ')
+
+    if (selectedAnswer === inputData?.title) {
+      setValidAnswer(true)
+    }
+    setGameCompleted(true)
+  }
+
+  const resetGame = () => {
+    // Reset game state
+    setSelectedWords([])
+
+    // Should we also reset the jumbled words here ?
+    const correctAnswer = inputData?.title.split(' ')
+    setJumbledWords(shuffleWords([...correctAnswer]))
+
+    setValidAnswer(false)
+    setGameCompleted(false)
+  }
+
+  return {
+    translation: inputData?.translation,
+    jumbledWords,
+    selectedWords,
+    gameCompleted,
+    validAnswer,
+    wordClicked,
+    checkAnswer,
+    resetGame,
+  }
+}
+
+export default WordScrambleData
