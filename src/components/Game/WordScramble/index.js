42d75122756e4c9273aa5235144943deb8df43aa
Sahil Sharma <sahil.sharma071997@gmail.com>
2023-12-01 11:05:51 -0800

    renamed wordScramble to PhraseScrambler

--- src/components/Game/WordScramble/index.js
+++
@@ -1,9 +0,0 @@
-import WordScrambleContainer from 'components/Game/WordScramble/WordScrambleContainer'
-import WordScrambleData from 'components/Game/WordScramble/WordScrambleData'
-import WordScramblePresentation from 'components/Game/WordScramble/WordScramblePresentation'
-
-export default {
-  Container: WordScrambleContainer,
-  Data: WordScrambleData,
-  Presentation: WordScramblePresentation,
-}
