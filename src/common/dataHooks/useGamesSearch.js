6676af36f4c5c77cc83037162b15c8b1b7cb238c
Nolan Van Hell <nolan@fpcc.ca>
2023-12-07 11:30:55 -0800

    FW-5214 Refactor ParachuteData and useGamesSearch

--- src/common/dataHooks/useGamesSearch.js
+++ src/common/dataHooks/useGamesSearch.js
@@ -1,14 +1,39 @@
 import { useQuery } from '@tanstack/react-query'
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 
+// FPCC
 import api from 'services/api'
-import SEARCH from 'common/constants'
+import {
+  SEARCH,
+  TYPES,
+  KIDS,
+  TYPE_WORD,
+  HAS_AUDIO,
+  HAS_TRANSLATION,
+  GAMES,
+  SORT,
+  HAS_UNRECOGNIZED_CHARS,
+} from 'common/constants'
 
-export function useParachuteSearch({ searchParams, perPage }) {
+export function useParachuteSearch({ perPage }) {
   const { sitename } = useParams()
-  const searchParamString = searchParams.toString()
+
+  const [searchParams] = useSearchParams()
+  const _searchParams = searchParams.get(TYPES)
+    ? searchParams
+    : new URLSearchParams({
+        [GAMES]: true,
+        [TYPES]: TYPE_WORD,
+        [KIDS]: true,
+        [HAS_AUDIO]: true,
+        [HAS_TRANSLATION]: true,
+        [SORT]: 'random',
+        [HAS_UNRECOGNIZED_CHARS]: false,
+      })
+  const searchParamString = _searchParams.toString()
+
   const { data, refetch } = useQuery(
-    [SEARCH, sitename, searchParams],
+    [SEARCH, sitename],
     () =>
       api.search.getParachute({
         sitename,
@@ -19,8 +44,36 @@ export function useParachuteSearch({ searchParams, perPage }) {
       enabled: !!sitename,
     },
   )
+
+  const getPuzzles = () => {
+    // If no words are found during the search then return an array with a single empty puzzle
+    if (data?.count === 0) {
+      return [[]]
+    }
+    const splitCharsBaseArray = data?.results?.map(
+      (item) => item?.entry?.splitCharsBase,
+    )
+    const puzzles = splitCharsBaseArray?.map((splitCharsBase) => {
+      // If the splitCharsBase is not empty, generate a puzzle
+      if (splitCharsBase?.length > 0) {
+        const puzzleParts = []
+        splitCharsBase.forEach((letter) => {
+          if (letter === ' ') {
+            puzzleParts.push({ letter: ' ', found: true })
+          } else {
+            puzzleParts.push({ letter, found: false })
+          }
+        })
+        return puzzleParts
+      }
+      return []
+    })
+    return puzzles
+  }
+
   return {
     data,
+    puzzles: getPuzzles(),
     refetch,
   }
 }
--- src/components/Game/Parachute/ParachuteData.js
+++ src/components/Game/Parachute/ParachuteData.js
@@ -1,52 +1,32 @@
 import { useState, useEffect } from 'react'
-import { useSearchParams } from 'react-router-dom'
 
 // FPCC
-import {
-  TYPES,
-  KIDS,
-  TYPE_WORD,
-  HAS_AUDIO,
-  HAS_TRANSLATION,
-  GAMES,
-  SORT,
-  HAS_UNRECOGNIZED_CHARS,
-} from 'common/constants'
 import { useCharacters } from 'common/dataHooks/useCharacters'
 import { useParachuteSearch } from 'common/dataHooks/useGamesSearch'
 
-const PUZZLES = 100
+const PUZZLES_PER_PAGE = 100
 const MAX_PAGES_WITHOUT_USABLE_PUZZLE = 10
 
 function ParachuteData() {
   const [currentWordIndex, setCurrentWordIndex] = useState(0)
-  const [pageSize, setPageSize] = useState()
-  const [puzzle, setPuzzle] = useState()
-  const [pagesWithoutUsablePuzzle, setPagesWithoutUsablePuzzle] = useState(0)
+  const [currentPuzzle, setCurrentPuzzle] = useState()
+  const [pagesWithoutUsablePuzzle, setPagesWithoutUsablePuzzle] = useState(1)
 
-  const [searchParams] = useSearchParams()
-  const _searchParams = searchParams.get(TYPES)
-    ? searchParams
-    : new URLSearchParams({
-        [GAMES]: true,
-        [TYPES]: TYPE_WORD,
-        [KIDS]: true,
-        [HAS_AUDIO]: true,
-        [HAS_TRANSLATION]: true,
-        [SORT]: 'random',
-        [HAS_UNRECOGNIZED_CHARS]: false,
-      })
-
-  const { data, refetch: getNextPage } = useParachuteSearch({
-    searchParams: _searchParams,
-    perPage: PUZZLES,
+  const {
+    data,
+    puzzles,
+    refetch: getNextPage,
+  } = useParachuteSearch({
+    perPage: PUZZLES_PER_PAGE,
   })
 
   const { data: characterData } = useCharacters()
   const characters = characterData?.characters?.map((item) => item?.title)
 
   const nextWord = () => {
-    if (currentWordIndex < Math.min(pageSize, (data?.count || {}) - 1)) {
+    if (
+      currentWordIndex < Math.min(PUZZLES_PER_PAGE, (data?.count || {}) - 1)
+    ) {
       setCurrentWordIndex(currentWordIndex + 1)
     } else {
       // If we run out of puzzles trigger a fetch for another page
@@ -57,43 +37,28 @@ function ParachuteData() {
   }
 
   useEffect(() => {
-    const createPuzzle = (splitCharsBase) => {
-      // If the splitCharsBase is not empty generate a puzzle
-      if (splitCharsBase?.length > 0) {
-        const puzzleParts = []
-        splitCharsBase.forEach((letter) => {
-          if (letter === ' ') {
-            puzzleParts.push({ letter: ' ', found: true })
-          } else {
-            puzzleParts.push({ letter, found: false })
-          }
-        })
-
+    const getPuzzle = () => {
+      if (puzzles[currentWordIndex].length > 0) {
         setPagesWithoutUsablePuzzle(0)
-        return puzzleParts
+        return puzzles[currentWordIndex]
       }
       // If we have already fetched the max number of pages and still not found a usable puzzle return an empty array to prevent possible infinite requests
       if (pagesWithoutUsablePuzzle < MAX_PAGES_WITHOUT_USABLE_PUZZLE) {
-        // If the splitCharsBase is empty then go to the next word
+        // If the puzzle pieces array is empty then go to the next word
         nextWord()
       }
       return []
     }
 
-    if (!pageSize && data?.pageSize) {
-      setPageSize(data?.pageSize)
-    }
     if (data?.results) {
-      setPuzzle(
-        createPuzzle(data?.results?.[currentWordIndex]?.entry?.splitCharsBase),
-      )
+      setCurrentPuzzle(getPuzzle())
     }
-  }, [data?.results, currentWordIndex, pageSize, data?.pageSize])
+  }, [currentWordIndex, data])
 
-  return pageSize > 0 && characters?.length > 0
+  return !!currentPuzzle && characters?.length > 0
     ? {
         isLoading: false,
-        puzzle,
+        puzzle: currentPuzzle,
         translation:
           data?.results?.[currentWordIndex]?.entry?.translations?.[0]?.text,
         audio: data?.results?.[currentWordIndex]?.entry?.relatedAudio?.[0],
