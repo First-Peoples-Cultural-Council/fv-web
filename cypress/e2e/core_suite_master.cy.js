ac31e2078ce81dd8efd986208c4d5eb0c57c736a
chris nahanee <chris@fpcc.ca>
2024-01-12 13:50:13 -0800

    adjusting loading screen location

--- cypress/e2e/core_suite_master.cy.js
+++ cypress/e2e/core_suite_master.cy.js
@@ -107,8 +107,8 @@ describe('log in/out', () => {
   it('1.2 - signin - no redirect', () => {
     cy.visit(`${Cypress.env('baseUrl')}`)
     cy.contains('Sign in').click()
-    cy.contains('Loading').should('not.exist')
     cy.origin('https://fpcc-dev.auth.ca-central-1.amazoncognito.com', () => {
+      cy.contains('Loading').should('not.exist')
       cy.url().should(
         'match',
         /^https:\/\/fpcc-dev\.auth\.ca-central-1\.amazoncognito\.com/,
