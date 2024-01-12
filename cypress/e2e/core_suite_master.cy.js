b003e920e45cf46d1201c490ed763214e03f8c94
chris nahanee <chris@fpcc.ca>
2024-01-12 15:34:30 -0800

    add before origin 2

--- cypress/e2e/core_suite_master.cy.js
+++ cypress/e2e/core_suite_master.cy.js
@@ -93,9 +93,7 @@ describe('log in/out', () => {
   beforeEach(() => {
     cy.viewport(1024, 768)
     cy.on('uncaught:exception', () => false)
-    cy.origin('https://fpcc-dev.auth.ca-central-1.amazoncognito.com', () => {
-      Cypress.require('../support/commands')
-    })
+    cy.origin('https://fpcc-dev.auth.ca-central-1.amazoncognito.com', () => {})
   })
 
   it('1.1 - signin/signout', () => {
@@ -110,7 +108,6 @@ describe('log in/out', () => {
   it('1.2 - signin - no redirect', () => {
     cy.visit(`${Cypress.env('baseUrl')}`)
     cy.contains('Sign in').click()
-
     cy.origin('https://fpcc-dev.auth.ca-central-1.amazoncognito.com', () => {
       cy.contains('Sign in with your email and password', {
         timeout: 10000,
