e6da3a1fb932e014974b0f67347abbc2a9940fed
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-01-23 14:24:02 -0800

    Fw 5442 Update end of game screen for wordsy (#312)
    * Added endgame modal, added a timer and a search button to the design
    
    * Updated styling for the wordsy end game modal
    
    * updated tailwind.config with wordsy colors, added link to look up words in site
    
    * refactored countdown timer to return strings
    
    * Updated text copy for wordsy modal
    
    * changed directory structure for wordsy-endGameModal

---
+++ src/components/Game/Wordsy/Utils/CountdownTimer.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+
+function convertTimeToLocalString(time) {
+  return time.toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  })
+}
+
+function CountdownTimer() {
+  const endTime = new Date()
+  endTime.setHours(23, 59, 59, 999)
+
+  const [countDown, setCountDown] = useState(endTime - new Date().time)
+
+  const [hours, setHours] = useState('')
+  const [minutes, setMinutes] = useState('')
+  const [seconds, setSeconds] = useState('')
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(endTime - new Date().getTime())
+    }, 1000)
+
+    return () => clearInterval(interval)
+  })
+
+  useEffect(() => {
+    setHours(
+      convertTimeToLocalString(
+        Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      ),
+    )
+    setMinutes(
+      convertTimeToLocalString(
+        Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60)),
+      ),
+    )
+    setSeconds(
+      convertTimeToLocalString(Math.floor((countDown % (1000 * 60)) / 1000)),
+    )
+  }, [countDown])
+
+  return `${hours}:${minutes}:${seconds}`
+}
+
+export default CountdownTimer
