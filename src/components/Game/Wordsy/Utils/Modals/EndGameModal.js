86eee2a3dbc013ec92237b154f92cc477134ccc0
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-01-23 11:30:50 -0800

    updated tailwind.config with wordsy colors, added link to look up words in site

--- src/components/Game/Wordsy/Utils/Modals/EndGameModal.js
+++ src/components/Game/Wordsy/Utils/Modals/EndGameModal.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link, useParams } from 'react-router-dom'
 
 // FPCC
 import Modal from 'components/Modal'
 import CountdownTimer from 'components/Game/Wordsy/Utils/CountdownTimer'
 
 function EndGameModal({ isModalOpen, setIsModalOpen, status, text, solution }) {
+  const { sitename } = useParams()
+
   const modalStyles =
-    'text-gray-900 rounded-lg p-6 lg:p-8 overflow-hidden shadow-xl transform transition-all sm:align-middle sm:max-w-sm sm:w-full'
-  const modalTextStyles = status === 'win' ? 'bg-green-200' : 'bg-red-200'
+    'text-fv-charcoal rounded-lg p-6 lg:p-8 overflow-hidden shadow-xl transform transition-all sm:align-middle sm:max-w-sm sm:w-full'
+  const modalTextStyles = status === 'win' ? 'bg-wordsy-green' : 'bg-wordsy-red'
 
   return (
     <Modal.Presentation
@@ -18,19 +21,22 @@ function EndGameModal({ isModalOpen, setIsModalOpen, status, text, solution }) {
       <div className={`${modalTextStyles} ${modalStyles}`}>
         <h3 className="text-sm text-center font-medium">{text}</h3>
 
-        <hr className="bg-gray-700 w-48 h-1  border-0 rounded my-4 mx-auto" />
+        <hr className="bg-fv-charcoal w-48 h-1  border-0 rounded my-4 mx-auto" />
 
         <div className={`mt-4 ${modalTextStyles}`}>
           <h3>
             Today&apos;s word is: <span className="font-bold">{solution}</span>.
           </h3>
           <div className="mt-4 flex justify-between">
-            <button
-              type="button"
+            <Link
+              to={{
+                pathname: `/${sitename}/words`,
+                search: `?q=${solution}&types=word`,
+              }}
               className="px-5 py-2 mx-2 bg-primary hover:bg-primary-dark font-medium rounded-lg shadow-sm text-base text-center text-white"
             >
               Look up <span className="font-bold">{solution}</span>.
-            </button>
+            </Link>
             <h3>
               Next word in <CountdownTimer />
             </h3>
