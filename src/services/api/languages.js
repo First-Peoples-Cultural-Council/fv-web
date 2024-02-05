602dda67cc9c880f8d82f335a8d138d43f0ef041
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-02-05 12:02:29 -0800

    Remove nested template literals

--- src/services/api/languages.js
+++ src/services/api/languages.js
@@ -3,10 +3,10 @@ import { LANGUAGES } from 'common/constants'
 
 const languages = {
   get: async ({ id }) => apiBase().get(`${LANGUAGES}/${id}/`).json(),
-  getAll: async ({ query }) =>
-    apiBase()
-      .get(`${LANGUAGES}?page=1&pageSize=100${query ? `&q=${query}` : ''}`)
-      .json(),
+  getAll: async ({ query }) => {
+    const _query = query ? `&q=${query}` : ''
+    return apiBase().get(`${LANGUAGES}?page=1&pageSize=100${_query}`).json()
+  },
 }
 
 export default languages
