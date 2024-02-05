4f7c15dbbf4fb9fd951a799064ca596d95946381
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-02-05 15:03:16 -0800

    Separate combined apis and rename site to sites

--- src/services/api/sites.js
+++ src/services/api/sites.js
@@ -1,10 +1,9 @@
 import { apiBase } from 'services/config'
-import { SITES, MY_SITES } from 'common/constants'
+import { SITES } from 'common/constants'
 
 const site = {
   get: async ({ sitename }) => apiBase().get(`${SITES}/${sitename}/`).json(),
-  getSites: async () => apiBase().get(SITES).json(),
-  mySites: async () => apiBase().get(MY_SITES).json(),
+  getAll: async () => apiBase().get(SITES).json(),
   update: async ({ sitename, properties }) =>
     apiBase().put(`${SITES}/${sitename}/`, { json: properties }).json(),
   partialUpdate: async ({ sitename, properties }) =>
