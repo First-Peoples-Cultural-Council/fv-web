92b4b8b7439ec521114529ef3721dc01a645174c
Cara Barter <carabarter@gmail.com>
2024-02-08 13:52:21 -0800

    Fw 4786 dashboard qol categories (#344)
    * reduced h of modal in CategoryArrayField, added overflow-y-scroll to CategoriesBrowserPresentation
    
    * add categories scrolling properly
    
    * styling complete, button only shows once category is selected

--- src/components/CategoriesBrowser/CategoriesBrowserPresentation.js
+++ src/components/CategoriesBrowser/CategoriesBrowserPresentation.js
@@ -19,8 +19,8 @@ function CategoriesBrowserPresentation({
   }
 
   return (
-    <main id="CategoriesBrowserPresentation">
-      <div className="h-full w-full bg-white p-4 min-h-screen">
+    <section id="CategoriesBrowserPresentation" className="h-full">
+      <div className="w-full bg-white p-4">
         <header className="p-2">
           <div className="flex-1 flex">
             <form className="w-full flex">
@@ -37,23 +37,25 @@ function CategoriesBrowserPresentation({
               </div>
             </form>
           </div>
-          <button
-            type="button"
-            onClick={() => chooseDocHandler(currentCategory)}
-            className="mx-auto my-4 bg-secondary border border-transparent rounded-lg shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-secondary-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-light"
-          >
-            {getIcon('Add', 'fill-current -ml-1 mr-2 h-5 w-5')}
-            <span>Add Category to document</span>
-          </button>
+          {currentCategory && (
+            <button
+              type="button"
+              onClick={() => chooseDocHandler(currentCategory)}
+              className="mx-auto my-4 bg-secondary border border-transparent rounded-lg shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-secondary-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-light"
+            >
+              {getIcon('Add', 'fill-current -ml-1 mr-2 h-5 w-5')}
+              <span>Add Category to document</span>
+            </button>
+          )}
         </header>
         <Loading.Container isLoading={isLoading}>
           {filteredCategories && (
             <section className="text-left">
               <div className="flex justify-start px-4 py-2 font-semibold">
-                <h2 className="w-1/2">Category</h2>
+                <h2 className="w-1/2 ml-4">Category</h2>
                 <h2 className="w-1/2">Parent Category</h2>
               </div>
-              <ul className="flex-col divide-y divide-gray-200 bg-white">
+              <ul className="flex-col divide-y divide-gray-200 bg-white overflow-y-scroll grow mt-2 h-72">
                 {filteredCategories?.map((category) => (
                   <button
                     data-testid="DashboardCategoryRow"
@@ -64,7 +66,7 @@ function CategoriesBrowserPresentation({
                       category?.id === currentCategory?.id
                         ? 'ring-2 ring-offset-2 ring-primary'
                         : 'focus-within:ring-2 focus-within:ring-offset-1 focus-within:ring-offset-gray-100 focus-within:ring-primary'
-                    } flex w-full p-4 cursor-pointer rounded-sm`}
+                    } flex mx-auto w-11/12 p-4 cursor-pointer rounded-sm`}
                   >
                     <div className="w-1/2 text-left">{category?.title}</div>
                     <div className="w-1/2 text-left">
@@ -80,7 +82,7 @@ function CategoriesBrowserPresentation({
           )}
         </Loading.Container>
       </div>
-    </main>
+    </section>
   )
 }
 
