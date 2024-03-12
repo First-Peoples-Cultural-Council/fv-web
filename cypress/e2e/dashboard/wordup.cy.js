3b3a69449563e967cc1181e6a5445cf3a28e6788
chris nahanee <chris@fpcc.ca>
2024-03-12 13:37:42 -0700

    changing phrase search setup

--- cypress/e2e/dashboard/wordup.cy.js
+++ cypress/e2e/dashboard/wordup.cy.js
@@ -80,6 +80,11 @@ describe('word Test', () => {
       delay: 2500,
     })
     cy.get('[id="SearchSubmit"]').click()
+    cy.contains('Clear Search').click()
+    cy.get('[data-testid="SearchInput"]').type(`${name2}`, {
+      delay: 2500,
+    })
+    cy.get('[id="SearchSubmit"]').click()
     cy.contains(name2).click()
     cy.get('[data-testid="EntryDrawerEdit"]').invoke('removeAttr', 'target')
     cy.get('[data-testid="EntryDrawerEdit"]').click()
