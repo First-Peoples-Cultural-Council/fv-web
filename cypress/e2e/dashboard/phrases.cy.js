ce9757122f01a97e570c0e7c0538fde540a2b9b0
chrisfpcc <98347472+chrisfpcc@users.noreply.github.com>
2024-04-11 10:16:00 -0700

    FW-3639 new tests (#413)
    * fixed tests, added new test - gallery
    
    * latest updates/fixes
    
    * fixing git action file
    
    * fixed retryability on all tests

--- cypress/e2e/dashboard/phrases.cy.js
+++ cypress/e2e/dashboard/phrases.cy.js
@@ -1,51 +1,81 @@
 /// <reference types="cypress" />
 
-describe('word Test', () => {
-  beforeEach(() => {
-    cy.on('uncaught:exception', () => false)
-    cy.viewport(1024, 768)
-    cy.visit(`${Cypress.env('baseUrl')}`)
-    cy.contains('Sign in').click()
-    cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
-      Cypress.require('/cypress/support/commands')
-    })
-    cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
-      cy.login(
-        Cypress.env('CYPRESS_FV_USERNAME'),
-        Cypress.env('CYPRESS_FV_PASSWORD'),
-      )
-    })
-  })
+describe(
+  'phrase Test',
+  {
+    retries: {
+      runMode: 2,
+      openMode: 1,
+    },
+  },
+  () => {
+    beforeEach(() => {
+      Cypress.Commands.add('middlestuff', (_translationwp) => {
+        cy.contains(_translationwp).click()
+        cy.get('[name="translations.0.text"]').type(
+          'an individual who fought for a warrior challenging a ruling of the Klingon High Council',
+        )
 
-  it('8.1 - create phrase', () => {
-    cy.on('uncaught:exception', () => false)
-    cy.viewport(1920, 1080)
+        cy.contains('Next step').click()
 
-    cy.contains('Explore Languages').click()
+        cy.contains('Next step').click()
+        cy.contains('Finish').click()
+        cy.contains('Dismiss').should('not.exist')
+      })
+      cy.viewport(1920, 1080)
+      cy.visit(`${Cypress.env('baseUrl')}`)
+      cy.contains('Sign in').click()
+      cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
+        Cypress.Commands.add('login', (email, password) => {
+          cy.on('uncaught:exception', () => false)
 
-    cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
-    cy.contains('Dashboard').click()
-    cy.contains('Create').click()
-    const name2 = `qatestwordh${new Date().getTime()}`
-    cy.contains('Create a phrase').click()
-    cy.contains('Finish').click()
-    cy.get('#title').type(name2)
-    cy.middlestuff('Add phrase translation')
-    cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).should('be.enabled')
-    cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
-    cy.contains('Dashboard').click()
-    cy.get('[data-testid="DashboardPresentationEdit"]').click()
-    cy.contains('Edit phrases').click()
-    cy.contains('Loading').should('be.visible')
-    cy.contains('Loading').should('not.exist')
-    cy.get('[data-testid="SearchInput"]').type(`${name2}`, {
-      delay: 50,
+          cy.get('#signInFormUsername').type(email, { force: true })
+          // lets try an incorrect password
+          cy.get('#signInFormPassword').type(`${password}{enter}`, {
+            force: true,
+          })
+        })
+
+        cy.login(
+          Cypress.env('CYPRESS_FV_USERNAME'),
+          Cypress.env('CYPRESS_FV_PASSWORD'),
+        )
+      })
     })
-    cy.get('[id="SearchSubmit"]').click()
-    cy.contains(name2).click()
-    cy.get('[data-testid="EntryDrawerEdit"]').invoke('removeAttr', 'target')
-    cy.get('[data-testid="EntryDrawerEdit"]').click()
-    cy.contains('Delete phrase').click()
-    cy.get('[data-testid="DeleteModal"]').contains('Delete').click()
-  })
-}) // end of describe
+    // const name2 = `qatestwordh${new Date().getTime()}`
+    const name2 = `testing this phrase`
+
+    it('8.1 - create phrase', () => {
+      cy.contains('Explore Languages').click()
+
+      cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
+      cy.contains('Dashboard').click()
+      cy.contains('Create').click()
+
+      cy.contains('Create a phrase').click()
+      cy.contains('Finish').click()
+      cy.get('#title').type(name2)
+      cy.middlestuff('Add phrase translation')
+      cy.get('div[id="NavUser"] button').click()
+      cy.contains('Sign out').click()
+    })
+
+    it('8.1a - delete phrase', () => {
+      cy.contains('Explore Languages').click()
+
+      cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
+      cy.contains('Dashboard').click()
+      cy.get('[data-testid="DashboardPresentationEdit"]').click()
+      cy.contains('Edit phrases').click()
+      cy.contains('Loading').should('be.visible')
+      cy.contains('Loading').should('not.exist')
+      cy.get('[data-testid="SearchInput"]').type(`${name2}`)
+      cy.get('[id="SearchSubmit"]').click()
+      cy.contains(name2).click()
+      cy.get('[data-testid="EntryDrawerEdit"]').invoke('removeAttr', 'target')
+      cy.get('[data-testid="EntryDrawerEdit"]').click()
+      cy.contains('Delete phrase').click()
+      cy.get('[data-testid="DeleteModal"]').contains('Delete').click()
+    })
+  },
+) // end of describe
