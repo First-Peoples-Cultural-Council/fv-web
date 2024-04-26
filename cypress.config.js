c252fe9b01675130e64d49a56f73dd6491231b12
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-04-26 09:33:10 -0700

    Added env variables to cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   defaultCommandTimeout: 8000,
+  env: {
+    baseUrl: 'https://localhost:3000/',
+    DIALECT: 'lilwat',
+  },
   e2e: {
     experimentalOriginDependencies: true,
   },
