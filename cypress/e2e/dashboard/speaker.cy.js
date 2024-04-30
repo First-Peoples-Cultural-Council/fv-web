a9177246c2cc5135975e281fdf67ccf45bbdd74e
chris nahanee <chris@fpcc.ca>
2024-04-30 14:56:17 -0700

    fixing speaker test

--- cypress/e2e/dashboard/speaker.cy.js
+++ cypress/e2e/dashboard/speaker.cy.js
@@ -1,58 +1,67 @@
 /// <reference types="cypress" />
 
-describe('Dashboard - Speaker testing', () => {
-  beforeEach(() => {
-    cy.viewport(1024, 768)
+describe(
+  'Dashboard - Speaker testing',
+  {
+    retries: {
+      runMode: 2,
+      openMode: 1,
+    },
+  },
+  () => {
+    beforeEach(() => {
+      cy.viewport(1024, 768)
 
-    Cypress.Commands.add('_login', () => {
-      cy.visit(`${Cypress.env('baseUrl')}`)
-      cy.contains('Sign in').click()
-      cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
-        Cypress.Commands.add('login', (email, password) => {
-          cy.on('uncaught:exception', () => false)
+      Cypress.Commands.add('_login', () => {
+        cy.visit(`${Cypress.env('baseUrl')}`)
+        cy.contains('Sign in').click()
+        cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
+          Cypress.Commands.add('login', (email, password) => {
+            cy.on('uncaught:exception', () => false)
 
-          cy.get('#signInFormUsername').type(email, { force: true })
-          // lets try an incorrect password
-          cy.get('#signInFormPassword').type(`${password}{enter}`, {
-            force: true,
+            cy.get('#signInFormUsername').type(email, { force: true })
+            // lets try an incorrect password
+            cy.get('#signInFormPassword').type(`${password}{enter}`, {
+              force: true,
+            })
           })
+          cy.contains('Sign in with your email and password').should('exist')
+          cy.login(
+            Cypress.env('CYPRESS_FV_USERNAME'),
+            Cypress.env('CYPRESS_FV_PASSWORD'),
+          )
         })
-        cy.contains('Sign in with your email and password').should('exist')
-        cy.login(
-          Cypress.env('CYPRESS_FV_USERNAME'),
-          Cypress.env('CYPRESS_FV_PASSWORD'),
-        )
+
+        cy.contains('Explore Languages').click()
       })
-
-      cy.contains('Explore Languages').click()
     })
-  })
-  it('Create Speaker', () => {
-    cy._login()
-    cy.visit(`${Cypress.env('baseUrl')}${Cypress.env('DIALECT')}`)
-    cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
-    cy.contains('Dashboard').click()
-    cy.contains('Create').click()
-    cy.contains('Add a speaker').click()
+    it('Create Speaker', () => {
+      cy._login()
+      cy.visit(`${Cypress.env('baseUrl')}${Cypress.env('DIALECT')}`)
+      cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
+      cy.contains('Dashboard').click()
+      cy.contains('Create').click()
+      cy.contains('Add a speaker').click()
 
-    cy.contains('Add Speaker').click()
-    cy.contains('name must be at least 1 characters').should('be.visible')
-    cy.contains('A bio is required').should('be.visible')
+      cy.contains('Add Speaker').click()
+      cy.contains('name must be at least 1 characters').should('be.visible')
+      cy.contains('A bio is required').should('be.visible')
 
-    cy.get('#name').type('qatestspeaker')
-    cy.get('#bio').type('qabio test - new speaker')
-    cy.contains('Add Speaker').click()
-  })
+      cy.get('#name').type('qatestspeaker')
+      cy.get('#bio').type('qabio test - new speaker')
+      cy.contains('Add Speaker').click()
+    })
 
-  it('Delete Speaker', () => {
-    cy._login()
-    cy.visit(`${Cypress.env('baseUrl')}${Cypress.env('DIALECT')}`)
-    cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
-    cy.contains('Dashboard').click()
-    cy.contains('Edit').click()
-    cy.contains('Edit speakers').click()
-    cy.get('td:contains("qatestspeaker")').siblings().children('a').click()
-    cy.contains('Delete Speaker').click()
-    cy.get('[data-testid="DeleteModal"]').contains('Delete').click()
-  })
-})
+    it('Delete Speaker', () => {
+      cy._login()
+      cy.visit(`${Cypress.env('baseUrl')}${Cypress.env('DIALECT')}`)
+      cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
+      cy.contains('Dashboard').click()
+      cy.contains('Edit').click()
+      cy.contains('Edit speakers').click()
+      cy.get('td:contains("qatestspeaker")').siblings().children('a').click()
+      cy.contains('Delete Speaker').click()
+      cy.get('[data-testid="DeleteModal"]').contains('Delete').click()
+    })
+  },
+)
