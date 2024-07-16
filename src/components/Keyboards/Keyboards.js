89ef5fc1cbba5a855ec46be38a704dd8a3880cb8
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-07-16 10:05:24 -0700

    FW-6001 Remove Chromebook link from form and widget (#511)
    * Remove Chromebook link from form and widget
    
    * Update text on Keyboards page
    
    * Add instructions for Chromebooks without tablet mode.

--- src/components/Keyboards/Keyboards.js
+++ src/components/Keyboards/Keyboards.js
@@ -225,25 +225,23 @@ function Keyboards() {
           ></iframe>
           <h2 className={headerStyle}>Using keyboards on Chromebooks</h2>
           <p className={paraStyle}>
-            If you are installing your keyboard on a Chromebook, Keyman
-            keyboards are not currently compatible. However, if the model of
-            your Chromebook has Tablet Mode, then you can use the FirstVoices
-            Keyboard App from the Google Play store when the Chromebook is in
-            tablet mode. And for Chromebooks that don&lsquo;t have tablet mode,
-            a FirstVoices Chromebook extension is now available for some
-            keyboards. If you would like your keyboard added to the Chromebook
-            extension, email us at{' '}
+            If the model of your Chromebook has Tablet Mode, then you can use
+            the FirstVoices Keyboard App from the Google Play store when the
+            Chromebook is in tablet mode.
+          </p>
+          <p className={paraStyle}>
+            For Chromebooks that do not have Tablet Mode, or if you are not able
+            to install the keyboard for any other reason, you can access a
+            no-install version of the keyboard right in your web browser on{' '}
             <a
-              href="mailto:hello@firstvoices.com"
+              href="https://keyman.com/"
               className="inline-url"
               target="_blank"
               rel="noreferrer noopener"
             >
-              hello@firstvoices.com
-            </a>
-            {/*
-             */}
-            .
+              the Keyman website.
+            </a>{' '}
+            Go to your language and click on &quot;Use keyboard online&quot;.
           </p>
           <iframe
             width="560"
