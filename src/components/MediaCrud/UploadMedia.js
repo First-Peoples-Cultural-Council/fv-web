43bdc3ee4601d9014cee4b4976a9685626f0a521
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-08-22 10:07:50 -0700

    refactored UploadMedia to remove useUppy hook (#564)

--- src/components/MediaCrud/UploadMedia.js
+++ src/components/MediaCrud/UploadMedia.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import Uppy from '@uppy/core'
-import { Dashboard, useUppy } from '@uppy/react'
+import { Dashboard } from '@uppy/react'
 import XHR from '@uppy/xhr-upload'
 import ImageEditor from '@uppy/image-editor'
 
@@ -22,91 +22,107 @@ import '@uppy/progress-bar/dist/style.css'
 import '@uppy/dashboard/dist/style.css'
 import '@uppy/image-editor/dist/style.css'
 
+function createUppy(
+  site,
+  maxFiles,
+  extensionList,
+  friendlyDocType,
+  setSelectedMedia,
+) {
+  const uppy = new Uppy({
+    id: 'mediaUpload',
+    autoProceed: false,
+    allowMultipleUploadBatches: true,
+    restrictions: {
+      maxNumberOfFiles: maxFiles,
+      requiredMetaFields: ['title'],
+    },
+    // eslint-disable-next-line no-unused-vars
+    onBeforeFileAdded: (currentFile, _files) => {
+      if (!extensionList.includes(getFileExtensions(currentFile.name))) {
+        uppy.info(
+          {
+            message: `Unsupported file type. Please upload media with the extension ${extensionList.join(
+              ', ',
+            )}`,
+            details:
+              'File couldn’t be uploaded because it was of unsupported type.',
+          },
+          'error',
+          5000,
+        )
+        return false
+      }
+      return true
+    },
+  })
+
+  uppy.on('file-added', (file) => {
+    uppy.setFileMeta(file.id, {
+      ...file.meta,
+      title: file.name,
+    })
+  })
+
+  uppy.use(ImageEditor, {
+    id: 'ImageEditor',
+    quality: 0.8,
+    cropperOptions: {
+      aspectRatio: 1,
+      viewMode: 1,
+      background: false,
+      autoCropArea: 1,
+      responsive: true,
+      croppedCanvasOptions: {},
+    },
+    actions: {
+      revert: true,
+      rotate: true,
+      granularRotate: true,
+      flip: true,
+      zoomIn: true,
+      zoomOut: true,
+      cropSquare: true,
+      cropWidescreen: true,
+      cropWidescreenVertical: true,
+    },
+  })
+
+  uppy.use(XHR, {
+    endpoint: api.media.getUploadEndpoint(site?.sitename, friendlyDocType),
+    fieldName: 'original',
+    formData: true,
+    headers: getAuthHeaderIfTokenExists(),
+    getResponseData: (responseText) => {
+      const _responseText = JSON.parse(responseText)
+      setSelectedMedia((oldArray) => [...oldArray, _responseText?.id])
+      return {
+        url: _responseText?.url,
+      }
+    },
+  })
+
+  return uppy
+}
+
 function UploadMedia({
   site,
   docType,
+  maxFiles,
   extensionList,
   setSelectedMedia,
-  maxFiles,
 }) {
   const friendlyDocType = getFriendlyDocType({ docType, plural: true })
 
-  const uppy = useUppy(() => {
-    const _uppy = new Uppy({
-      id: 'mediaUpload',
-      autoProceed: false,
-      allowMultipleUploadBatches: true,
-      restrictions: {
-        maxNumberOfFiles: maxFiles,
-        requiredMetaFields: ['title'],
-      },
-      // eslint-disable-next-line no-unused-vars
-      onBeforeFileAdded: (currentFile, _files) => {
-        if (!extensionList.includes(getFileExtensions(currentFile.name))) {
-          uppy.info(
-            {
-              message: `Unsupported file type. Please upload media with the extension ${extensionList.join(
-                ', ',
-              )}`,
-              details:
-                'File couldn’t be uploaded because it was of unsupported type.',
-            },
-            'error',
-            5000,
-          )
-          return false
-        }
-        return true
-      },
-    })
-
-    _uppy.on('file-added', (file) => {
-      uppy.setFileMeta(file.id, {
-        ...file.meta,
-        title: file.name,
-      })
-    })
-
-    _uppy.use(ImageEditor, {
-      id: 'ImageEditor',
-      quality: 0.8,
-      cropperOptions: {
-        aspectRatio: 1,
-        viewMode: 1,
-        background: false,
-        autoCropArea: 1,
-        responsive: true,
-        croppedCanvasOptions: {},
-      },
-      actions: {
-        revert: true,
-        rotate: true,
-        granularRotate: true,
-        flip: true,
-        zoomIn: true,
-        zoomOut: true,
-        cropSquare: true,
-        cropWidescreen: true,
-        cropWidescreenVertical: true,
-      },
-    })
-
-    _uppy.use(XHR, {
-      endpoint: api.media.getUploadEndpoint(site?.sitename, friendlyDocType),
-      fieldName: 'original',
-      formData: true,
-      headers: getAuthHeaderIfTokenExists(),
-      getResponseData: (responseText) => {
-        const _responseText = JSON.parse(responseText)
-        setSelectedMedia((oldArray) => [...oldArray, _responseText?.id])
-        return {
-          url: _responseText?.url,
-        }
-      },
-    })
-
-    return _uppy
-  })
+  const [uppy] = useState(
+    createUppy(
+      site,
+      maxFiles,
+      extensionList,
+      friendlyDocType,
+      setSelectedMedia,
+    ),
+  )
 
   useEffect(() => () => uppy.close({ reason: 'unmount' }), [uppy])
 
