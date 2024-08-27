79d1f36b912f01e25b8bbfa75ee9e2cda09f9406
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-08-27 16:26:13 -0700

    Delete old MediaCrud

--- src/components/MediaCrud/index.js
+++
@@ -1,7 +0,0 @@
-import MediaCrudContainer from 'components/MediaCrud/MediaCrudContainer'
-import MediaCrudData from 'components/MediaCrud/MediaCrudData'
-
-export default {
-  Container: MediaCrudContainer,
-  Data: MediaCrudData,
-}
