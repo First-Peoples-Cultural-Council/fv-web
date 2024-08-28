a64d6160d2125828dc5764642451e944e62e5251
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-08-28 16:31:10 -0700

    Create Modal wrapper to reuse on separate type modals

--- src/components/AddMediaModal/AddMediaModalContainer.js
+++ src/components/AddMediaModal/AddMediaModalContainer.js
@@ -2,15 +2,14 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 // FPCC
-import AddMediaModalData from 'components/AddMediaModal/AddMediaModalData'
 import VideoLinkForm from 'components/VideoLinkForm'
 import { TYPE_AUDIO, TYPE_IMAGE, TYPE_VIDEO } from 'common/constants'
 import UploadAudio from 'components/UploadAudio'
 import UploadVisualMedia from 'components/UploadVisualMedia'
 import AudioSelector from 'components/AudioSelector'
 import VisualMediaSelector from 'components/VisualMediaSelector'
-import Modal from 'components/Modal'
-import { getFriendlyDocType } from 'common/utils/stringHelpers'
+import useArrayStateManager from 'common/hooks/useArrayStateManager'
+import AddMediaModalWrapper from 'components/AddMediaModalWrapper'
 
 function AddMediaModalContainer({
   savedMedia,
@@ -22,8 +21,8 @@ function AddMediaModalContainer({
   modalOpen,
   closeModal,
 }) {
-  const { selectedMedia, setSelectedMedia, mediaSelectHandler } =
-    AddMediaModalData({ type, maxFiles })
+  const { selectedItems, setSelectedItems, handleSelectAdditionalItems } =
+    useArrayStateManager({ maxItems: maxFiles })
 
   let tabOptions = [
     {
@@ -51,45 +50,12 @@ function AddMediaModalContainer({
 
   const [selectedTab, setSelectedTab] = useState(tabOptions[1])
 
-  const tabButton = (tab) => {
-    const itemsSelected = selectedMedia.length > 0
-    const tabHasSelectedItems = itemsSelected && selectedTab.id === tab.id
-    // If no files are uploaded/selected, allow the user to switch tabs
-    // otherwise switch to Insert Media button
-    // allowing user to attach the selected/uploaded files to the document.
-    const handleOnClick = () => {
-      if (tabHasSelectedItems) updateSavedMedia(selectedMedia)
-      setSelectedTab(tab)
-    }
-    return (
-      <button
-        key={`${tab.id}-btn`}
-        data-testid={`${tab.id}-btn`}
-        type="button"
-        className={`capitalize disabled:pointer-events-none disabled:bg-gray-100 disabled:opacity-50 ${
-          selectedTab.id === tab.id
-            ? 'btn-contained'
-            : 'btn-outlined hover:btn-contained'
-        }`}
-        onClick={handleOnClick}
-        disabled={itemsSelected && selectedTab.id !== tab.id}
-      >
-        {tabHasSelectedItems
-          ? `Insert ${selectedMedia.length} ${getFriendlyDocType({
-              docType: type,
-              plural: true,
-            })}`
-          : tab.btnLabel}
-      </button>
-    )
-  }
-
   const uploadTabContents =
     type === TYPE_AUDIO ? (
-      <UploadAudio setSelectedMedia={setSelectedMedia} />
+      <UploadAudio setSelectedMedia={setSelectedItems} />
     ) : (
       <UploadVisualMedia
-        setSelectedMedia={setSelectedMedia}
+        setSelectedMedia={setSelectedItems}
         type={type}
         maxFiles={maxFiles}
       />
@@ -99,49 +65,42 @@ function AddMediaModalContainer({
     type === TYPE_AUDIO ? (
       <AudioSelector.Container
         savedMedia={savedMedia}
-        selectedMedia={selectedMedia}
-        mediaSelectHandler={mediaSelectHandler}
+        selectedMedia={selectedItems}
+        mediaSelectHandler={handleSelectAdditionalItems}
       />
     ) : (
       <VisualMediaSelector.Container
         type={type}
         savedMedia={savedMedia}
-        selectedMedia={selectedMedia}
-        mediaSelectHandler={mediaSelectHandler}
+        selectedMedia={selectedItems}
+        mediaSelectHandler={handleSelectAdditionalItems}
       />
     )
 
   return (
-    <Modal.Presentation
-      isOpen={modalOpen}
-      closeHandler={closeModal}
-      isDashboard
+    <AddMediaModalWrapper
+      selectedMedia={selectedItems}
+      updateSavedMedia={updateSavedMedia}
+      type={type}
+      modalOpen={modalOpen}
+      closeModal={closeModal}
+      tabOptions={tabOptions}
+      selectedTab={selectedTab}
+      setSelectedTab={setSelectedTab}
     >
-      <div className="h-4/5-screen w-3/4-screen mx-auto rounded-lg overflow-hidden bg-gray-50 p-4">
-        <div id="AddMediaModalContainer" className="h-full flex flex-col">
-          <div>
-            <h2 className="text-2xl font-bold text-fv-charcoal mb-4">
-              {selectedTab.title}
-            </h2>
-          </div>
-          <div className="w-full bg-gray-50 flex justify-center space-x-2">
-            {tabOptions.map((tab) => tabButton(tab))}
-          </div>
-          <div className="grow mt-2">
-            {selectedTab.id === 'upload-tab' && uploadTabContents}
-            {selectedTab.id === 'search-tab' && searchTabContents}
-            {selectedTab.id === 'video-link-tab' && (
-              <VideoLinkForm
-                relatedVideoLinks={relatedVideoLinks}
-                appendVideoLinks={appendVideoLinks}
-                closeModal={closeModal}
-                maxLinks={maxFiles}
-              />
-            )}
-          </div>
-        </div>
-      </div>
-    </Modal.Presentation>
+      <>
+        {selectedTab.id === 'upload-tab' && uploadTabContents}
+        {selectedTab.id === 'search-tab' && searchTabContents}
+        {selectedTab.id === 'video-link-tab' && (
+          <VideoLinkForm
+            relatedVideoLinks={relatedVideoLinks}
+            appendVideoLinks={appendVideoLinks}
+            closeModal={closeModal}
+            maxLinks={maxFiles}
+          />
+        )}
+      </>
+    </AddMediaModalWrapper>
   )
 }
 
--- src/components/AddMediaModal/index.js
+++ src/components/AddMediaModal/index.js
@@ -1,7 +1,5 @@
 import AddMediaModalContainer from 'components/AddMediaModal/AddMediaModalContainer'
-import AddMediaModalData from 'components/AddMediaModal/AddMediaModalData'
 
 export default {
   Container: AddMediaModalContainer,
-  Data: AddMediaModalData,
 }
