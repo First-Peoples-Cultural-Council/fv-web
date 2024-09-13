bbb627b0abd2ed4bd57941dbc33439a10db6edcb
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-09-13 15:10:18 -0700

    Replace SearchSelector with smaller components in DashboardMedia

--- src/components/DashboardMediaVisual/DashboardMediaVisualContainer.js
+++ src/components/DashboardMediaVisual/DashboardMediaVisualContainer.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types'
 // FPCC
 import DashboardMediaVisualPresentation from 'components/DashboardMediaVisual/DashboardMediaVisualPresentation'
 import useMediaSearch from 'common/dataHooks/useMediaSearch'
-import MediaDetails from 'components/MediaDetails'
-import SearchSelector from 'components/SearchSelector'
+import SelectorSearchbox from 'components/SelectorSearchbox'
+import SelectorResultsWrapper from 'components/SelectorResultsWrapper'
 import { TYPE_IMAGE, TYPE_VIDEO } from 'common/constants'
 
 function DashboardMediaVisualContainer({ type }) {
@@ -23,40 +23,44 @@ function DashboardMediaVisualContainer({ type }) {
     typePlural,
   } = useMediaSearch({ type })
 
-  const hasResults = !!(media?.pages !== undefined && media?.pages?.[0]?.results?.length > 0)
+  const hasResults = !!(
+    media?.pages !== undefined && media?.pages?.[0]?.results?.length > 0
+  )
 
   return (
-    <SearchSelector.Presentation
-      searchQuery={searchValue}
-      searchPromptText={`Search all ${typePlural}`}
-      setSearchQuery={handleTextFieldChange}
-      search={handleSearchSubmitWithUrlSync}
-      headerSection=""
-      resultsSection={
-        <div className="grid grid-cols-3 w-full">
-          <main className="col-span-2 pt-4 mx-2">
-            <section className="p-2 h-full" aria-labelledby="results-header">
-              <h1 id="results-header" className="capitalize flex text-2xl font-bold text-fv-charcoal mb-4">
-                {typePlural}
-              </h1>
+    <div
+      data-testid="DashboardMediaAudioContainer"
+      className="h-full bg-gray-50"
+    >
+      <div className="h-full w-full flex flex-col">
+        <div className="w-full">
+          <SelectorSearchbox.Presentation
+            onSearchChange={handleTextFieldChange}
+            onSearchSubmit={handleSearchSubmitWithUrlSync}
+            searchPlaceholder={`Search all ${typePlural}`}
+            searchValue={searchValue}
+          />
+        </div>
+        <div>
+          <SelectorResultsWrapper.Presentation
+            hasResults={hasResults}
+            isLoading={isLoadingEntries}
+            loadRef={loadRef}
+            resultsSection={
               <DashboardMediaVisualPresentation
                 data={media}
                 infiniteScroll={infiniteScroll}
                 currentFile={currentFile}
                 setCurrentFile={setCurrentFile}
                 loadLabel={loadLabel}
+                type={type}
+                typePlural={typePlural}
               />
-            </section>
-          </main>
-          <aside className="col-span-1 bg-white p-8 border-1 border-gray-200">
-            <MediaDetails.Visual file={currentFile} docType={type} />
-          </aside>
+            }
+          />
         </div>
-      }
-      isLoadingEntries={isLoadingEntries}
-      hasResults={hasResults}
-      loadRef={loadRef}
-    />
+      </div>
+    </div>
   )
 }
 
--- src/components/DashboardMediaVisual/DashboardMediaVisualPresentation.js
+++ src/components/DashboardMediaVisual/DashboardMediaVisualPresentation.js
@@ -1,77 +1,97 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// FPCC
+import MediaDetails from 'components/MediaDetails'
+
 function DashboardMediaVisualPresentation({
   data,
   infiniteScroll,
   currentFile,
   setCurrentFile,
   loadLabel,
+  type,
+  typePlural,
 }) {
   const { isFetchingNextPage, fetchNextPage, hasNextPage } = infiniteScroll
   return (
     <div
       id="DashboardMediaVisualPresentation"
-      className="overflow-y-auto h-full"
+      className="grid grid-cols-3 w-full"
     >
-      <div>
-        <ul className="p-2 grid grid-cols-4 gap-y-8 gap-x-6 xl:gap-x-8">
-          {data?.pages !== undefined &&
-            data?.pages?.[0]?.results?.length > 0 &&
-            data?.pages?.map((page) => (
-              <React.Fragment key={page?.pageNumber}>
-                {page.results.map((doc) => (
-                  <li key={doc?.id} className="relative">
-                    <div
-                      className={`${
-                        doc?.id === currentFile?.id
-                          ? 'ring-4 ring-offset-2 ring-secondary'
-                          : 'focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-secondary'
-                      } group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 overflow-hidden`}
-                    >
-                      <img
-                        src={doc?.thumbnail}
-                        alt={doc?.title}
-                        className={`${
-                          doc?.id === currentFile?.id
-                            ? ''
-                            : 'group-hover:opacity-75'
-                        } object-cover pointer-events-none`}
-                      />
-                      <button
-                        data-testid=""
-                        type="button"
-                        className="absolute inset-0 focus:outline-none"
-                        onClick={() => setCurrentFile(doc)}
-                      >
-                        <span className="sr-only">
-                          View details for {doc?.title}
-                        </span>
-                      </button>
-                    </div>
-                    <p className="mt-2 block text-sm font-medium text-fv-charcoal truncate pointer-events-none">
-                      {doc?.title}
-                    </p>
-                    {doc?.width && doc?.height && (
-                      <p className="mt-2 block text-sm font-medium text-fv-charcoal-light truncate pointer-events-none">{`${doc?.width}x${doc?.height}`}</p>
-                    )}
-                  </li>
-                ))}
-              </React.Fragment>
-            ))}
-        </ul>
-        <div className="pt-10 text-center text-fv-charcoal font-medium">
-          <button
-            data-testid="load-btn"
-            type="button"
-            className={!hasNextPage ? 'cursor-text' : ''}
-            onClick={() => fetchNextPage()}
-            disabled={!hasNextPage || isFetchingNextPage}
+      <main className="col-span-2 pt-4 mx-2">
+        <section className="p-2 h-full" aria-labelledby="results-header">
+          <h1
+            id="results-header"
+            className="capitalize flex text-2xl font-bold text-fv-charcoal mb-4"
           >
-            {loadLabel}
-          </button>
-        </div>
-      </div>
+            {typePlural}
+          </h1>
+          <div className="overflow-y-auto h-full">
+            <div>
+              <ul className="p-2 grid grid-cols-4 gap-y-8 gap-x-6 xl:gap-x-8">
+                {data?.pages !== undefined &&
+                  data?.pages?.[0]?.results?.length > 0 &&
+                  data?.pages?.map((page) => (
+                    <React.Fragment key={page?.pageNumber}>
+                      {page.results.map((doc) => (
+                        <li key={doc?.id} className="relative">
+                          <div
+                            className={`${
+                              doc?.id === currentFile?.id
+                                ? 'ring-4 ring-offset-2 ring-secondary'
+                                : 'focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-secondary'
+                            } group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 overflow-hidden`}
+                          >
+                            <img
+                              src={doc?.thumbnail}
+                              alt={doc?.title}
+                              className={`${
+                                doc?.id === currentFile?.id
+                                  ? ''
+                                  : 'group-hover:opacity-75'
+                              } object-cover pointer-events-none`}
+                            />
+                            <button
+                              data-testid=""
+                              type="button"
+                              className="absolute inset-0 focus:outline-none"
+                              onClick={() => setCurrentFile(doc)}
+                            >
+                              <span className="sr-only">
+                                View details for {doc?.title}
+                              </span>
+                            </button>
+                          </div>
+                          <p className="mt-2 block text-sm font-medium text-fv-charcoal truncate pointer-events-none">
+                            {doc?.title}
+                          </p>
+                          {doc?.width && doc?.height && (
+                            <p className="mt-2 block text-sm font-medium text-fv-charcoal-light truncate pointer-events-none">{`${doc?.width}x${doc?.height}`}</p>
+                          )}
+                        </li>
+                      ))}
+                    </React.Fragment>
+                  ))}
+              </ul>
+              <div className="pt-10 text-center text-fv-charcoal font-medium">
+                <button
+                  data-testid="load-btn"
+                  type="button"
+                  className={!hasNextPage ? 'cursor-text' : ''}
+                  onClick={() => fetchNextPage()}
+                  disabled={!hasNextPage || isFetchingNextPage}
+                >
+                  {loadLabel}
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <aside className="col-span-1 bg-white p-8 border-1 border-gray-200">
+        <MediaDetails.Visual file={currentFile} docType={type} />
+      </aside>
     </div>
   )
 }
@@ -83,6 +103,8 @@ DashboardMediaVisualPresentation.propTypes = {
   currentFile: object,
   setCurrentFile: func,
   loadLabel: string,
+  type: string,
+  typePlural: string,
 }
 
 export default DashboardMediaVisualPresentation
