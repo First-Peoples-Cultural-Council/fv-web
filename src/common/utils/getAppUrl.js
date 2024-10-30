eb77dd39844071cdb927c8156faae060be7a9ad5
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2024-10-30 12:29:39 -0700

    fw-6218, make prod apps the default

--- src/common/utils/getAppUrl.js
+++ src/common/utils/getAppUrl.js
@@ -2,9 +2,10 @@ import GlobalConfiguration from 'src/GlobalConfiguration'
 import PropTypes from 'prop-types'
 
 function getAppUrl({ slug }) {
-  return GlobalConfiguration.ENV_APP === 'production'
-    ? `https://${slug}.firstvoicesapp.com`
-    : `https://${slug}.${GlobalConfiguration.ENV_APP}.firstvoicesapp.com`
+  return GlobalConfiguration.ENV_APP === 'dev' ||
+    GlobalConfiguration.ENV_APP === 'preprod'
+    ? `https://${slug}.${GlobalConfiguration.ENV_APP}.firstvoicesapp.com`
+    : `https://${slug}.firstvoicesapp.com`
 }
 
 function getAppLogoUrl({ slug }) {
