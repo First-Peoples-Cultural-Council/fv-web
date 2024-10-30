5f92aa959abe7094033b88ffb8c9f4d3380541ea
Sarah Mundy <109107992+sarahfirstvoices@users.noreply.github.com>
2024-10-30 12:38:19 -0700

    Fw-6218, fw-6219, fix app logo URLs and prod app URLs (#658)
    * fw-6218, make prod apps the default
    
    * fw-6219, use the prod app logo url format

--- src/common/utils/getAppUrl.js
+++ src/common/utils/getAppUrl.js
@@ -2,14 +2,15 @@ import GlobalConfiguration from 'src/GlobalConfiguration'
 import PropTypes from 'prop-types'
 
 function getAppUrl({ slug }) {
-  return GlobalConfiguration.ENV_APP === 'production'
-    ? `https://${slug}.firstvoicesapp.com`
-    : `https://${slug}.${GlobalConfiguration.ENV_APP}.firstvoicesapp.com`
+  return GlobalConfiguration.ENV_APP === 'dev' ||
+    GlobalConfiguration.ENV_APP === 'preprod'
+    ? `https://${slug}.${GlobalConfiguration.ENV_APP}.firstvoicesapp.com`
+    : `https://${slug}.firstvoicesapp.com`
 }
 
 function getAppLogoUrl({ slug }) {
   const appUrl = getAppUrl({ slug })
-  return `${appUrl}/assets/${slug}/logo192.png`
+  return `${appUrl}/${slug}/logo192.png`
 }
 
 const { string } = PropTypes
