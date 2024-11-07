83ce6614c4b24cc48ebf3abf46f824fbf33b76d2
Chris Nahanee <chris@fpcc.ca>
2024-11-07 15:45:24 -0800

    fixes tests to latest code

--- cypress/e2e/dashboard/speaker.cy.js
+++ cypress/e2e/dashboard/speaker.cy.js
@@ -12,31 +12,25 @@ describe(
     beforeEach(() => {
       cy.viewport(1024, 768)
 
-      Cypress.Commands.add('_login', () => {
-        cy.visit(`${Cypress.env('baseUrl')}`)
-        cy.contains('Sign in').click()
-        cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
-          Cypress.Commands.add('login', (email, password) => {
-            cy.on('uncaught:exception', () => false)
-
-            cy.get('#signInFormUsername').type(email, { force: true })
-            // lets try an incorrect password
-            cy.get('#signInFormPassword').type(`${password}{enter}`, {
-              force: true,
-            })
+      cy.visit(`${Cypress.env('baseUrl')}`)
+      cy.contains('Sign in').click()
+      cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
+        Cypress.Commands.add('login', (email, password) => {
+          cy.get('#signInFormUsername').type(email, { force: true })
+          // lets try an incorrect password
+          cy.get('#signInFormPassword').type(`${password}{enter}`, {
+            force: true,
           })
-          cy.contains('Sign in with your email and password').should('exist')
-          cy.login(
-            Cypress.env('CYPRESS_FV_USERNAME'),
-            Cypress.env('CYPRESS_FV_PASSWORD'),
-          )
         })
 
-        cy.contains('Explore Languages').click()
+        cy.login(
+          Cypress.env('CYPRESS_FV_USERNAME'),
+          Cypress.env('CYPRESS_FV_PASSWORD'),
+        )
       })
+      cy.contains('Explore Languages').click()
     })
     it('Create Speaker', () => {
-      cy._login()
       cy.visit(`${Cypress.env('baseUrl')}${Cypress.env('DIALECT')}`)
       cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
       cy.contains('Dashboard').click()
@@ -54,17 +48,12 @@ describe(
     })
 
     it('Delete Speaker', () => {
-      cy._login()
       cy.visit(`${Cypress.env('baseUrl')}${Cypress.env('DIALECT')}`)
       cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
       cy.contains('Dashboard').click()
       cy.contains('Edit').click()
       cy.contains('Edit speakers').click()
-      cy.get('td:contains("qatestspeaker")')
-        .first()
-        .siblings()
-        .children('a')
-        .click()
+      cy.get('[data-testid="edit-speaker-qatestspeaker"]').click()
       cy.contains('Delete Speaker').click()
       cy.get('[data-testid="DeleteModal"]').contains('Delete').click()
     })
--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -11,26 +11,25 @@ describe(
   () => {
     beforeEach(() => {
       cy.viewport(1024, 768)
-      Cypress.Commands.add('_login', () => {
-        cy.visit(`${Cypress.env('baseUrl')}`)
-        cy.contains('Sign in').click()
-        cy.origin(
-          'https://fpcc-dev.auth.ca-central-1.amazoncognito.com',
-          () => {
-            cy.contains('Sign in with your email and password').should('exist')
-            cy.login(
-              Cypress.env('CYPRESS_FV_USERNAME'),
-              Cypress.env('CYPRESS_FV_PASSWORD'),
-            )
-          },
-        )
+      cy.visit(`${Cypress.env('baseUrl')}`)
+      cy.contains('Sign in').click()
+      cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
+        Cypress.Commands.add('login', (email, password) => {
+          cy.get('#signInFormUsername').type(email, { force: true })
+          cy.get('#signInFormPassword').type(`${password}{enter}`, {
+            force: true,
+          })
+        })
 
-        cy.contains('Explore Languages').click()
+        cy.login(
+          Cypress.env('CYPRESS_FV_USERNAME'),
+          Cypress.env('CYPRESS_FV_PASSWORD'),
+        )
       })
+      cy.contains('Explore Languages').click()
     })
 
     it('1.1 - signin/signout', () => {
-      cy._login()
       cy.reload()
       cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
       cy.contains('Dashboard').should('exist')
@@ -39,6 +38,8 @@ describe(
 
     it('1.2 - signin - no redirect', () => {
       cy.visit(`${Cypress.env('baseUrl')}`)
+      cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
+      cy.contains('Sign out', { timeout: 12000 }).click()
       cy.contains('Sign in').click()
       cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
         cy.contains('Sign in with your email and password', {
@@ -48,7 +49,8 @@ describe(
     })
 
     it('1.3 - signin - browser back', () => {
-      cy.visit(`${Cypress.env('baseUrl')}`)
+      cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
+      cy.contains('Sign out', { timeout: 12000 }).click()
       cy.contains('Sign in').click()
       cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
         cy.contains('Sign in with your email and password').should('exist')
@@ -56,6 +58,15 @@ describe(
       })
 
       cy.contains('Sign in')
+    })
+
+    it('sign up', () => {
+      cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
+      cy.contains('Sign out', { timeout: 12000 }).click()
+      cy.contains('Sign in').click()
+      cy.origin(`${Cypress.env('CYPRESS_ORIGIN')}`, () => {
+        cy.contains('Sign up').click({ force: true })
+      })
     })
   },
 ) // end of describe
