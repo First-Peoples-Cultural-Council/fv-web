68fc86f1f1b929989c741d8df178b3c523942c26
Sahil Sharma <sahil.sharma071997@gmail.com>
2024-11-14 11:15:31 -0800

    removed unused imports

--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -1,7 +1,5 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
-const TerserPlugin = require('terser-webpack-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 const definitions = {
   CONFIGURATION_SOURCE: JSON.stringify('Caddy'),
