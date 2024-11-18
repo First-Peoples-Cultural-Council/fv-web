25fa4676245a50bdde6a4cfa98bf4a11cfe9892b
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-11-18 15:51:22 -0800

    Delete unused LandingAbout component

--- src/components/LandingAbout/index.js
+++
@@ -1,9 +0,0 @@
-import LandingAboutContainer from 'components/LandingAbout/LandingAboutContainer'
-import LandingAboutData from 'components/LandingAbout/LandingAboutData'
-import WidgetTextPresentation from 'components/WidgetText/WidgetTextPresentation'
-
-export default {
-  Container: LandingAboutContainer,
-  Data: LandingAboutData,
-  Presentation: WidgetTextPresentation,
-}
