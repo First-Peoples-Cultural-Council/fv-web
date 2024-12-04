39497a66eaf04dd4b1af1c263c90b5b1bd8e46ba
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-12-04 15:21:36 -0800

    Remove error navigate from Categories

--- src/components/Categories/CategoriesData.js
+++ src/components/Categories/CategoriesData.js
@@ -1,28 +1,16 @@
-import { useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 // FPCC
 import { useCategories } from 'common/dataHooks/useCategories'
 
 function CategoriesData() {
   const { sitename } = useParams()
-  const navigate = useNavigate()
 
   // Data fetch
-  const { isInitialLoading, error, isError, data } = useCategories()
-
-  useEffect(() => {
-    if (isError) {
-      navigate(
-        `/${sitename}/error?status=${error?.response?.status}&statusText=${error?.response?.statusText}&url=${error?.response?.url}`,
-        { replace: true },
-      )
-    }
-  }, [error, isError, navigate, sitename])
+  const categoryQueryReturn = useCategories()
 
   return {
-    categories: data?.results,
-    isLoading: isInitialLoading,
+    categoryQueryReturn,
     sitename,
   }
 }
