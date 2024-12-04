1ae03ebca4e5a6f9762e41fd24ed824a887f77f4
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-12-04 15:47:18 -0800

    Remove error navigate from Story

--- src/components/Story/StoryContainer.js
+++ src/components/Story/StoryContainer.js
@@ -3,29 +3,26 @@ import PropTypes from 'prop-types'
 import StoryData from 'components/Story/StoryData'
 import StoryPresentation from 'components/Story/StoryPresentation'
 import StoryPresentationDrawer from 'components/Story/StoryPresentationDrawer'
-import ErrorHandler from 'components/ErrorHandler'
-import Loading from 'components/Loading'
+import LoadOrError from 'components/LoadOrError'
 import { useParams } from 'react-router-dom'
 
 function StoryContainer({ docId, sitename, isDrawer }) {
   const { sitename: sitenameParams } = useParams()
   const sitenameToSend = sitename || sitenameParams
 
-  const { entry, isLoading, notFound, error } = StoryData({
+  const { entry, storyQueryReturn } = StoryData({
     docId,
     sitename: sitenameToSend,
   })
 
-  return notFound ? (
-    <ErrorHandler.Container error={{ status: error.response.status }} />
-  ) : (
-    <Loading.Container isLoading={isLoading}>
+  return (
+    <LoadOrError queryReturn={storyQueryReturn}>
       {isDrawer ? (
         <StoryPresentationDrawer entry={entry} sitename={sitenameToSend} />
       ) : (
         <StoryPresentation entry={entry} />
       )}
-    </Loading.Container>
+    </LoadOrError>
   )
 }
 
--- src/components/Story/StoryData.js
+++ src/components/Story/StoryData.js
@@ -1,36 +1,22 @@
-import { useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useStory } from 'common/dataHooks/useStories'
 
 function StoryData({ docId, sitename }) {
   const { id, sitename: paramsSitename } = useParams()
-  const navigate = useNavigate()
 
   const idToSend = docId || id
   const sitenameToSend = sitename || paramsSitename
 
   // Data fetch
-  const { data, isError, isInitialLoading, isFetched, error } = useStory({
+  const storyQueryReturn = useStory({
     id: idToSend,
     sitename: sitenameToSend,
   })
-  const entry = data?.title ? data : {}
-
-  useEffect(() => {
-    if (isError) {
-      navigate(
-        `/${sitenameToSend}/error?status=${error?.response?.status}&statusText=${error?.response?.statusText}&url=${error?.response?.url}`,
-        { replace: true },
-      )
-    }
-  }, [isError])
 
   return {
-    notFound: !!(isFetched && entry === null),
-    isLoading: isInitialLoading || isError,
-    entry,
+    storyQueryReturn,
+    entry: storyQueryReturn?.data?.title ? storyQueryReturn?.data : {},
     sitename: sitenameToSend,
-    error,
   }
 }
 
