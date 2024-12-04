a5448975e692953bc47e739a90fee99d23046c83
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-12-04 10:54:00 -0800

    Improve optimisation for smaller screens

--- src/components/Gallery/GalleryPresentation.js
+++ src/components/Gallery/GalleryPresentation.js
@@ -11,14 +11,14 @@ function GalleryPresentation({ data }) {
   return (
     <section
       data-testid="GalleryPresentation"
-      className="py-6 px-10 bg-white space-y-4"
+      className="py-3 lg:py-6 px-4 lg:px-10 bg-white space-y-1 lg:space-y-4"
     >
       <SiteDocHead titleArray={[data?.title, 'Galleries']} />
       <SectionTitle.Presentation
         title={data?.title}
         accentColor="charcoal-900"
       />
-      <div className="text-center text-charcoal-900 text-xl">
+      <div className="text-center text-charcoal-900 text-base lg:text-xl">
         {data?.titleTranslation}
       </div>
       {(data?.introTranslation || data?.intro) && (
@@ -27,7 +27,7 @@ function GalleryPresentation({ data }) {
           <div>{data?.introTranslation}</div>
         </div>
       )}
-      <div className="p-4 columns-4 gap-6">
+      <div className="p-2 md:p-4 columns-1 md:columns-3 xl:columns-4 gap-6">
         {data?.galleryItems?.length > 0 &&
           data?.galleryItems?.map((image) => (
             <LazyLoader key={image?.id}>
