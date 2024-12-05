67bb15b7bdb3f859f675942fc6e40f5006d9a842
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-12-04 16:35:31 -0800

    Remove error navigate from custom Page

--- src/components/Page/PageContainer.js
+++ src/components/Page/PageContainer.js
@@ -4,35 +4,35 @@ import PropTypes from 'prop-types'
 import PageData from 'components/Page/PageData'
 import WidgetArea from 'components/WidgetArea'
 import PageBanner from 'components/PageBanner'
-import ErrorHandler from 'components/ErrorHandler'
+import LoadOrError from 'components/LoadOrError'
 import SiteDocHead from 'components/SiteDocHead'
 
 function PageContainer({ pageSlug }) {
-  const { banner, title, subtitle, widgets, notFound } = PageData({ pageSlug })
+  const { banner, title, subtitle, widgets, pageQueryReturn } = PageData({
+    pageSlug,
+  })
   const { background, backgroundType, showLogo } = banner
-  return notFound ? (
-    <ErrorHandler.Container
-      error={{ status: 404, statusText: 'Page not found' }}
-    />
-  ) : (
-    <main id="CustomPage">
-      {title && <SiteDocHead titleArray={[title]} description={subtitle} />}
-      <PageBanner.Presentation
-        variant={subtitle ? 'CENTER' : 'LEFT'}
-        background={background}
-        backgroundType={backgroundType}
-        textNode={
-          <div>
-            <h1 className="font-bold text-5xl p-3">{title}</h1>
-            <p>{subtitle}</p>
-          </div>
-        }
-        showLogo={showLogo}
-      />
-      <div className="pb-16 max-w-7xl mx-auto">
-        <WidgetArea.Container widgetData={widgets} />
-      </div>
-    </main>
+  return (
+    <LoadOrError queryReturn={pageQueryReturn}>
+      <main id="CustomPage">
+        {title && <SiteDocHead titleArray={[title]} description={subtitle} />}
+        <PageBanner.Presentation
+          variant={subtitle ? 'CENTER' : 'LEFT'}
+          background={background}
+          backgroundType={backgroundType}
+          textNode={
+            <div>
+              <h1 className="font-bold text-3xl lg:text-5xl p-3">{title}</h1>
+              <p>{subtitle}</p>
+            </div>
+          }
+          showLogo={showLogo}
+        />
+        <div className="pb-16 max-w-7xl mx-auto">
+          <WidgetArea.Container widgetData={widgets} />
+        </div>
+      </main>
+    </LoadOrError>
   )
 }
 
