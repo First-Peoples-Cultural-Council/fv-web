d2e63c655be9637e22d77340a90d0c6198e8f7a8
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2024-12-06 12:00:26 -0800

    Revert previous commit and change ErrorHandler only

--- src/common/hooks/useGoBack.js
+++ src/common/hooks/useGoBack.js
@@ -1,12 +1,13 @@
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 // FPCC
 
 const useGoBack = ({ fallbackRoute }) => {
+  const location = useLocation()
   const navigate = useNavigate()
 
   const goBack = () => {
-    const thereIsAPrevPage = navigate?.length > 1
+    const thereIsAPrevPage = location.key !== 'default'
     if (thereIsAPrevPage) {
       return navigate(-1)
     }
