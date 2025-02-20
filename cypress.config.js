66a8748041abc2a8072b836204b15fa766e6b3ad
Sahil Sharma <sahil.sharma071997@gmail.com>
2025-02-20 15:45:10 -0800

    Cypress user_suite dependabot bug fix (#749)
    * Added projectId to cypress.config.js
    
    * Changed DIALECT to CYPRESS_DIALECT in cypress.config.js
    
    * added the dependabot condition back

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   defaultCommandTimeout: 8000,
   env: {
     baseUrl: 'https://localhost:3000/',
-    DIALECT: 'lilwat',
+    CYPRESS_DIALECT: 'lilwat',
     CYPRESS_FV_USERNAME: process.env.CYPRESS_FV_USERNAME,
     CYPRESS_FV_PASSWORD: process.env.CYPRESS_FV_PASSWORD,
     CYPRESS_FV_INITIALS: process.env.CYPRESS_FV_INITIALS,
@@ -13,5 +13,6 @@ module.exports = defineConfig({
   },
   e2e: {
     experimentalOriginDependencies: true,
+    projectId: process.env.CYPRESS_PROJECT_ID,
   },
 })
