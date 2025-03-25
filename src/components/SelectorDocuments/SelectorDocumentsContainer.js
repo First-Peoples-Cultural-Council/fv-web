4e211f14c947b2c9eb4f1c15b98d9d10a210f5b2
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2025-03-25 15:24:58 -0700

    Add SelectorDocuments

---
+++ src/components/SelectorDocuments/SelectorDocumentsContainer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// FPCC
+import SelectorSearchbox from 'components/SelectorSearchbox'
+import SelectorResultsWrapper from 'components/SelectorResultsWrapper'
+import SelectorDocumentsPresentation from 'components/SelectorDocuments/SelectorDocumentsPresentation'
+import useMediaSearchModal from 'common/dataHooks/useMediaSearchModal'
+import { TYPE_DOCUMENT } from 'common/constants'
+
+function SelectorDocumentsContainer({
+  formMedia,
+  selectedMedia,
+  mediaSelectHandler,
+}) {
+  const infiniteQueryResponse = useMediaSearchModal({ type: TYPE_DOCUMENT })
+
+  return (
+    <div
+      data-testid="SelectorDocumentsContainer"
+      className="h-full bg-charcoal-50"
+    >
+      <div className="h-full w-full flex flex-col">
+        <div className="w-3/4 mx-auto">
+          <SelectorSearchbox.Presentation
+            onSearchChange={infiniteQueryResponse?.handleSearchTermChange}
+            onSearchSubmit={infiniteQueryResponse?.handleSearchSubmit}
+            searchPlaceholder="Search all documents"
+            searchValue={infiniteQueryResponse?.displayedSearchTerm}
+          />
+        </div>
+        <div className="grow h-72 overflow-y-scroll">
+          <SelectorResultsWrapper.Presentation
+            infiniteQueryResponse={infiniteQueryResponse}
+            resultsSection={
+              <SelectorDocumentsPresentation
+                infiniteQueryResponse={infiniteQueryResponse}
+                formMedia={formMedia}
+                selectedMedia={selectedMedia}
+                mediaSelectHandler={mediaSelectHandler}
+              />
+            }
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// PROPTYPES
+const { array, func } = PropTypes
+SelectorDocumentsContainer.propTypes = {
+  formMedia: array,
+  selectedMedia: array,
+  mediaSelectHandler: func,
+}
+
+export default SelectorDocumentsContainer
