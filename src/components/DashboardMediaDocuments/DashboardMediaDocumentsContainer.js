e1da7eea19f9ed440982a7c0b163c38d932f4227
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2025-03-28 15:44:57 -0700

    Add Document list view

---
+++ src/components/DashboardMediaDocuments/DashboardMediaDocumentsContainer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+// FPCC
+import DashboardMediaDocumentsPresentation from 'components/DashboardMediaDocuments/DashboardMediaDocumentsPresentation'
+import useMediaSearch from 'common/dataHooks/useMediaSearch'
+import { TYPE_DOCUMENT } from 'common/constants'
+import SelectorSearchbox from 'components/SelectorSearchbox'
+import SelectorResultsWrapper from 'components/SelectorResultsWrapper'
+
+function DashboardMediaDocumentsContainer() {
+  const infiniteQueryResponse = useMediaSearch({ type: TYPE_DOCUMENT })
+
+  return (
+    <div
+      data-testid="DashboardMediaDocumentsContainer"
+      className="h-full min-h-screen bg-charcoal-50"
+    >
+      <div className="h-full w-full flex flex-col">
+        <div className="w-full sticky top-0 z-30 bg-white">
+          <SelectorSearchbox.Presentation
+            onSearchChange={infiniteQueryResponse?.handleSearchTermChange}
+            onSearchSubmit={
+              infiniteQueryResponse?.handleSearchSubmitWithUrlSync
+            }
+            searchPlaceholder="Search all documents"
+            searchValue={infiniteQueryResponse?.displayedSearchTerm}
+          />
+        </div>
+        <div>
+          <SelectorResultsWrapper.Presentation
+            infiniteQueryResponse={infiniteQueryResponse}
+            resultsSection={
+              <DashboardMediaDocumentsPresentation
+                infiniteQueryResponse={infiniteQueryResponse}
+              />
+            }
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardMediaDocumentsContainer
---
+++ src/components/DashboardMediaDocuments/DashboardMediaDocumentsPresentation.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// FPCC
+import { DOCUMENT_PATH } from 'common/constants'
+import DashboardMediaDetails from 'components/DashboardMediaDetails'
+import getIcon from 'common/utils/getIcon'
+import InfiniteLoadBtn from 'components/InfiniteLoadBtn'
+
+function DashboardMediaDocumentsPresentation({ infiniteQueryResponse }) {
+  const headerClass =
+    'px-6 py-3 text-xs font-medium text-charcoal-900 uppercase tracking-wider'
+  const columnClass =
+    'px-6 py-4 text-sm text-charcoal-900 whitespace-nowrap truncate'
+
+  return (
+    <div
+      id="DashboardMediaDocumentsPresentation"
+      className="grid grid-cols-3 w-full"
+    >
+      <main className="col-span-2 pt-2 mx-2">
+        <section className="p-2 h-full" aria-labelledby="results-header">
+          <h1
+            id="results-header"
+            className="capitalize flex text-2xl font-bold text-charcoal-900 mb-2"
+          >
+            Documents
+          </h1>
+          <div>
+            <div>
+              <table className="w-full table-fixed divide-y divide-charcoal-100">
+                <thead className="bg-charcoal-50">
+                  <tr>
+                    <th scope="col" className={headerClass}>
+                      Title
+                    </th>
+                    <th scope="col" className={headerClass}>
+                      Type
+                    </th>
+                    <th scope="col" className={headerClass}>
+                      Description
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-charcoal-100">
+                  {infiniteQueryResponse?.data?.pages?.[0]?.results?.length &&
+                    infiniteQueryResponse?.data?.pages?.map((page) => (
+                      <React.Fragment key={page?.pageNumber}>
+                        {page.results.map((doc) => (
+                          <tr
+                            key={doc?.id}
+                            className={`${
+                              doc?.id === infiniteQueryResponse?.currentFile?.id
+                                ? 'ring-2 ring-scarlet-800'
+                                : ''
+                            } m-2 rounded-lg relative`}
+                            onClick={() =>
+                              infiniteQueryResponse?.setCurrentFile(doc)
+                            }
+                          >
+                            <td className={columnClass}>{doc.title}</td>
+                            <td className={columnClass}>{doc.mimeType}</td>
+                            <td className={columnClass}>{doc?.description}</td>
+                          </tr>
+                        ))}
+                      </React.Fragment>
+                    ))}
+                </tbody>
+              </table>
+              <InfiniteLoadBtn infiniteQueryResponse={infiniteQueryResponse} />
+            </div>
+          </div>
+        </section>
+      </main>
+      <aside className="col-span-1">
+        <DashboardMediaDetails
+          mediaTypePath={DOCUMENT_PATH}
+          file={infiniteQueryResponse?.currentFile}
+          thumbnail={
+            <div className="block w-full">
+              {getIcon(
+                'Reports',
+                'h-32 mx-auto fill-current text-charcoal-300',
+              )}
+            </div>
+          }
+        />
+      </aside>
+    </div>
+  )
+}
+
+// PROPTYPES
+const { object } = PropTypes
+DashboardMediaDocumentsPresentation.propTypes = {
+  infiniteQueryResponse: object,
+}
+
+export default DashboardMediaDocumentsPresentation
