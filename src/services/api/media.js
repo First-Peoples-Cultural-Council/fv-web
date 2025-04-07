d5ae29837aa0ab1ed196981ddc0f2f362c983b45
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2025-04-07 12:24:19 -0700

    Opt for generic media data hooks where possible

--- src/services/api/media.js
+++ src/services/api/media.js
@@ -1,29 +1,15 @@
 import GlobalConfiguration from 'src/GlobalConfiguration'
 import { apiBase } from 'services/config'
-import { SITES, AUDIO_PATH, IMAGE_PATH, VIDEO_PATH } from 'common/constants'
+import { SITES, AUDIO_PATH } from 'common/constants'
+import { getPathForMediaType } from 'common/utils/mediaHelpers'
 
 const media = {
-  get: async ({ sitename, mediaTypePath, pageParam, perPage = 24 }) => {
-    // Get list of media files of specified type for given site
-    const response = await apiBase()
-      .get(
-        `${SITES}/${sitename}/${mediaTypePath}/?page=${pageParam}&pageSize=${perPage}`,
-      )
-      .json()
-    const lastPage = response?.previous
-    const nextPage = response?.next
-    return { ...response, nextPage, lastPage }
+  get: async ({ sitename, mediaType, id }) => {
+    const mediaTypePath = getPathForMediaType(mediaType)
+    return apiBase().get(`${SITES}/${sitename}/${mediaTypePath}/${id}`).json()
   },
-  getAudio: async ({ sitename, id, headers }) =>
-    apiBase()
-      .extend(headers)
-      .get(`${SITES}/${sitename}/${AUDIO_PATH}/${id}`)
-      .json(),
-  getImage: async ({ sitename, id }) =>
-    apiBase().get(`${SITES}/${sitename}/${IMAGE_PATH}/${id}`).json(),
-  getVideo: async ({ sitename, id }) =>
-    apiBase().get(`${SITES}/${sitename}/${VIDEO_PATH}/${id}`).json(),
-  getUploadEndpoint: ({ sitename, mediaTypePath }) => {
+  getUploadEndpoint: ({ sitename, mediaType }) => {
+    const mediaTypePath = getPathForMediaType(mediaType)
     const url = new URL(
       `${SITES}/${sitename}/${mediaTypePath}`,
       GlobalConfiguration.API_URL,
@@ -32,24 +18,18 @@ const media = {
   },
   uploadAudio: async ({ sitename, data }) =>
     apiBase().post(`${SITES}/${sitename}/${AUDIO_PATH}`, { body: data }).json(),
-  deleteAudio: async ({ sitename, id }) =>
-    apiBase().delete(`${SITES}/${sitename}/${AUDIO_PATH}/${id}`).json(),
-  deleteImage: async ({ sitename, id }) =>
-    apiBase().delete(`${SITES}/${sitename}/${IMAGE_PATH}/${id}`).json(),
-  deleteVideo: async ({ sitename, id }) =>
-    apiBase().delete(`${SITES}/${sitename}/${VIDEO_PATH}/${id}`).json(),
-  updateAudio: async ({ id, sitename, data }) =>
-    apiBase()
-      .put(`${SITES}/${sitename}/${AUDIO_PATH}/${id}`, { json: data })
-      .json(),
-  updateImage: async ({ id, sitename, data }) =>
-    apiBase()
-      .put(`${SITES}/${sitename}/${IMAGE_PATH}/${id}`, { json: data })
-      .json(),
-  updateVideo: async ({ id, sitename, data }) =>
-    apiBase()
-      .put(`${SITES}/${sitename}/${VIDEO_PATH}/${id}`, { json: data })
-      .json(),
+  delete: async ({ sitename, id, mediaType }) => {
+    const mediaTypePath = getPathForMediaType(mediaType)
+    return apiBase()
+      .delete(`${SITES}/${sitename}/${mediaTypePath}/${id}`)
+      .json()
+  },
+  update: async ({ id, data, mediaType, sitename }) => {
+    const mediaTypePath = getPathForMediaType(mediaType)
+    return apiBase()
+      .put(`${SITES}/${sitename}/${mediaTypePath}/${id}`, { json: data })
+      .json()
+  },
 }
 
 export default media
