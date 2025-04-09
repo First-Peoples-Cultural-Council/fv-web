094c805d15857db52fb31b54f93e9d832bca44ee
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2025-04-09 14:09:14 -0700

    Add VideoCrud

---
+++ src/components/VideoCrud/VideoCrudData.js
@@ -0,0 +1,27 @@
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
+
+// FPCC
+import { useVideo, useVideoUpdate } from 'common/dataHooks/useVideos'
+import { MEDIA, VIDEO_PATH } from 'common/constants'
+
+function VideoCrudData() {
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
+  const { sitename } = useParams()
+  const id = searchParams.get('id')
+
+  const queryResponse = useVideo({ id, edit: true })
+
+  const { onSubmit } = useVideoUpdate({ id })
+  const submitHandler = (formData) => onSubmit(formData)
+  const backHandler = () =>
+    navigate(`/${sitename}/dashboard/${MEDIA}/${VIDEO_PATH}`)
+
+  return {
+    queryResponse,
+    submitHandler,
+    backHandler,
+  }
+}
+
+export default VideoCrudData
---
+++ src/components/VideoCrud/index.js
@@ -0,0 +1,9 @@
+import VideoCrudContainer from 'components/VideoCrud/VideoCrudContainer'
+import VideoCrudPresentation from 'components/VideoCrud/VideoCrudPresentation'
+import VideoCrudData from 'components/VideoCrud/VideoCrudData'
+
+export default {
+  Container: VideoCrudContainer,
+  Presentation: VideoCrudPresentation,
+  Data: VideoCrudData,
+}
