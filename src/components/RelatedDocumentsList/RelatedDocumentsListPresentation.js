5c8c60ccb97cf30929f28700faf5e824055fe261
Bronson Chapman <bronsonchapman01@gmail.com>
2025-04-22 12:56:14 -0600

    further visual styling to match design

--- src/components/RelatedDocumentsList/RelatedDocumentsListPresentation.js
+++ src/components/RelatedDocumentsList/RelatedDocumentsListPresentation.js
@@ -19,36 +19,44 @@ function RelatedDocumentsListPresentation({
             </th>
           </tr>
         </thead>
-        <tbody className="border border-charcoal-600 rounded">
-          {documents?.map((document) => (
-            <tr key={document?.id} className="border-b border-charcoal-600">
-              <td className="p-2">
-                <span>
-                  {getIcon(
-                    'DocumentRed',
-                    'w-6 h-6 fill-current mr-3 inline-flex',
-                  )}
-                </span>
-              </td>
-              <td className="border-l-2 border-charcoal-600 px-4 py-2">
-                <span className="text-scarlet-900 text-xl">
-                  {document?.title}
-                </span>
-                <p className="text-charcoal-700 text-sm">
-                  Acknowledgements: {document?.acknowledgement}
-                </p>
-              </td>
-              <td className="p-2">
-                <Link to={document?.original.path}>
-                  {getIcon(
-                    'DownloadRed',
-                    'w-6 h-6 fill-current mr-3 inline-flex',
-                  )}
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <div className="border border-charcoal-600 rounded-lg">
+          <tbody>
+            {documents?.map((document) => (
+              <tr
+                key={document?.id}
+                className="border-b border-charcoal-600 last:border-b-0"
+              >
+                <td className="p-2">
+                  <span>
+                    {getIcon(
+                      'DocumentRed',
+                      'w-6 h-6 fill-current mr-1 inline-flex',
+                    )}
+                  </span>
+                </td>
+                <td className="px-4 py-2 relative">
+                  <div className="absolute left-0 top-1/2 -translate-y-1/2 w-0.5 h-10 bg-charcoal-300"></div>
+                  <div className="pl-1">
+                    <span className="text-scarlet-900 text-xl">
+                      {document?.title}
+                    </span>
+                    <p className="text-charcoal-700 text-sm">
+                      Acknowledgements: {document?.acknowledgement}
+                    </p>
+                  </div>
+                </td>
+                <td className="p-2">
+                  <Link to={document?.original.path}>
+                    {getIcon(
+                      'DownloadRed',
+                      'w-6 h-6 fill-current mr-3 inline-flex',
+                    )}
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </div>
       </table>
     )
   )
