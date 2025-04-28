5b022dadb355d14b35aa94a6e26ce0baa67ca575
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2025-04-28 11:05:36 -0700

    FW-6324, FW-6353 upgrade eslint v9 and Prettier v3 (#803)
    * Update eslint and prettier configs
    
    * Upgrade eslint and related dependencies

--- eslint-custom-rules/index.js
+++ eslint-custom-rules/index.js
@@ -1,2 +1,3 @@
+// https://eslint.org/docs/latest/extend/custom-rule-tutorial
 const dataTestIdRule = require('./require-data-testid')
 module.exports = { rules: { 'require-data-testid': dataTestIdRule } }
---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ */
+
+const config = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 80,
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  htmlWhitespaceSensitivity: 'css',
+  jsxSingleQuote: false,
+  endOfLine: 'lf',
+}
+
+export default config
