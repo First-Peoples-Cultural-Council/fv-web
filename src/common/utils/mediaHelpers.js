af33531c5e1f5fc59708c85d135f461e00e80628
Cara Barter <carabarter@gmail.com>
2025-05-02 14:37:40 -0700

    FW-5105-audiobar-show-file-size-in-KB-instead-MB (#816)
    * adjusted getReadableFileSize to be conditional for small files to display in kb
    
    * rejig to simplify
    
    * rejig to simplify

--- src/common/utils/mediaHelpers.js
+++ src/common/utils/mediaHelpers.js
@@ -44,8 +44,22 @@ export const getMediaPath = ({ mediaObject, type, size = ORIGINAL }) => {
   }
 }
 
-export const getReadableFileSize = (size) =>
-  `${(size / (1024 * 1024)).toFixed(2)} MB`
+//reference https://www.codalas.com/en/2303/how-to-display-file-size-in-a-readable-format-in-javascript
+export const getReadableFileSize = (attachmentSize) => {
+  const DEFAULT_SIZE = 0
+
+  if (!attachmentSize) {
+    return `${DEFAULT_SIZE} KB`
+  }
+
+  const sizeInKb = attachmentSize / 1024
+
+  if (sizeInKb > 1024) {
+    return `${(sizeInKb / 1024).toFixed(2)} MB`
+  } else {
+    return `${sizeInKb.toFixed(2)} KB`
+  }
+}
 
 export const isDisplayablePropMedia = (property, value) =>
   (typeof value === 'string' || value instanceof String) &&
