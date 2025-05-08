3863324a8e74899d1947e51fddecb5eecd97163e
Sahil Sharma <sahil.sharma071997@gmail.com>
2025-05-08 15:23:40 -0700

    Added dotenv-webpack, and setup the webpack config to pick values from the .env file instead of the system vars.

--- README.md
+++ README.md
@@ -72,13 +72,15 @@ Important Note: when running 'npm ci' for the first time you will be silently pr
 $ npm run start
 ```
 
-1. Configure required environment variables.
+1. Configure required environment variables. Create a `.env` file by duplicating the `.env.example` file. Fill in the values for the following variables.
 
-- AWS_CLIENT_ID - OAuth2 client id
-- OIDC_AUTHORITY_URL - Authority URL for retrieving OIDC metadata
-- OAUTH2_REDIRECT_URL - Destination to redirect after signing in
-- END_SESSION_URL - OAuth2 end session (or logout) endpoint, if not included in the OIDC metadata
-- APP_ENV - denotes environment for app, possible values are dev, preprod, and prod
+- API_URL - Use the default value provided in the .env.example for a locally running server, or point it to to a server as required.
+- APP_ENV - Denotes environment for app, possible values are dev, preprod, and prod. Defaults to 'dev'.
+- BUILD_STRING - Modification is not necessary for local/dev setups and this gets set automatically on different environments. Defaults to 'local'.
+- AWS_CLIENT_ID - OAuth2 client id.
+- OIDC_AUTHORITY_URL - Authority URL for retrieving OIDC metadata.
+- OAUTH2_REDIRECT_URL - Destination to redirect after signing in.
+- END_SESSION_URL - OAuth2 end session (or logout) endpoint, if not included in the OIDC metadata.
 
 8. Visit [localhost:3000](http://localhost:3000) in your web browser to view the FirstVoices app.
 
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,50 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
 const path = require('path')
 const alias = require('./webpack.alias')
-
-const getEnvVariables = (env) => ({
-  ENV_API_URL: env?.API_URL
-    ? JSON.stringify(env.API_URL)
-    : JSON.stringify('/api/1.0/'),
-  ENV_APP_ENV: env?.APP_ENV ? JSON.stringify(env.APP_ENV) : JSON.stringify(''),
-  ENV_UNDER_MAINTENANCE: env?.UNDER_MAINTENANCE
-    ? JSON.stringify(env.UNDER_MAINTENANCE)
-    : JSON.stringify(''),
-  ENV_AWS_CLIENT_ID: process.env.AWS_CLIENT_ID
-    ? JSON.stringify(process.env.AWS_CLIENT_ID)
-    : JSON.stringify(''),
-  ENV_BUILD_STRING: env?.BUILD_STRING
-    ? JSON.stringify(env.BUILD_STRING)
-    : JSON.stringify(''),
-  ENV_OIDC_AUTHORITY_URL: process.env.OIDC_AUTHORITY_URL
-    ? JSON.stringify(process.env.OIDC_AUTHORITY_URL)
-    : JSON.stringify(''),
-  ENV_OAUTH2_REDIRECT_URL: process.env.OAUTH2_REDIRECT_URL
-    ? JSON.stringify(process.env.OAUTH2_REDIRECT_URL)
-    : JSON.stringify(''),
-  ENV_END_SESSION_URL: process.env.END_SESSION_URL
-    ? JSON.stringify(process.env.END_SESSION_URL)
-    : JSON.stringify(''),
-  ENV_SENTRY_DSN: process.env.SENTRY_DSN
-    ? JSON.stringify(process.env.SENTRY_DSN)
-    : JSON.stringify(''),
-  ENV_SENTRY_ENVIRONMENT: process.env.SENTRY_ENVIRONMENT
-    ? JSON.stringify(process.env.SENTRY_ENVIRONMENT)
-    : JSON.stringify(''),
-  ENV_SENTRY_RELEASE: process.env.SENTRY_RELEASE
-    ? JSON.stringify(process.env.SENTRY_RELEASE)
-    : JSON.stringify(''),
-  ENV_SENTRY_TRACES_SAMPLE_RATE: process.env.SENTRY_TRACES_SAMPLE_RATE
-    ? JSON.stringify(process.env.SENTRY_TRACES_SAMPLE_RATE)
-    : JSON.stringify(1.0),
-  ENV_SENTRY_ERROR_SAMPLE_RATE: process.env.SENTRY_ERROR_SAMPLE_RATE
-    ? JSON.stringify(process.env.SENTRY_ERROR_SAMPLE_RATE)
-    : JSON.stringify(1.0),
-})
+const Dotenv = require('dotenv-webpack')
 
 module.exports = (env) => {
-  const envVariables = getEnvVariables(env)
-
   return {
     cache: {
       compression: 'gzip',
@@ -98,17 +57,11 @@ module.exports = (env) => {
     },
     plugins: [
       new CompressionPlugin(),
-      new webpack.DefinePlugin({
-        BUILD_DATE: JSON.stringify(
-          new Date().toLocaleString('en-CA', { timeZone: 'America/Vancouver' }),
-        ),
-        ...envVariables,
+      new Dotenv({
+        path: path.resolve(__dirname, '../.env'),
       }),
       new HtmlWebpackPlugin({
         template: './public/index.html',
-        templateParameters: {
-          BUILD_DATE: env.BUILD_DATE,
-        },
         minify: {
           collapseWhitespace: true,
         },
