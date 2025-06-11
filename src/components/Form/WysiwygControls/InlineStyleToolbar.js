0a1bac5504aa3d8c50b6ae42469823c3205688ec
Bronson Chapman <bronsonchapman01@gmail.com>
2025-06-11 15:39:05 -0600

    Add hyperlink functionality to tiptap editor

--- src/components/Form/WysiwygControls/InlineStyleToolbar.js
+++ src/components/Form/WysiwygControls/InlineStyleToolbar.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
 
 // FPCC
 import Button from 'components/Form/WysiwygControls/Button'
+import getIcon from 'common/utils/getIcon'
 
 function InlineStyleToolbar({ editor }) {
+  const setLink = useCallback(() => {
+    const url = window.prompt(
+      'Enter a URL. To remove the link, leave it blank and click OK.',
+    )
+    if (url) {
+      editor.chain().focus().setLink({ href: url }).run()
+    } else {
+      editor.chain().focus().unsetLink().run()
+    }
+  }, [editor])
+
   return (
     <span className="flex border-b border-charcoal-100 text-xl text-charcoal-700">
       <Button
@@ -14,6 +26,10 @@ function InlineStyleToolbar({ editor }) {
       <Button
         onClickHandler={() => editor.chain().focus().toggleItalic().run()}
         label={<span className="italic">I</span>}
+      />
+      <Button
+        onClickHandler={setLink}
+        label={getIcon('Link', `w-5 h-5 fill-current`)}
       />
     </span>
   )
