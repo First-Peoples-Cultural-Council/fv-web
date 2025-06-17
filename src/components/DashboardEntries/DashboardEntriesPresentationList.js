b91b1006fe80753adfcbf0ee6bdee760a178b4ff
Cara Barter <carabarter@gmail.com>
2025-06-17 16:34:49 -0700

    added conditional created by and last modified by

--- src/components/DashboardEntries/DashboardEntriesPresentationList.js
+++ src/components/DashboardEntries/DashboardEntriesPresentationList.js
@@ -25,6 +25,8 @@ function DashboardEntriesPresentationList({
   emptyListMessage,
   entryLabel = 'Language Entry',
 }) {
+  console.log({ searchInfiniteQueryResponse })
+
   const [drawerOpen, setDrawerOpen] = useState(false)
   const [selectedItem, setSelectedItem] = useState({})
   const { sitename } = useParams()
@@ -147,11 +149,18 @@ function DashboardEntriesPresentationList({
                               )}
                             </button>
                           </td>
-                          <td className="p-4 text-charcoal-900 text-xs whitespace-nowrap">
-                            {entry?.created}
+                          <td className="p-4 text-charcoal-900 text-xs">
+                            <p>{entry?.created}</p>
+                            <p className="whitespace-nowrap">
+                              {entry?.createdBy && `by ${entry.createdBy}`}
+                            </p>
                           </td>
-                          <td className="p-4 text-charcoal-900 text-xs whitespace-nowrap">
-                            {entry?.lastModified}
+                          <td className="p-4 text-charcoal-900 text-xs">
+                            <p>{entry?.lastModified}</p>
+                            <p className="whitespace-nowrap">
+                              {entry?.lastModifiedBy &&
+                                `by ${entry?.lastModifiedBy}`}
+                            </p>
                           </td>
                           <td className="p-4 whitespace-nowrap">
                             <span
