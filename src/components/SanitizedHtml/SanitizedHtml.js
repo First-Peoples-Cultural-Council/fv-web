a121f018f1fbb87d1aa013204b1b451cd4f2b3bc
Bronson Chapman <bronsonchapman01@gmail.com>
2025-06-18 17:06:21 -0600

    Open local links in the same tab

--- src/components/SanitizedHtml/SanitizedHtml.js
+++ src/components/SanitizedHtml/SanitizedHtml.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import DOMPurify from 'dompurify'
 
+const LOCAL_DOMAINS = ['firstvoices.com']
+
 DOMPurify.addHook('uponSanitizeElement', (node, data) => {
   if (data.tagName === 'iframe') {
     const src = node.getAttribute('src') || ''
@@ -11,8 +13,16 @@ DOMPurify.addHook('uponSanitizeElement', (node, data) => {
   }
 
   if (data.tagName === 'a') {
-    node.setAttribute('target', '_blank')
-    node.setAttribute('rel', 'noopener noreferrer')
+    const href = node.getAttribute('href') || ''
+
+    const isLocal = LOCAL_DOMAINS.some((domain) => href.includes(domain))
+    if (!isLocal) {
+      node.setAttribute('target', '_blank')
+      node.setAttribute('rel', 'noopener noreferrer')
+    } else {
+      node.setAttribute('target', '_self')
+      node.removeAttribute('rel')
+    }
   }
 })
 
