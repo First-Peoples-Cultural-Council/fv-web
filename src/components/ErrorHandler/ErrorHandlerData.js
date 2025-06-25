d85f7d04ae37ce1fbb54bd36644d5cff8162f827
Bronson Chapman <64934597+bronson-c@users.noreply.github.com>
2025-06-25 09:36:47 -0600

    return to homepage on error page back button (#883)

--- src/components/ErrorHandler/ErrorHandlerData.js
+++ src/components/ErrorHandler/ErrorHandlerData.js
@@ -5,7 +5,18 @@ import { useSearchParams, useNavigate } from 'react-router'
 function ErrorHandlerData() {
   const [searchParams] = useSearchParams()
   const navigate = useNavigate()
-  const backHandler = () => navigate(-1)
+
+  const backHandler = () => {
+    // If the user's history is the same site, go back in history
+    // If the user's history is not the same site, navigate to the home page
+    const isInternalHistory =
+      window.history.state && window.history.state.idx > 0
+    if (isInternalHistory) {
+      navigate(-1)
+    } else {
+      navigate('/')
+    }
+  }
 
   const status = searchParams.get('status') || '0'
   const statusText = searchParams.get('statusText') || null
