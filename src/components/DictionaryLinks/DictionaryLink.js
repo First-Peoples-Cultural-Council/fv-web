83a1caa46bb19f4ed2a4c2479c3082998fcbb1e4
Cara Barter <carabarter@gmail.com>
2025-07-08 15:50:55 -0700

    redoing button classes to be correct

--- src/components/DictionaryLinks/DictionaryLink.js
+++ src/components/DictionaryLinks/DictionaryLink.js
@@ -11,13 +11,10 @@ function DictionaryLink({ linkProperties }) {
       id={linkProperties?.id}
       className="transition duration-500 ease-in-out ml-5 xl:ml-8"
     >
-      <Link
-        className="flex items-center transition duration-500 ease-in-out p-2 grow rounded-lg capitalize cursor-pointer text-lg xl:text-xl text-charcoal-900"
-        to={linkProperties?.linkTo}
-      >
+      <Link className="btn-tertiary btn-lg" to={linkProperties?.linkTo}>
         {getIcon(
           linkProperties?.iconId,
-          'btn-tertiary btn-md-icon inline-flex fill-current mr-2 xl:mr-5',
+          'inline-flex fill-current mr-2 xl:mr-5',
         )}
         <span>{linkProperties?.label}</span>
       </Link>
