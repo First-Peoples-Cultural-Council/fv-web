e67f7d6687766c8b6b7b00029248992c8f39ab30
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2025-07-10 10:05:30 -0700

    Reuse BannerBackground for custom pages

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,7 +1,9 @@
 import PageContainer from 'components/Page/PageContainer'
 import PageData from 'components/Page/PageData'
+import PagePresentation from 'components/Page/PagePresentation'
 
 export default {
   Container: PageContainer,
   Data: PageData,
+  Presentation: PagePresentation,
 }
