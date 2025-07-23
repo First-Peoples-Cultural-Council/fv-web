9da5ba3c04a01e73d1d5e50f87e802622e194740
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2025-07-23 14:59:49 -0700

    Remove unused wysiwyg helper

--- src/common/utils/wysiwygStateHelpers.js
+++ src/common/utils/wysiwygStateHelpers.js
@@ -1,35 +1,12 @@
-import {
-  ContentState,
-  convertFromHTML,
-  convertFromRaw,
-  convertToRaw,
-} from 'draft-js'
+import { ContentState, convertFromRaw, convertToRaw } from 'draft-js'
 
 // WysiwygState is used to refer to Draft-js ContentState
 // See https://draftjs.org/docs/api-reference-content-state/ for more details and the difference between EditorState and ContentState
 
 // FPCC
-import {
-  isStringWithLength,
-  extractTextFromHtml,
-  safeJsonParse,
-} from 'common/utils/stringHelpers'
+import { isStringWithLength, safeJsonParse } from 'common/utils/stringHelpers'
 
 function wysiwygStateHelpers() {
-  // Converts an HTML string into Draft-js ContentState
-  const getWysiwygStateFromHtml = (htmlString) => {
-    const textFromHtml = extractTextFromHtml(htmlString)
-    if (!isStringWithLength(textFromHtml)) {
-      return ContentState.createFromText('')
-    }
-
-    const blocksFromHTML = convertFromHTML(htmlString)
-    return ContentState.createFromBlockArray(
-      blocksFromHTML.contentBlocks,
-      blocksFromHTML.entityMap,
-    )
-  }
-
   // Converts Draft-js ContentState into raw JSON format
   const getJsonFromWysiwygState = (wysiwygState) => {
     if (!wysiwygState) return ''
@@ -56,7 +33,6 @@ function wysiwygStateHelpers() {
 
   return {
     getJsonFromWysiwygState,
-    getWysiwygStateFromHtml,
     getWysiwygStateFromJson,
   }
 }
