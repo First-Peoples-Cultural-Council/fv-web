20fa4e480dcaaee963692f0f67effbeb27e85b92
Guy McAuliffe <guy@fpcc.ca>
2025-09-12 11:10:08 -0700

    Remove title tags from svgs to prevent unintentional tooltips

--- src/common/icons/BackArrow.js
+++ src/common/icons/BackArrow.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-/**
- * @summary BackArrow
- * @component
- *
- * @param {object} props
- *
- * @returns {node} jsx markup
- */
+
 function BackArrow({ styling }) {
   return (
     <svg
@@ -15,7 +8,6 @@ function BackArrow({ styling }) {
       viewBox="0 0 100 100"
       className={styling}
     >
-      <title>BackArrow</title>
       <g>
         <g>
           <path d="M5273.1,2400.1v-2c0-2.8-5-4-9.7-4s-9.7,1.3-9.7,4v2c0,1.8,0.7,3.6,2,4.9l5,4.9c0.3,0.3,0.4,0.6,0.4,1v6.4 c0,0.4,0.2,0.7,0.6,0.8l2.9,0.9c0.5,0.1,1-0.2,1-0.8v-7.2c0-0.4,0.2-0.7,0.4-1l5.1-5C5272.4,2403.7,5273.1,2401.9,5273.1,2400.1z      M5263.4,2400c-4.8,0-7.4-1.3-7.5-1.8v0c0.1-0.5,2.7-1.8,7.5-1.8c4.8,0,7.3,1.3,7.5,1.8C5270.7,2398.7,5268.2,2400,5263.4,2400z" />
--- src/common/icons/Mute.js
+++ src/common/icons/Mute.js
@@ -8,7 +8,6 @@ function Mute({ styling }) {
       viewBox="0 0 24 24"
       className={styling}
     >
-      <title>Mute</title>
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M7 9v6h4l5 5V4l-5 5H7z" />
     </svg>
