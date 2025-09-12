9cc1eb7c0f4302003c11df59812060815fd0a8af
Guy McAuliffe <guy@fpcc.ca>
2025-09-12 12:53:57 -0700

    Use Tooltip for AudioButton

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -5,7 +5,7 @@ function Tooltip({ hide, message, children }) {
   return (
     <div className="inline-flex items-center justify-center">
       <div className="group relative flex max-w-max flex-col items-center justify-center">
-        <div className="absolute z-40 left-1/2 bottom-9 ml-auto mr-auto min-w-max -translate-x-1/2 scale-0 transform rounded-lg px-3 py-2 text-xs font-medium transition-all duration-200 group-hover:scale-100">
+        <div className="absolute z-40 left-1/2 bottom-9 ml-auto mr-auto min-w-max -translate-x-1/2 scale-0 transform rounded-lg px-3 py-2 text-xs font-medium transition-all duration-300 group-hover:scale-100">
           {!hide && (
             <div className="flex max-w-32 flex-col items-center">
               <div className="max-w-32 text-wrap rounded bg-blumine-800 p-2 text-center text-xs text-white">
