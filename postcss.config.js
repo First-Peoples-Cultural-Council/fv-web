3f04107ccd4ae4827d621f39fc6d472505bc415f
Guy McAuliffe <guy@fpcc.ca>
2025-09-15 16:31:27 -0700

    Remove cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,10 @@
 const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
-const cssnano = require('cssnano')
 
 module.exports = {
   plugins: [
     'postcss-preset-env',
     tailwindcss('./tailwind.config.js'),
     autoprefixer,
-    process.env.NODE_ENV === 'production'
-      ? cssnano({ preset: 'default' })
-      : null,
   ],
 }
