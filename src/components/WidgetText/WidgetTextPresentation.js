2c43262068c0b50240ac6dc77a65eec04e62c02c
Lance Patterson <lancepatterson@MacBookPro.lan>
2025-09-17 10:10:52 -0700

    about open new tab fix

--- src/components/WidgetText/WidgetTextPresentation.js
+++ src/components/WidgetText/WidgetTextPresentation.js
@@ -95,7 +95,8 @@ function WidgetTextPresentation({ widgetData }) {
             <div className="rounded-full shadow">
               <a
                 href={url}
-                {...(url.includes(FIRSTVOICESLINK) ||
+                {...(url.startsWith('/') ||
+                url.includes(FIRSTVOICESLINK) ||
                 url.startsWith(`/${sitename}`)
                   ? { target: '_self' }
                   : { target: '_blank', rel: 'noopener noreferrer' })}
