d2911951ca13338634facab99cd1404ef6a66235
Guy McAuliffe <guy@fpcc.ca>
2025-09-17 09:48:44 -0700

    simplify postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
-const tailwindcss = require('tailwindcss')
-const autoprefixer = require('autoprefixer')
-
 module.exports = {
   plugins: [
     'postcss-preset-env',
-    tailwindcss('./tailwind.config.js'),
-    autoprefixer,
+    require('tailwindcss'),
+    require('autoprefixer'),
   ],
 }
