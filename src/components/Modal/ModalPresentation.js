ba07eac491da4d5a1912d255a0ffda64c4297621
Guy McAuliffe <guy@fpcc.ca>
2025-09-18 13:24:58 -0700

    bg-opacity deprecated

--- src/components/Modal/ModalPresentation.js
+++ src/components/Modal/ModalPresentation.js
@@ -15,7 +15,7 @@ function ModalPresentation({
     <Dialog open={isOpen} onClose={closeHandler} className="relative z-10">
       <DialogBackdrop
         transition
-        className="fixed inset-0 bg-charcoal-700 bg-opacity-75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-leave:duration-200 data-enter:ease-out data-leave:ease-in"
+        className="fixed inset-0 bg-charcoal-700/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-leave:duration-200 data-enter:ease-out data-leave:ease-in"
       />
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
