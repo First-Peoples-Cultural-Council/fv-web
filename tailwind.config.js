7cfd367c0fab5f33a8d912051b29d94d57890efb
Guy McAuliffe <guy@fpcc.ca>
2025-09-23 09:57:55 -0700

    Move core and wordsy colours to main.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,24 +11,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // Core Colors
-        blumine: customColorsConfig.blumine, // 400, 500, 600, 700, 800, 900
-        scarlet: customColorsConfig.scarlet, // 400, 500, 600, 700, 800, 900
-        jade: customColorsConfig.jade, // 300, 400, 500, 700, 800
-        ochre: customColorsConfig.ochre, // 200, 400, 500, 600, 700, 800
-        charcoal: customColorsConfig.charcoal, // 100, 200, 300, 500, 700, 900
         // Type Color Aliases
         'phrase-color': customColorsConfig.ochre,
         'song-color': customColorsConfig.scarlet,
         'story-color': customColorsConfig.blumine,
         'word-color': customColorsConfig.jade,
-        // Misc aliases
-        'wordsy-correct': {
-          DEFAULT: customColorsConfig.jade[500],
-        },
-        'wordsy-present': {
-          DEFAULT: customColorsConfig.ochre[600],
-        },
       },
     },
   },
