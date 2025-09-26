ce37805a97bd105a380fd2a826c1248d0e1ccb37
Guy McAuliffe <guy@fpcc.ca>
2025-09-26 15:36:07 -0700

    FW-5181 Header buttons cursor fix

--- src/components/NavBar/NavBarPresentationMenu.js
+++ src/components/NavBar/NavBarPresentationMenu.js
@@ -2,13 +2,7 @@ import React, { Fragment } from 'react'
 import { Link } from 'react-router'
 import PropTypes from 'prop-types'
 import { useTranslation } from 'react-i18next'
-import {
-  Menu,
-  MenuItem,
-  MenuItems,
-  MenuButton,
-  Transition,
-} from '@headlessui/react'
+import { Menu, MenuItem, MenuItems, MenuButton } from '@headlessui/react'
 
 // FPCC
 import getIcon from 'common/utils/getIcon'
@@ -20,18 +14,14 @@ function NavBarPresentationMenu({ menuItemData, sitename }) {
   const generateMenuItems = (items) =>
     items.map((menuItem) => (
       <MenuItem key={`HeaderMenu_${menuItem.title}`}>
-        {({ focus }) => (
-          <Link
-            to={`/${sitename + menuItem.href}`}
-            className={`${
-              focus ? 'bg-charcoal-100 text-charcoal-900' : 'text-charcoal-900'
-            } flex w-full rounded-lg`}
-          >
-            <div className="px-2 py-1 w-full text-lg font-medium">
-              {menuItem.transKey ? t(menuItem.transKey) : menuItem.title}
-            </div>
-          </Link>
-        )}
+        <Link
+          to={`/${sitename + menuItem.href}`}
+          className="data-focus:bg-charcoal-100 text-charcoal-900 flex w-full rounded-lg"
+        >
+          <div className="px-2 py-1 w-full text-lg font-medium">
+            {menuItem.transKey ? t(menuItem.transKey) : menuItem.title}
+          </div>
+        </Link>
       </MenuItem>
     ))
 
@@ -43,7 +33,7 @@ function NavBarPresentationMenu({ menuItemData, sitename }) {
       </p>
       {i18n?.language !== 'en' && (
         // Tooltip
-        <span className="absolute top-16 scale-0 group-hover:scale-100 rounded-sm text-charcoal-900 p-2 text-sm bg-white">
+        <span className="absolute top-12 scale-0 group-hover:scale-100 rounded-sm text-charcoal-900 p-2 text-sm bg-white">
           {transKey ? t(transKey) : title}
         </span>
       )}
@@ -55,43 +45,20 @@ function NavBarPresentationMenu({ menuItemData, sitename }) {
   )
 
   const buttonStyling =
-    'h-8 xl:h-10 group p-1 rounded-lg inline-flex w-full justify-center items-center text-base xl:text-lg font-medium text-white hover:text-charcoal-50'
+    'h-8 xl:h-10 w-40 xl:w-44 group p-1 rounded-lg inline-flex w-full justify-center items-center text-base xl:text-lg font-medium text-white hover:text-charcoal-50'
 
   return hasItems ? (
-    <Menu
-      data-testid="NavBar-Menu"
-      as="div"
-      className="relative inline-block w-40 xl:w-44"
-    >
-      <div>
-        <MenuButton
-          data-testid={`${transKey}-button`}
-          className={buttonStyling}
-        >
-          {generateButtonContents()}
-        </MenuButton>
-      </div>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <MenuItems className="absolute top-10 w-auto min-w-full right-0 p-2 transform lg:-translate-x-0 bg-white rounded-lg shadow-lg ring-1 ring-black/50 focus:outline-hidden">
-          <div className="bg-white space-y-2">
-            {generateMenuItems(itemsData)}
-          </div>
-        </MenuItems>
-      </Transition>
+    <Menu data-testid="NavBar-Menu" as="div" className="relative inline-block">
+      <MenuButton data-testid={`${transKey}-button`} className={buttonStyling}>
+        {generateButtonContents()}
+      </MenuButton>
+
+      <MenuItems className="absolute top-10 w-auto min-w-full right-0 p-2 transition duration-100 ease-out transform lg:-translate-x-0 bg-white rounded-lg shadow-lg ring-1 ring-black/50 focus:outline-hidden">
+        <div className="bg-white space-y-2">{generateMenuItems(itemsData)}</div>
+      </MenuItems>
     </Menu>
   ) : (
-    <div
-      data-testid="NavBar-link"
-      className="relative inline-block w-40 xl:w-44"
-    >
+    <div data-testid="NavBar-link" className="relative inline-block">
       <Link
         data-testid={`${transKey}-button`}
         to={`/${sitename + href}`}
