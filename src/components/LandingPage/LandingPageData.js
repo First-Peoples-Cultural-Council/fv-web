4379e37d44040bbf6f2c626bf4845bbde9d56aca
Lance Patterson <118156195+lancepatterson@users.noreply.github.com>
2025-09-29 08:58:09 -0700

    FW-5628 - QA Request for link opening in prod (#988)
    * About no longer opens new tab - QA Request
    
    * added back requested code & fixed new tab
    
    * about open new tab fix
    
    * Fixed link opening in prod
    
    ---------
    
    Co-authored-by: Lance Patterson <lancepatterson@MacBookPro.lan>

--- src/components/LandingPage/LandingPageData.js
+++ src/components/LandingPage/LandingPageData.js
@@ -6,7 +6,6 @@ import languages from 'assets/images/languages-background.png'
 import {
   WIDGET_TEXT,
   WIDGET_QUOTES,
-  FIRSTVOICESLINK,
   FORMAT_RIGHT,
 } from 'src/common/constants'
 
@@ -52,7 +51,7 @@ function LandingPageData() {
       mockData: true,
       image: appsImage,
       title: 'FIRSTVOICES APPS',
-      url: `https://${FIRSTVOICESLINK}/apps`,
+      url: `/apps`,
       urlLabel: 'Learn more',
       textWithFormatting:
         '<p>The wealth of language data uploaded by Indigenous communities onto their FirstVoices language sites can now also be accessed through mobile and desktop apps. The app pulls content directly from entries on FirstVoices and functions with offline capabilities. The apps are updated throughout the year in order to ensure that the latest content that has been added to a language site also appears on the app.</p>',
