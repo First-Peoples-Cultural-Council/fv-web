408646fda5dcfd958340894e017d9ee40a19126d
Guy McAuliffe <guy@fpcc.ca>
2025-10-07 10:28:43 -0700

    Make top padding on Image and Video consistent with other widgets

--- src/components/WidgetImage/WidgetImagePresentation.js
+++ src/components/WidgetImage/WidgetImagePresentation.js
@@ -8,17 +8,20 @@ function WidgetImagePresentation({ widgetData }) {
   const { caption, image } = widgetData.settings
 
   return (
-    <section className="w-full pt-8" data-testid="WidgetImagePresentation">
-      <div className="rounded overflow-hidden flex flex-col mx-auto max-w-2xl">
-        <div className="flex flex-col mx-auto overflow-hidden px-6 md:px-0 pt-6">
-          {image && <ImgFromId.Container className="w-full py-4" id={image} />}
-          {caption && (
-            <div className="flex flex-wrap">
-              <p>{caption}</p>
-            </div>
-          )}
-        </div>
-      </div>
+    <section className="py-3 md:py-6" data-testid="WidgetImagePresentation">
+      <figure className="flex flex-col mx-auto px-3 md:px-0 space-y-2">
+        {image && (
+          <ImgFromId.Container
+            className="max-h-[80vh] object-contain"
+            id={image}
+          />
+        )}
+        {caption && (
+          <figcaption className="text-center max-w-4xl mx-auto">
+            {caption}
+          </figcaption>
+        )}
+      </figure>
     </section>
   )
 }
