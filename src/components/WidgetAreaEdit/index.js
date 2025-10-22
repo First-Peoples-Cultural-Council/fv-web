82956532a5545f5a469697bc7698a9cb161abf06
Guy McAuliffe <guy@fpcc.ca>
2025-10-22 14:10:53 -0700

    Delete old data layer

--- src/components/WidgetAreaEdit/index.js
+++ src/components/WidgetAreaEdit/index.js
@@ -1,9 +1,7 @@
 import WidgetAreaEditContainer from 'components/WidgetAreaEdit/WidgetAreaEditContainer'
 import WidgetAreaEditPresentation from 'components/WidgetAreaEdit/WidgetAreaEditPresentation'
-import WidgetAreaEditData from 'components/WidgetAreaEdit/WidgetAreaEditData'
 
 export default {
   Container: WidgetAreaEditContainer,
   Presentation: WidgetAreaEditPresentation,
-  Data: WidgetAreaEditData,
 }
