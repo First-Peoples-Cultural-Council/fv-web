9fba0da1475e31b988ad990aa35a76b0f749f203
Guy McAuliffe <guy@fpcc.ca>
2025-10-24 15:05:57 -0700

    Add new widget to page-widgets on load

--- src/common/dataHooks/useWidgetAreaEdit.js
+++ src/common/dataHooks/useWidgetAreaEdit.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useSearchParams } from 'react-router'
 
 // FPCC
 import { usePageWidgetsUpdate } from 'common/dataHooks/usePages'
@@ -8,13 +9,17 @@ import {
   objectsToIdsAdaptor,
   arrayToObjectWithIdKeys,
 } from 'common/dataAdaptors/misc'
+import { HOME, ID_TO_ADD } from 'common/constants'
+import { isUUID } from 'common/utils/stringHelpers'
 
 export function useWidgetAreaEdit({ destination, currentWidgets }) {
+  const [searchParams] = useSearchParams()
+
   const [widgetIds, setWidgetIds] = useState(
     objectsToIdsAdaptor(currentWidgets),
   )
 
-  const isHomePage = destination === 'isHomePage'
+  const isHomePage = destination === HOME
   const pageSlug = !isHomePage ? destination : null
 
   // Fetch all widgets for this site
@@ -44,6 +49,13 @@ export function useWidgetAreaEdit({ destination, currentWidgets }) {
     // must supply array for sites with no widgets
     const filteredIds = widgetIds?.length > 0 ? [id, ...widgetIds] : [id]
     saveWidgetOrder(filteredIds)
+  }
+
+  //   Check for widget to add in url params (from useWidgetCreate redirect)
+  const widgetIdToAdd = searchParams.get(ID_TO_ADD)
+
+  if (isUUID(widgetIdToAdd) && !widgetIds?.includes(widgetIdToAdd)) {
+    handleAddWidget(widgetIdToAdd)
   }
 
   return {
