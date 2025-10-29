127403f5c2de2ceb6a294e1617c7120e007854a0
Guy McAuliffe <guy@fpcc.ca>
2025-10-29 15:42:25 -0700

    Move VideoLink Thumbnail fetching into thumbnail component

--- src/components/MediaThumbnail/VideoLinkThumbnail.js
+++ src/components/MediaThumbnail/VideoLinkThumbnail.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function VideoLinkThumbnail(props) {
-  const {
-    link,
-    containerStyles = 'block relative w-48 h-32 rounded-lg bg-charcoal-50 overflow-hidden',
-  } = props
+// FPCC
+import { useVimeoThumbnail } from 'common/dataHooks/useVideos'
+
+function VideoLinkThumbnail({
+  link,
+  containerStyles = 'block relative w-48 h-32 rounded-lg bg-charcoal-50 overflow-hidden',
+}) {
+  const queryResponse = useVimeoThumbnail({ link })
+
+  const getThumbnailFromLink = () => {
+    if (link?.text?.toLowerCase()?.includes('youtu')) {
+      const id = link?.text?.match(
+        /(?:^(?:https?:\/\/)?|^)(?:www.)?(?:(?:(?:(?:youtube\.com\/watch\?v=)|(?:youtu\.be\/))(.{11}?)))/,
+      )[1]
+      return `https://img.youtube.com/vi/${id}/0.jpg`
+    }
+
+    return queryResponse?.data?.thumbnail_url || ''
+  }
+  const thumbnail = link?.thumbnail
+    ? link?.thumbnail
+    : getThumbnailFromLink(link)
+
   return (
     <div className={containerStyles}>
       <img
-        src={link}
+        src={thumbnail}
         alt="video thumbnail"
         className="group-hover:opacity-75 aspect-3/2 w-full object-cover pointer-events-none"
       />
@@ -17,9 +35,9 @@ function VideoLinkThumbnail(props) {
   )
 }
 
-const { string } = PropTypes
+const { string, object } = PropTypes
 VideoLinkThumbnail.propTypes = {
-  link: string,
+  link: object,
   containerStyles: string,
 }
 
