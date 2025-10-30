87f4578dc45027f572210285c580e34aff0e7156
Guy McAuliffe <guy@fpcc.ca>
2025-10-30 11:30:31 -0700

    Sort alphabetically and separate Dictionary Entry limits

--- src/common/constants/limits.js
+++ src/common/constants/limits.js
@@ -1,16 +1,17 @@
-// MEDIA
-export const MAX_AUDIO = 10
-export const MAX_AUDIO_BASIC = 3
-export const MAX_VIDEOS = 10
-export const MAX_IMAGES = 10
-export const MAX_DOCUMENTS = 10
-// OTHER
+// Dictionary Entry Limits
 export const MAX_ACKNOWLEDGEMENTS = 10
+export const MAX_AUDIO = 10
 export const MAX_CATEGORIES = 10
-export const MAX_GALLERY_ITEMS = 40
-export const MAX_LYRICS = 25
+export const MAX_DOCUMENTS = 10
+export const MAX_IMAGES = 10
 export const MAX_NOTES = 10
 export const MAX_PRONUNCIATIONS = 3
 export const MAX_RELATED_ENTRIES = 10
 export const MAX_SPELLINGS = 3
 export const MAX_TRANSLATIONS = 10
+export const MAX_VIDEOS = 10
+
+// Other
+export const MAX_AUDIO_BASIC = 3 // Characters, Story Covers, and Story Pages
+export const MAX_GALLERY_ITEMS = 40
+export const MAX_LYRICS = 25
