f613c78db7c8d0ed1f11318156810f114b245ea4
Guy McAuliffe <guy@fpcc.ca>
2025-11-24 15:15:41 -0800

    Remove outer div which was preventing Search box from growing

--- src/components/SearchFormMinimal/SearchFormMinimal.js
+++ src/components/SearchFormMinimal/SearchFormMinimal.js
@@ -10,36 +10,34 @@ function SearchFormMinimal({
   displayedSearchTerm,
 }) {
   return (
-    <div id="SearchFormMinimal">
-      <div className="w-full flex rounded-full h-10">
-        <form
-          onSubmit={handleSearchNavigation}
-          className="flex items-stretch grow"
-        >
-          <label id="SearchLabel" htmlFor="SearchInput" className="sr-only">
-            Search FirstVoices
-          </label>
-          <input
-            data-testid="SearchInput"
-            id="SearchInput"
-            aria-labelledby="SearchLabel"
-            className="w-full text-sm placeholder:text-charcoal-500 text-charcoal-700 rounded-none rounded-l-full pl-4 focus:outline-hidden truncate"
-            type="text"
-            placeholder="Search"
-            onInput={handleSearchTermChange}
-            value={displayedSearchTerm}
-          />
-        </form>
-        <button
-          type="button"
-          data-testid="SearchSubmit"
-          aria-label="Search/Go"
-          onClick={handleSearchNavigation}
-          className="relative inline-flex items-center px-2 py-1.5 text-charcoal-500 rounded-r-full bg-charcoal-50 hover:bg-charcoal-50"
-        >
-          {getIcon('Search', 'fill-current h-5 w-5 ')}
-        </button>
-      </div>
+    <div id="SearchFormMinimal" className="w-full flex rounded-full h-10">
+      <form
+        onSubmit={handleSearchNavigation}
+        className="flex items-stretch grow"
+      >
+        <label id="SearchLabel" htmlFor="SearchInput" className="sr-only">
+          Search FirstVoices
+        </label>
+        <input
+          data-testid="SearchInput"
+          id="SearchInput"
+          aria-labelledby="SearchLabel"
+          className="w-full text-sm placeholder:text-charcoal-500 text-charcoal-700 rounded-none rounded-l-full pl-4 focus:outline-hidden truncate"
+          type="text"
+          placeholder="Search"
+          onInput={handleSearchTermChange}
+          value={displayedSearchTerm}
+        />
+      </form>
+      <button
+        type="button"
+        data-testid="SearchSubmit"
+        aria-label="Search/Go"
+        onClick={handleSearchNavigation}
+        className="relative inline-flex items-center px-2 py-1.5 text-charcoal-500 rounded-r-full bg-charcoal-50 hover:bg-charcoal-50 border"
+      >
+        {getIcon('Search', 'fill-current h-5 w-5 ')}
+      </button>
     </div>
   )
 }
