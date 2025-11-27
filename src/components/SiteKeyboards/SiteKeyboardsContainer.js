eca5f48da4ebd773cb136a26acd73c7c9dfce0dc
Guy McAuliffe <guy@fpcc.ca>
2025-11-27 15:20:32 -0800

    Move padding to keyboard widget

--- src/components/SiteKeyboards/SiteKeyboardsContainer.js
+++ src/components/SiteKeyboards/SiteKeyboardsContainer.js
@@ -21,13 +21,11 @@ function SiteKeyboardsContainer() {
       className="max-w-7xl mx-auto pt-2 md:pt-4 lg:pt-8 bg-white"
     >
       <SiteDocHead titleArray={['Keyboards']} />
-      <div className="px-4 sm:px-6 lg:px-8 space-y-4">
+      <div className="px-4 sm:px-6 lg:px-8">
         <SectionTitle.Presentation title="KEYBOARDS" />
-
         {queryResponse?.data?.widgets?.length > 0 && (
           <WidgetArea.Container widgetData={queryResponse?.data?.widgets} />
         )}
-
         <SiteKeyboardsPresentation title={site?.title} />
       </div>
     </div>
