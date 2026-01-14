49d2233b821422a2e5e4adf7287a484ae353e538
Chris Nahanee <chris@fpcc.ca>
2026-01-14 14:36:46 -0800

    adding aditional check for membership

--- cypress/e2e/dashboard/member_management.cy.js
+++ cypress/e2e/dashboard/member_management.cy.js
@@ -68,6 +68,7 @@ describe(
       cy.contains(`${Cypress.env('CYPRESS_FV_INITIALS')}`).click()
       cy.contains('Dashboard').click()
       cy.contains('Member Management').click()
+      cy.get('#PaginationControlsPresentation').should('be.visible')
       cy.intercept(`${Cypress.env('CYPRESS_SERVER')}`).as('getNext')
       cy.get('[data-testid="next-page-btn"]').click()
       cy.get('[data-testid^="page"]').each((_page) => {
