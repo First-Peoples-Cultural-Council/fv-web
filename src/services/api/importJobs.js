ed02375c091dae6ed0844c6e3adb19325b9c37a3
Guy McAuliffe <guy@fpcc.ca>
2026-02-25 16:38:10 -0800

    Add import-jobs api service

---
+++ src/services/api/importJobs.js
@@ -0,0 +1,25 @@
+import { apiBase } from 'services/config'
+import { SITES, IMPORT_JOBS } from 'common/constants'
+
+const importJobs = {
+  get: async ({ sitename, id }) =>
+    apiBase().get(`${SITES}/${sitename}/${IMPORT_JOBS}/${id}`).json(),
+  getAll: async ({ sitename, pageParam, perPage = 48 }) =>
+    apiBase()
+      .get(
+        `${SITES}/${sitename}/${IMPORT_JOBS}/?page=${pageParam}&pageSize=${perPage}`,
+      )
+      .json(),
+  create: async ({ sitename, formData }) =>
+    apiBase()
+      .post(`${SITES}/${sitename}/${IMPORT_JOBS}/`, { body: formData })
+      .json(),
+  validate: async ({ sitename, id }) =>
+    apiBase()
+      .post(`${SITES}/${sitename}/${IMPORT_JOBS}/${id}/validate/`)
+      .json(),
+  delete: async ({ sitename, id }) =>
+    apiBase().delete(`${SITES}/${sitename}/${IMPORT_JOBS}/${id}`).json(),
+}
+
+export default importJobs
