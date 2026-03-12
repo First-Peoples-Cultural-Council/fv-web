19cb125f1c6c17422455a0c9615761790dda6f4c
Guy McAuliffe <guy@fpcc.ca>
2026-03-12 15:41:02 -0700

    Styling tweaks for imports list and report

--- src/components/DashboardImports/ValidationStatusBtn.js
+++ src/components/DashboardImports/ValidationStatusBtn.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 
 // FPCC
 import Modal from 'components/Modal'
-import Form from 'components/Form'
 import { getLastPathSegment } from 'common/utils/urlHelpers'
 import Tooltip from 'components/Tooltip'
 import getIcon from 'common/utils/getIcon'
@@ -11,7 +10,8 @@ import { useImportJobValidate } from 'common/dataHooks/useImportJobs'
 
 function ValidationStatusBtn({ importJob }) {
   const [modalOpen, setModalOpen] = useState(false)
-  const contentStyling = 'text-charcoal-900 pb-4'
+  const labelStyling = 'text-sm text-blumine-700 mb-2'
+  const contentStyling = 'text-base text-charcoal-900 pb-4'
   const { mutate: validateImportJob } = useImportJobValidate()
 
   function getButton(validationStatus) {
@@ -69,82 +69,84 @@ function ValidationStatusBtn({ importJob }) {
       >
         <div
           data-testid="ImportValidationReportModal"
-          className="bg-white p-4 rounded-md min-w-3xl max-w-7xl mb-4"
+          className="bg-white rounded-md"
         >
-          <h2 className="text-center text-3xl text-blumine-800 pb-2">
-            Validation Report
+          <h2 className="text-center text-3xl text-blumine-800 py-4">
+            {importJob?.title}
+            <div className="text-xl mt-1">Validation Report</div>
           </h2>
-          <div className="text-left grid grid-cols-2 gap-4 p-6 divide-y-2 divide-charcoal-200">
-            <div className="col-span-1">
-              <Form.FieldLabel nameId="title" text="Title" />
-              <div className={contentStyling}>{importJob?.title}</div>
-            </div>
-            <div className="col-span-1">
-              <Form.FieldLabel nameId="csvFile" text="Import CSV filename" />
-              <div className={contentStyling}>
-                {getLastPathSegment(importJob?.data?.path)}
+          <div className="text-left px-8 pb-8 min-w-3xl max-w-5xl">
+            <div className="text-left divide-y-2 divide-charcoal-200 space-y-4">
+              <div>
+                <div data-testid="fileName" className={labelStyling}>
+                  File name
+                </div>
+                <div className={contentStyling}>
+                  {getLastPathSegment(importJob?.data?.path)}
+                </div>
+              </div>
+              <div>
+                <div data-testid="acceptedColumns" className={labelStyling}>
+                  Accepted columns
+                </div>
+                <div className={`${contentStyling}  grid grid-cols-3 gap-x-4`}>
+                  {importJob?.validationReport?.acceptedColumns?.length > 0
+                    ? importJob?.validationReport?.acceptedColumns?.map(
+                        (col) => (
+                          <div key={col} className="col-span-1">
+                            {col}
+                          </div>
+                        ),
+                      )
+                    : 'None'}
+                </div>
+              </div>
+              <div>
+                <div data-testid="ignoredColumns" className={labelStyling}>
+                  Ignored Columns
+                </div>
+                <div className={`${contentStyling}  grid grid-cols-3 gap-x-4`}>
+                  {importJob?.validationReport?.ignoredColumns?.length > 0
+                    ? importJob?.validationReport?.ignoredColumns?.map(
+                        (col) => (
+                          <div key={col} className="col-span-1">
+                            {col}
+                          </div>
+                        ),
+                      )
+                    : 'None'}
+                </div>
+              </div>
+              <div className="grid grid-cols-3 gap-4 border-b-2 border-charcoal-200">
+                <div className="col-span-1">
+                  <div data-testid="newRows" className={labelStyling}>
+                    Total number of new rows
+                  </div>
+                  <div className={contentStyling}>
+                    {importJob?.validationReport?.newRows}
+                  </div>
+                </div>
+                <div className="col-span-1">
+                  <div data-testid="updatedRows" className={labelStyling}>
+                    Total number of rows to be updated
+                  </div>
+                  <div className={contentStyling}>
+                    {importJob?.validationReport?.updatedRows || 'N/A'}
+                  </div>
+                </div>
+                <div className="col-span-1">
+                  <div data-testid="errorRows" className={labelStyling}>
+                    Total number of rows with errors
+                  </div>
+                  <div className={contentStyling}>
+                    {importJob?.validationReport?.errorRows}
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-span-2">
-              <Form.FieldLabel
-                nameId="acceptedColumns"
-                text="Accepted columns"
-              />
-              <div className={`${contentStyling}  grid grid-cols-3 gap-x-4`}>
-                {importJob?.validationReport?.acceptedColumns?.length > 0
-                  ? importJob?.validationReport?.acceptedColumns?.map((col) => (
-                      <div key={col} className="col-span-1">
-                        {col}
-                      </div>
-                    ))
-                  : 'None'}
-              </div>
-            </div>
-            <div className="col-span-2">
-              <Form.FieldLabel nameId="ignoredColumns" text="Ignored Columns" />
-              <div className={contentStyling}>
-                {importJob?.validationReport?.ignoredColumns?.length > 0
-                  ? importJob?.validationReport?.ignoredColumns?.map((col) => (
-                      <div key={col} className="col-span-1">
-                        {col}
-                      </div>
-                    ))
-                  : 'None'}
-              </div>
-            </div>
-
-            <div className="col-span-1">
-              <Form.FieldLabel
-                nameId="newRows"
-                text="Total number of new rows"
-              />
-              <div className={contentStyling}>
-                {importJob?.validationReport?.newRows}
-              </div>
-            </div>
-            <div className="col-span-1">
-              <Form.FieldLabel
-                nameId="updatedRows"
-                text="Total number of rows to be updated"
-              />
-              <div className={contentStyling}>
-                {importJob?.validationReport?.updatedRows || 'N/A'}
-              </div>
-            </div>
-            <div className="col-span-2">
-              <Form.FieldLabel
-                nameId="errorRows"
-                text="Total number of rows with errors"
-              />
-              <div className={contentStyling}>
-                {importJob?.validationReport?.errorRows}
-              </div>
-            </div>
-            <div className="col-span-2">
-              <div className="text-base font-semibold text-blumine-800">
-                Error details
-              </div>
-              <div className="mt-6">
+            <div className="mt-6">
+              <div className={labelStyling}>Error details</div>
+              <div>
                 {importJob?.validationReport?.errorDetails?.length > 0 ? (
                   <div className="overflow-hidden shadow outline-1 outline-charcoal-300 rounded-md">
                     <table className="relative min-w-full divide-y divide-charcoal-300">
@@ -171,11 +173,11 @@ function ValidationStatusBtn({ importJob }) {
                         {importJob?.validationReport?.errorDetails?.map(
                           (row) => (
                             <tr key={row?.rowNumber}>
-                              <td className="whitespace-nowrap py-4 pl-6 pr-3 text-sm font-medium text-charcoal-900">
+                              <td className="whitespace-nowrap py-4 pl-6 pr-3 text-left justify-start text-sm font-medium text-charcoal-900">
                                 {row?.rowNumber}
                               </td>
-                              <td className="whitespace-nowrap px-3 py-4 text-sm text-charcoal-500">
-                                <ul>
+                              <td className="px-3 py-4 text-left text-sm text-charcoal-500">
+                                <ul className="list-disc">
                                   {row?.errors?.map((error) => (
                                     <li key={error}>{error}</li>
                                   ))}
