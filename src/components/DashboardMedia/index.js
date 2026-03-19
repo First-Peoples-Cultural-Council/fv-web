34201bdfdefe3c442a888815e3f202b7d2533bfa
Guy McAuliffe <guy@fpcc.ca>
2026-03-19 16:04:39 -0700

    Remove unnecessary layer

--- src/components/DashboardMedia/index.js
+++ src/components/DashboardMedia/index.js
@@ -1,9 +1,7 @@
 import DashboardMediaContainer from 'components/DashboardMedia/DashboardMediaContainer'
 import DashboardMediaData from 'components/DashboardMedia/DashboardMediaData'
-import DashboardMediaPresentation from 'components/DashboardMedia/DashboardMediaPresentation'
 
 export default {
   Container: DashboardMediaContainer,
   Data: DashboardMediaData,
-  Presentation: DashboardMediaPresentation,
 }
