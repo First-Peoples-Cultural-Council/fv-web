81d8a954f3b9e43660e3cfceef88590c49277831
Guy McAuliffe <guy@fpcc.ca>
2026-04-16 16:21:01 -0700

    Update styling on Dictionary links

--- src/components/DictionaryLinks/DictionaryLinks.js
+++ src/components/DictionaryLinks/DictionaryLinks.js
@@ -8,55 +8,49 @@ function DictionaryLinks() {
   const { sitename } = useParams()
   const { pathname } = useLocation()
   return (
-    <nav data-testid="DictionaryLinks">
-      <h2 className="text-xl xl:text-2xl font-medium ml-4 xl:ml-7 text-charcoal-900">
-        BROWSE BY:
-      </h2>
-      <ul className="inline-block list-none">
-        {!pathname?.includes('categories') && (
-          <DictionaryLink
-            key="CategoriesLink"
-            linkProperties={{
-              id: 'CategoryLink',
-              iconId: 'Categories',
-              linkTo: `/${sitename}/categories`,
-              label: 'Categories',
-            }}
-          />
-        )}
-        {!pathname?.includes('alphabet') && (
-          <DictionaryLink
-            key="AlphabetLink"
-            linkProperties={{
-              id: 'AlphabetLink',
-              iconId: 'Alphabet',
-              linkTo: `/${sitename}/alphabet`,
-              label: 'Alphabet',
-            }}
-          />
-        )}
-        {!pathname?.includes('/words') && (
-          <DictionaryLink
-            key="WordsLink"
-            linkProperties={{
-              id: 'WordsLink',
-              iconId: 'Word',
-              linkTo: `/${sitename}/words`,
-              label: 'Words',
-            }}
-          />
-        )}
-        {!pathname?.includes('/phrases') && (
-          <DictionaryLink
-            key="PhrasesLink"
-            linkProperties={{
-              id: 'PhrasesLink',
-              iconId: 'Phrase',
-              linkTo: `/${sitename}/phrases`,
-              label: 'Phrases',
-            }}
-          />
-        )}
+    <nav data-testid="DictionaryLinks" className="ml-4 xl:ml-7">
+      <h2 className="text-xl font-bold text-charcoal-900 mb-4">Browse by:</h2>
+      <ul className="inline-block list-none space-y-2">
+        <DictionaryLink
+          key="WordsLink"
+          current={pathname?.includes('/words')}
+          linkProperties={{
+            id: 'WordsLink',
+            iconId: 'Word',
+            linkTo: `/${sitename}/words`,
+            label: 'Words',
+          }}
+        />
+        <DictionaryLink
+          key="AlphabetLink"
+          current={pathname?.includes('alphabet')}
+          linkProperties={{
+            id: 'AlphabetLink',
+            iconId: 'Alphabet',
+            linkTo: `/${sitename}/alphabet/startsWith`,
+            label: 'Alphabet',
+          }}
+        />
+        <DictionaryLink
+          key="PhrasesLink"
+          current={pathname?.includes('/phrases')}
+          linkProperties={{
+            id: 'PhrasesLink',
+            iconId: 'Phrase',
+            linkTo: `/${sitename}/phrases`,
+            label: 'Phrases',
+          }}
+        />
+        <DictionaryLink
+          key="CategoriesLink"
+          current={pathname?.includes('categories')}
+          linkProperties={{
+            id: 'CategoryLink',
+            iconId: 'Categories',
+            linkTo: `/${sitename}/categories`,
+            label: 'Categories',
+          }}
+        />
       </ul>
     </nav>
   )
