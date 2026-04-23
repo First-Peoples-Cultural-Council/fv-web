620168d1537f65571be919c9cb3d574d51aa37af
Guy McAuliffe <guy@fpcc.ca>
2026-04-23 12:01:10 -0700

    Update App widget

--- src/components/WidgetApps/WidgetAppsPresentation.js
+++ src/components/WidgetApps/WidgetAppsPresentation.js
@@ -1,103 +1,77 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 // FPCC
-import SectionTitle from 'components/SectionTitle'
 import { useSiteStore } from 'context/SiteContext'
 import getIcon from 'common/utils/getIcon'
-import { getAppUrl, getAppLogoUrl } from 'common/utils/getAppUrl'
+import { getAppDetails } from 'common/utils/appHelpers'
 import LogoPresentation from 'components/SiteLogo/LogoPresentation'
 import QrcodeCanvas from 'components/Actions/QrcodeCanvas'
 
 function WidgetAppsPresentation() {
   const { site } = useSiteStore()
-  const appUrl = getAppUrl({ slug: site?.sitename })
-  const appLogoSrc = getAppLogoUrl({ slug: site?.sitename })
+  const appDetails = getAppDetails({ site })
 
   return (
-    <section className="px-4 lg:px-0 py-3 lg:py-6 bg-white">
-      <div className="mx-5 lg:mx-10 mb-4 md:mb-6 lg:mb-8 xl:mb-12">
-        <SectionTitle.Presentation title="DOWNLOAD MOBILE APP" />
-        <h2 className="text-center text-blumine-600 text-sm md:text-base lg:text-2xl mt-2 md:mt-3.5 lg:mt-5">
-          {site?.title}
-        </h2>
-      </div>
-      <div className="grid grid-cols-7 gap-6 md:gap-4 mx-auto max-w-5xl">
-        <div className="md:text-lg text-charcoal-700 col-span-7 md:col-span-4 space-y-4 md:space-y-8">
-          <div className="space-y-4 md:space-y-8">
-            <div className="flex">
-              <div className="flex-none w-12 p-1">
-                {getIcon('Search', 'fill-current h-5 w-auto mx-auto')}
-              </div>
-              <p className="grow">Browse words and phrases in the dictionary</p>
+    <section
+      id="WidgetAppsPresentation"
+      className="mx-auto bg-white py-3 sm:py-8"
+    >
+      <div className="mx-auto max-w-4xl px-6 md:px-10 my-4">
+        <div className="isolate overflow-hidden md:flex md:items-center md:gap-x-6">
+          <div className="mx-auto text-center md:mx-0 md:flex-auto md:py-0 md:text-left">
+            <h2 className="text-balance text-blumine-700 text-3xl sm:text-4xl font-semibold tracking-tight">
+              Get the {site?.title} Language App to use on your favourite
+              devices.
+            </h2>
+            <div className="text-left mt-6">
+              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-charcoal-600 md:max-w-none">
+                {appDetails?.features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold text-blumine-700">
+                      {getIcon(
+                        feature.icon,
+                        'absolute left-1 top-1 size-5  fill-current',
+                      )}
+                    </dt>
+                    <dd className="inline">{feature.description}</dd>
+                  </div>
+                ))}
+              </dl>
             </div>
-            <div className="flex">
-              <div className="flex-none w-12 p-1">
-                {getIcon('Flashcard', 'fill-current h-5 w-auto mx-auto')}
-              </div>
-              <p className="grow">Practice with flashcards</p>
-            </div>
-            <div className="flex">
-              <div className="flex-none w-12 p-1">
-                {getIcon('Bookmark', 'fill-current h-5 w-auto mx-auto')}
-              </div>
-              <p className="grow">
-                Bookmark content and more with the {site?.title} mobile app!
-              </p>
-            </div>
-            <div className="flex">
-              <div className="flex-none w-12 p-1">
-                {getIcon('Devices', 'fill-current h-5 w-auto mx-auto')}
-              </div>
-              <p className="grow">
-                Compatible with iPhone, iPad, Android, Chromebook, Windows, and
-                more.
-              </p>
-            </div>
-          </div>
-
-          <p>
-            <a
-              className="inline-url"
-              href={appUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Install the {site?.title} Language App
-            </a>{' '}
-            directly from the web by selecting “Install” or “Add to Homescreen”
-            in your browser.
-          </p>
-          <p>
-            <a
-              className="inline-url text-base italic"
-              href="https://firstvoices.atlassian.net/wiki/spaces/FIR1/pages/644579350"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Installation Help Link
-            </a>
-          </p>
-        </div>
-        <div className="col-span-7 md:col-span-3">
-          <div className="flex-col items-center justify-center space-y-2">
-            <LogoPresentation
-              imgSrc={appLogoSrc}
-              altText={`${site?.title} Logo`}
-              additionalStyling="h-44 w-44 mx-auto border"
-            />
-            <div className="flex justify-center w-full">
-              <QrcodeCanvas url={appUrl} />
-            </div>
-            <div className="flex justify-center w-full">
+            <div className="mt-10 flex items-center justify-center gap-x-6 md:justify-start">
               <a
-                href={appUrl}
+                href={appDetails?.url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="btn-primary btn-lg"
+                className="btn-primary btn-md"
               >
-                {getIcon('Download')}
-                <span>Install now</span>
+                Go to the App
               </a>
+
+              <Link
+                to={`/${site?.sitename}/apps`}
+                className="btn-tertiary btn-md"
+              >
+                <span>Learn more</span>
+                {getIcon('RightArrow')}
+              </Link>
+            </div>
+          </div>
+          <div className="h-full w-full flex items-center justify-center p-7 lg:p-12">
+            <div className="flex-col items-center justify-center space-y-7">
+              <div className="flex justify-center w-full">
+                <div className="flex items-center justify-center rounded-3xl shadow-lg border border-charcoal-500 p-4">
+                  <LogoPresentation
+                    imgSrc={appDetails?.logoUrl}
+                    altText={`${site?.title} Logo`}
+                    additionalStyling="h-32 w-32 md:h-52 md:w-52 mx-0 border"
+                  />
+                </div>
+              </div>
+              <div className="flex justify-center w-full">
+                <QrcodeCanvas url={appDetails?.url} />
+              </div>
             </div>
           </div>
         </div>
