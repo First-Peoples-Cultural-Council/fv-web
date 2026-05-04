b886311f3d5f35f082594265179eb4b4fa51e686
Guy McAuliffe <guy@fpcc.ca>
2026-05-04 10:51:33 -0700

    Increase list vh for DashboardTable

--- src/components/DashboardTable/DashboardTablePresentation.js
+++ src/components/DashboardTable/DashboardTablePresentation.js
@@ -16,7 +16,7 @@ function DashboardTablePresentation({
         <div className="bg-white shadow-sm rounded-lg">
           {title && <h2 className="sr-only">{title}</h2>}
 
-          <div className="max-h-[75vh] overflow-auto pt-2 bg-charcoal-50 shadow-sm rounded-lg">
+          <div className="max-h-[90vh] overflow-auto pt-2 bg-charcoal-50 shadow-sm rounded-lg">
             <table className="min-w-full divide-y divide-charcoal-100">
               <thead className="text-charcoal-500 rounded-lg overflow-hidden">
                 {tableHead}
