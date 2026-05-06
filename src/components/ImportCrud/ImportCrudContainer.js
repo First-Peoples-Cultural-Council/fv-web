db9444e5f311baf6a66d665d1de0afb759f421e0
Guy McAuliffe <guy@fpcc.ca>
2026-05-06 14:38:14 -0700

    Remove media uploading from import create form

--- src/components/ImportCrud/ImportCrudContainer.js
+++ src/components/ImportCrud/ImportCrudContainer.js
@@ -3,25 +3,19 @@ import React from 'react'
 // FPCC
 import ImportCrudPresentation from 'components/ImportCrud/ImportCrudPresentation'
 import ImportCrudData from 'components/ImportCrud/ImportCrudData'
-import LoadOrError from 'components/LoadOrError'
 import SiteDocHead from 'components/SiteDocHead'
 
 function ImportCrudContainer() {
-  const { backHandler, importJobId, queryResponse, submitHandler, uppy } =
-    ImportCrudData()
-
-  const action = importJobId ? 'Edit' : 'Create'
+  const { backHandler, submitHandler } = ImportCrudData()
 
   return (
-    <LoadOrError queryResponse={queryResponse} bypass={!importJobId}>
-      <SiteDocHead titleArray={[`${action} Import Job`]} />
+    <>
+      <SiteDocHead titleArray={['Create', 'Import Job']} />
       <ImportCrudPresentation
         backHandler={backHandler}
-        dataToEdit={queryResponse?.data}
         submitHandler={submitHandler}
-        uppy={uppy}
       />
-    </LoadOrError>
+    </>
   )
 }
 
--- src/components/ImportCrud/ImportCrudData.js
+++ src/components/ImportCrud/ImportCrudData.js
@@ -1,7 +1,4 @@
-import { useNavigate, useParams, useSearchParams } from 'react-router'
-import { useState } from 'react'
-import Uppy from '@uppy/core'
-import XHR from '@uppy/xhr-upload'
+import { useNavigate, useParams } from 'react-router'
 
 // Uppy
 import '@uppy/core/css/style.min.css'
@@ -9,85 +6,21 @@ import '@uppy/dashboard/css/style.min.css'
 import '@uppy/image-editor/css/style.min.css'
 
 // FPCC
-import GlobalConfiguration from 'src/GlobalConfiguration'
-import { getAuthHeaderIfTokenExists } from 'common/utils/authHelpers'
-import { useSiteStore } from 'context/SiteContext'
-import {
-  IMPORT_JOBS,
-  SITES,
-  SUPPORTED_IMPORT_MEDIA_EXTENSIONS,
-} from 'common/constants'
-import {
-  useImportJob,
-  useImportJobCreate,
-} from 'common/dataHooks/useImportJobs'
+import { useImportJobCreate } from 'common/dataHooks/useImportJobs'
 
 function ImportCrudData() {
-  const { site } = useSiteStore()
   const { sitename } = useParams()
 
   const navigate = useNavigate()
   const backHandler = () => navigate(`/${sitename}/dashboard/imports`)
 
-  const [searchParams] = useSearchParams()
-  const importJobId = searchParams.get('id') || null
-
-  const queryResponse = useImportJob({ id: importJobId })
   const { mutate: create } = useImportJobCreate()
 
   const submitHandler = (formData) => create(formData)
 
-  const uploadEndpoint = new URL(
-    `${SITES}/${sitename}/${IMPORT_JOBS}/${importJobId}/media/`,
-    GlobalConfiguration.API_URL,
-  )
-
-  const allowedFileTypes = SUPPORTED_IMPORT_MEDIA_EXTENSIONS.map(
-    (extension) => `.${extension}`,
-  )
-
-  // IMPORTANT: passing an initializer function to prevent Uppy from being reinstantiated on every render.
-  const [uppy] = useState(() =>
-    new Uppy({
-      id: 'mediaUpload',
-      autoProceed: false,
-      allowMultipleUploadBatches: true,
-      restrictions: {
-        allowedFileTypes: allowedFileTypes,
-      },
-    })
-      .on('upload-error', (file, error, xhr) => {
-        if (xhr) {
-          const parsedResponse = JSON.parse(xhr?.response)
-          const serverMessage = parsedResponse?.[0]
-            ? parsedResponse?.[0]
-            : `Server-side validation failed: ${xhr?.responseText}`
-
-          // Display the message in the Uppy UI
-          uppy.info(serverMessage, 'error', 6000)
-        }
-      })
-      .use(XHR, {
-        endpoint: uploadEndpoint.href,
-        fieldName: 'file',
-        formData: true,
-        headers: getAuthHeaderIfTokenExists(),
-        async getResponseData(xhr) {
-          if (xhr.status >= 200 && xhr.status < 300) {
-            return { ok: true, status: xhr.status }
-          }
-          return { ok: false, status: xhr.status }
-        },
-      }),
-  )
-
   return {
     submitHandler,
     backHandler,
-    site,
-    queryResponse,
-    importJobId,
-    uppy,
   }
 }
 
