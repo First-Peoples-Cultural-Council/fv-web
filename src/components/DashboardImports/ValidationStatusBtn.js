492ab98773e1a14ac2a28e9e3d4a136faf35f2e1
Guy McAuliffe <guy@fpcc.ca>
2026-05-14 09:03:07 -0700

    Render via switch

--- src/components/DashboardImports/ValidationStatusBtn.js
+++ src/components/DashboardImports/ValidationStatusBtn.js
@@ -20,87 +20,80 @@ function ValidationStatusBtn({ importJob, handleRefetch }) {
   const [modalOpen, setModalOpen] = useState(false)
   const { mutate: validateImportJob } = useImportJobValidate()
 
-  function getButton(validationStatus) {
-    switch (validationStatus) {
-      case COMPLETE:
-        return (
-          <div className="inline-flex items-center justify-center space-x-2">
-            <button
-              data-testid="validation-results-btn"
-              type="button"
-              onClick={() => setModalOpen(true)}
-              className="btn-secondary btn-sm text-nowrap"
-            >
-              <span>View results</span>
-              {importJob?.validationReport?.errorRows > 0 ? (
-                <Tooltip message="Errors found">
-                  {getIcon(
-                    'ExclamationTriangleSolid',
-                    'fill-current text-ochre-600',
-                  )}
-                </Tooltip>
-              ) : (
-                getIcon('CheckCircleSolid')
-              )}
-            </button>
+  switch (importJob?.validationStatus) {
+    case COMPLETE:
+      return (
+        <div className="inline-flex items-center justify-center space-x-2">
+          <button
+            data-testid="validation-results-btn"
+            type="button"
+            onClick={() => setModalOpen(true)}
+            className="btn-secondary btn-sm text-nowrap"
+          >
+            <span>View results</span>
+            {importJob?.validationReport?.errorRows > 0 ? (
+              <Tooltip message="Errors found">
+                {getIcon(
+                  'ExclamationTriangleSolid',
+                  'fill-current text-ochre-600',
+                )}
+              </Tooltip>
+            ) : (
+              getIcon('CheckCircleSolid')
+            )}
+          </button>
 
-            <Tooltip message="Re-validate">
-              <button
-                data-testid="revalidate-btn"
-                type="button"
-                onClick={() => validateImportJob(importJob?.id)}
-                className="btn-tertiary btn-md-icon"
-              >
-                {getIcon('TryAgain')}
-              </button>
-            </Tooltip>
-          </div>
-        )
-      case STARTED:
-      case ACCEPTED:
-        return (
-          <Tooltip message="Click to check if validation is complete">
+          <Modal.Presentation
+            isOpen={modalOpen}
+            closeHandler={() => setModalOpen(false)}
+          >
+            <ValidationReport importJob={importJob} />
+          </Modal.Presentation>
+
+          <Tooltip message="Re-validate">
             <button
-              data-testid="validate-btn"
+              data-testid="revalidate-btn"
               type="button"
-              onClick={handleRefetch}
-              className="btn-tertiary btn-sm"
+              onClick={() => validateImportJob(importJob?.id)}
+              className="btn-tertiary btn-md-icon"
             >
-              <span>In progress</span>
+              {getIcon('TryAgain')}
             </button>
           </Tooltip>
-        )
-      case FAILED:
-      case CANCELLED:
-      case EXPIRED:
-        return (
-          <p className="whitespace-nowrap text-sm text-scarlet-800">{`Validation ${importJob?.validationStatus}!`}</p>
-        )
-      default:
-        return (
+        </div>
+      )
+    case STARTED:
+    case ACCEPTED:
+      return (
+        <Tooltip message="Click to check if validation is complete">
           <button
             data-testid="validate-btn"
             type="button"
-            onClick={() => validateImportJob(importJob?.id)}
-            className="btn-primary btn-sm"
+            onClick={handleRefetch}
+            className="btn-tertiary btn-sm"
           >
-            <span>Validate</span>
+            <span>In progress</span>
           </button>
-        )
-    }
+        </Tooltip>
+      )
+    case FAILED:
+    case CANCELLED:
+    case EXPIRED:
+      return (
+        <p className="whitespace-nowrap text-sm text-scarlet-800">{`Validation ${importJob?.validationStatus}!`}</p>
+      )
+    default:
+      return (
+        <button
+          data-testid="validate-btn"
+          type="button"
+          onClick={() => validateImportJob(importJob?.id)}
+          className="btn-primary btn-sm"
+        >
+          <span>Validate</span>
+        </button>
+      )
   }
-
-  return (
-    <>
-      {getButton(importJob?.validationStatus)}
-      <Modal.Presentation
-        isOpen={modalOpen}
-        closeHandler={() => setModalOpen(false)}
-      >
-        <ValidationReport importJob={importJob} />
-      </Modal.Presentation>
-    </>
-  )
 }
 // PROPTYPES
 const { func, object } = PropTypes
