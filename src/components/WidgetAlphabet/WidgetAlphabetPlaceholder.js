89851023a900fcff705908994ca395426908b4ac
Guy McAuliffe <guy@fpcc.ca>
2026-05-15 11:17:14 -0700

    Update placeholder styling

--- src/components/WidgetAlphabet/WidgetAlphabetPlaceholder.js
+++ src/components/WidgetAlphabet/WidgetAlphabetPlaceholder.js
@@ -3,26 +3,29 @@ import SectionTitle from 'components/SectionTitle'
 
 function WidgetAlphabetPlaceholder() {
   return (
-    <section className="py-12 bg-white" data-testid="WidgetAlphabetPlaceholder">
-      <div className="mx-10 mb-12">
-        <SectionTitle.Presentation />
+    <section
+      data-testid="WidgetAlphabetPlaceholder"
+      className="py-3 md:py-6 bg-white"
+    >
+      <div className="mx-5 lg:mx-10 mb-6 lg:mb-12">
+        <SectionTitle.Presentation title="ALPHABET" />
       </div>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-7 gap-2">
-          <div className="col-span-7 md:col-span-4 md:pr-4 xl:pr-8 md:border-r-2 border-charcoal-200">
-            <div className="grid grid-cols-7 md:grid-cols-5 lg:grid-cols-7">
+      <div className="max-w-7xl mx-auto px-4 lg:px-8">
+        <div className="grid grid-cols-12 gap-6 lg:gap-11">
+          <div className="col-span-7 py-3 pr-6 lg:pr-11 border-r-2 border-charcoal-200 content-center">
+            <div className="grid grid-cols-6 lg:grid-cols-7 gap-3">
               {[...new Array(34)].map((_, i) => (
                 <span
                   key={i}
-                  className="bg-charcoal-50 text-charcoal-50 col-span-1 font-medium inline-flex justify-center m-1 p-5 rounded-sm text-2xl"
+                  className="bg-charcoal-50 col-span-1 font-medium inline-flex justify-center p-4 rounded-sm text-2xl"
                 >
                   &nbsp;
                 </span>
               ))}
             </div>
           </div>
-          <div className="col-span-7 md:col-span-3 mt-8 md:mt-0">
-            <div className="text-center font-bold sm:text-3xl text-2xl m-10 bg-charcoal-50 text-charcoal-50">
+          <div className="col-span-5 content-center">
+            <div className="text-center sm:text-3xl text-2xl p-20 bg-charcoal-50 text-charcoal-50">
               &nbsp;
             </div>
           </div>
