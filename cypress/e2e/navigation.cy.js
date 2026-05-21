6c22997a794f46e208f0075c4557c1c27d2fbfa3
Guy McAuliffe <guy@fpcc.ca>
2026-05-21 09:52:22 -0700

    Update data-testid in test

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -38,6 +38,10 @@ describe(
     it('12.1 - alphabet', () => {
       cy.visit(`${Cypress.env('baseUrl')}${Cypress.env('CYPRESS_DIALECT')}`)
       cy.contains('ALPHABET').should('exist')
+      cy.get('[data-testid="character-detail-header"] button').should(
+        'have.length',
+        2,
+      )
     })
 
     it('explore languages - search', () => {
