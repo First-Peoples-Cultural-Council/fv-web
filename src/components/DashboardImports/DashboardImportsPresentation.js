8a745371394f15937690181f58920bdab534f56b
Guy McAuliffe <guy@fpcc.ca>
2026-06-09 11:13:30 -0700

    Add "ready for import" status constant

--- src/components/DashboardImports/DashboardImportsPresentation.js
+++ src/components/DashboardImports/DashboardImportsPresentation.js
@@ -14,6 +14,7 @@ import getIcon from 'common/utils/getIcon'
 import {
   ACCEPTED,
   STARTED,
+  READY_FOR_IMPORT,
   COMPLETE,
   FAILED,
   CANCELLED,
@@ -67,6 +68,7 @@ function DashboardImportsPresentation({
         return `This import has ${importJob?.status}. Contact support for more information`
       case CANCELLED:
         return `This import was ${importJob?.status}. Contact support for more information`
+      case READY_FOR_IMPORT:
       default:
         return <span className="capitalize">{importJob?.status}</span> || ''
     }
