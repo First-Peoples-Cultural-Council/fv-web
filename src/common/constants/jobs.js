108c73e75b11e1d807e9e322d00b0766e43a78b4
Guy McAuliffe <guy@fpcc.ca>
2026-06-10 15:25:56 -0700

    Add Export confirmation modal

--- src/common/constants/jobs.js
+++ src/common/constants/jobs.js
@@ -7,4 +7,5 @@ export const FAILED = 'failed'
 export const CANCELLED = 'cancelled'
 export const EXPIRED = 'expired'
 
-export const EXPORT_LIMIT = 7500
+export const EXPORT_SIZE_LIMIT = 7500
+export const EXPORT_JOBS_PER_USER_LIMIT = 10
