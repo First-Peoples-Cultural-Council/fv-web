8f42c5ec8b3df54971b66f179af2ccceca4212a2
Guy McAuliffe <guy@fpcc.ca>
2026-07-06 14:09:49 -0700

    Remove classNames function

--- src/components/SearchTypeSelector/SearchTypeSelectorContainer.js
+++ src/components/SearchTypeSelector/SearchTypeSelectorContainer.js
@@ -8,23 +8,27 @@ import { TYPE_DICTIONARY, TYPE_PHRASE, TYPE_WORD } from 'common/constants'
 
 function SearchTypeSelectorContainer({ selectedSearchType, setSearchType }) {
   const dictionaryTypes = [
-    { label: 'WORDS', icon: 'Word', value: TYPE_WORD },
+    {
+      label: 'WORDS',
+      icon: 'Word',
+      value: TYPE_WORD,
+      tabStyling: 'rounded-l-lg border-r-0',
+    },
     {
       label: 'PHRASES',
       icon: 'Phrase',
       value: TYPE_PHRASE,
+      tabStyling: '',
     },
     {
       label: 'BOTH',
       icon: 'All',
       value: TYPE_DICTIONARY,
+      tabStyling: 'rounded-r-lg border-l-0',
     },
   ]
 
   const [t] = useTranslation()
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
 
   return (
     <div id="SearchTypeSelectorContainer">
@@ -32,23 +36,18 @@ function SearchTypeSelectorContainer({ selectedSearchType, setSearchType }) {
         className="flex items-center relative w-auto rounded-lg"
         aria-label="Tabs"
       >
-        {dictionaryTypes?.map((tab, tabIndex) => (
+        {dictionaryTypes?.map((tab) => (
           <button
             data-testid={`${tab?.value}-tab-btn`}
             type="button"
             key={tab?.value}
             value={tab?.value}
             onClick={() => setSearchType(tab?.value)}
-            className={classNames(
+            className={`${
               tab?.value === selectedSearchType
                 ? `text-white bg-blumine-800 border-blumine-800`
-                : 'text-charcoal-500 bg-white hover:text-charcoal-700 hover:bg-charcoal-50 border-charcoal-100',
-              tabIndex === 0 ? 'rounded-l-lg border-r-0' : '',
-              tabIndex === dictionaryTypes.length - 1
-                ? 'rounded-r-lg border-l-0'
-                : '',
-              'group relative min-w-auto flex items-center border-2 py-2 px-4 font-medium text-center focus:outline-hidden focus:ring-2 focus:ring-inset focus:ring-charcoal-500',
-            )}
+                : 'text-charcoal-500 bg-white hover:text-charcoal-700 hover:bg-charcoal-50 border-charcoal-100'
+            } ${tab?.tabStyling} group relative min-w-auto flex items-center border-2 py-2 px-4 font-medium text-center focus:outline-hidden focus:ring-2 focus:ring-inset focus:ring-charcoal-500`}
             aria-current={
               tab?.value === selectedSearchType ? tab?.label : undefined
             }
