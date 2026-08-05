8fb17efc9f207d106657e161f1e97817baff4269
Guy McAuliffe <guy@fpcc.ca>
2026-08-05 12:50:03 -0700

    Simplify styling logic for cell

--- src/components/Game/Wordsy/Grid/Cell.js
+++ src/components/Game/Wordsy/Grid/Cell.js
@@ -1,32 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function getClassList(status, value) {
-  let baseClasses =
-    'w-10 h-10 border-solid border-2 flex items-center justify-center mx-0.5 text-lg font-bold rounded-sm'
-  if (!status) {
-    baseClasses += ' bg-white border-charcoal-100'
-  }
-  if (value && !status) {
-    baseClasses += ' border-black'
-  }
-  if (status === 'absent') {
-    baseClasses += ' bg-charcoal-300 text-white border-charcoal-300'
-  }
-  if (status === 'correct') {
-    baseClasses += ' bg-wordsy-correct text-white border-wordsy-correct'
-  }
-  if (status === 'present') {
-    baseClasses += ' bg-wordsy-present text-white border-wordsy-present'
-  }
-  if (value) {
-    baseClasses += ' cell-animation'
-  }
-  return baseClasses
-}
-
 function Cell({ status, value }) {
-  return <div className={getClassList(status, value)}>{value}</div>
+  function getStatusStyling(status, value) {
+    switch (status) {
+      case 'absent':
+        return 'bg-charcoal-300 border-charcoal-300 text-white'
+      case 'correct':
+        return 'bg-wordsy-correct border-wordsy-correct text-white'
+      case 'present':
+        return 'bg-wordsy-present border-wordsy-present text-white '
+      default:
+        return `bg-white ${value ? 'border-black' : 'border-charcoal-100'}`
+    }
+  }
+
+  return (
+    <div
+      className={`w-10 h-10 border-solid border-2 flex items-center justify-center mx-0.5 text-lg font-bold rounded-sm ${getStatusStyling(status, value)}`}
+    >
+      {value}
+    </div>
+  )
 }
 
 const { string } = PropTypes
