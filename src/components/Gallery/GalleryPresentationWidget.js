4d5156f74933f0fcdaa944ad1e344a3c2d871ea5
Guy McAuliffe <guy@fpcc.ca>
2026-08-19 13:39:24 -0700

    Add new mobile view for GalleryWidget and columns

--- src/components/Gallery/GalleryPresentationWidget.js
+++ src/components/Gallery/GalleryPresentationWidget.js
@@ -1,47 +1,98 @@
 import React from 'react'
 import { Link } from 'react-router'
 import PropTypes from 'prop-types'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 
 // FPCC
 import SectionTitle from 'components/SectionTitle'
 import ImageWithLightbox from 'components/ImageWithLightbox'
+import MediaThumbnail from 'components/MediaThumbnail'
+import { getMediaPath } from 'common/utils/mediaHelpers'
+import { IMAGE, SMALL } from 'common/constants'
 
 function GalleryPresentationWidget({ data, sitename }) {
-  const images =
-    window.innerWidth >= 1024
-      ? data?.galleryItems?.slice(0, 7)
-      : data?.galleryItems?.slice(0, 4)
+  const images = data?.galleryItems?.slice(0, 8)
   return (
     <section
       data-testid="GalleryPresentationWidget"
-      className="py-3 md:py-6 px-2 lg:px-10 bg-charcoal-900 space-y-2 lg:space-y-4"
+      className="pt-3 pb-6 md:py-6"
     >
-      <SectionTitle.Presentation
-        title={data?.title}
-        bgColor="charcoal-900"
-        accentColor="white"
-      />
-      <div className="text-center text-white text-xl">
-        {data?.titleTranslation}
+      <div className="mx-2 md:mx-5 lg:mx-10">
+        <SectionTitle.Presentation title={data?.title} />
+        <div className="px-16 text-center text-charcoal-800 text-xl my-7 md:my-8">
+          {data?.titleTranslation}
+        </div>
       </div>
-      <div className="mx-auto text-center">
-        {images?.length > 0 &&
-          images?.map((image) => (
-            <div key={image?.id} className="inline-flex m-2">
-              <ImageWithLightbox.Presentation
-                image={image}
-                imgStyling="object-contain md:h-[33vh] xl:h-[40vh] w-full"
-              />
-            </div>
-          ))}
+
+      {/* Desktop View */}
+      <div className="hidden md:block px-24">
+        <div className="columns-1 md:columns-3 xl:columns-4 gap-6">
+          {images?.length > 0 &&
+            images?.map((image) => (
+              <div key={image?.id} className="inline-flex m-2">
+                <ImageWithLightbox.Presentation image={image} />
+              </div>
+            ))}
+        </div>
+        <div className="flex justify-center mt-15">
+          <Link
+            className="btn-primary btn-lg"
+            to={`/${sitename}/galleries/${data?.id}`}
+          >
+            Go to gallery
+          </Link>
+        </div>
       </div>
-      <div className="flex justify-center">
-        <Link
-          className="btn-tertiary btn-lg"
-          to={`/${sitename}/galleries/${data?.id}`}
-        >
-          Go to gallery
-        </Link>
+
+      {/* Mobile View */}
+      <div className="block md:hidden px-8">
+        <TabGroup>
+          <TabPanels>
+            {images?.map((image) => (
+              <TabPanel
+                key={image?.id}
+                className="aspect-square max-h-96 w-full flex items-center rounded-lg bg-charcoal-100 overflow-hidden"
+              >
+                <div className="relative group flex w-full h-full">
+                  <img
+                    className="object-contain rounded-lg w-full"
+                    src={getMediaPath({
+                      mediaObject: image,
+                      type: IMAGE,
+                      size: SMALL,
+                    })}
+                    alt={image?.title}
+                  />
+                </div>
+              </TabPanel>
+            ))}
+          </TabPanels>
+          {images?.length > 1 && (
+            <TabList className="flex overflow-x-auto space-x-4 snap-x mt-3">
+              {images?.map((image) => (
+                <Tab
+                  key={image?.id}
+                  className="shrink-0 snap-start rounded-lg overflow-hidden w-15 h-15 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-blumine-600 data-selected:border-4 data-selected:border-blumine-500"
+                >
+                  <MediaThumbnail.Image
+                    key={image?.id}
+                    imageObject={image}
+                    containerStyles=""
+                    imageStyles="w-15 h-15 object-cover"
+                  />
+                </Tab>
+              ))}
+              <Link
+                to={`/${sitename}/galleries/${data?.id}`}
+                className="shrink-0 snap-start rounded-lg overflow-hidden w-15 h-15 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-blumine-600 data-selected:border-4 data-selected:border-blumine-500"
+              >
+                <div className="w-15 h-15 flex text-xs text-white items-center justify-center bg-blumine-800">
+                  See More
+                </div>
+              </Link>
+            </TabList>
+          )}
+        </TabGroup>
       </div>
     </section>
   )
