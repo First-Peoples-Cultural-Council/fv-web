befdebca25e0284e3e496108f955d867a8c55a78
Guy McAuliffe <guy@fpcc.ca>
2026-08-27 14:21:01 -0700

    Include members only words in totals if logged in and member

--- src/common/dataHooks/useStats.js
+++ src/common/dataHooks/useStats.js
@@ -3,6 +3,9 @@ import { useQuery } from '@tanstack/react-query'
 
 // FPCC
 import api from 'services/api'
+import { useUserStore } from 'context/UserContext'
+import { atLeastMember } from 'common/constants/roles'
+import { isAtLeastRole } from 'common/utils/membershipHelpers'
 import { SIX_MONTHS, STATS, STATS_TYPES_FOR_WIDGET } from 'common/constants'
 
 export function useStats() {
@@ -17,22 +20,35 @@ export function useStats() {
 
 export function useStatsForWidget() {
   const { sitename } = useParams()
+
+  const { user } = useUserStore()
+  const isMember = isAtLeastRole({
+    user,
+    sitename: sitename,
+    roleRegex: atLeastMember,
+  })
+
   const queryResponse = useQuery({
     queryKey: [STATS, sitename],
     queryFn: () => api.stats.get({ sitename }),
-    select: (data) => statsAdaptor(data),
+    select: (data) => statsAdaptor(data, isMember),
   })
 
   return queryResponse
 }
 
-const statsAdaptor = (data) => {
+const statsAdaptor = (data, isMember) => {
   const dataToReturn = Object.fromEntries(
     STATS_TYPES_FOR_WIDGET.map((type) => [
       type,
       {
-        total: data?.aggregate?.[type]?.public,
-        recent: data?.temporal?.[type]?.[SIX_MONTHS]?.public,
+        total: isMember
+          ? data?.aggregate?.[type]?.members + data?.aggregate?.[type]?.public
+          : data?.aggregate?.[type]?.public,
+        recent: isMember
+          ? data?.temporal?.[type]?.[SIX_MONTHS]?.members +
+            data?.temporal?.[type]?.[SIX_MONTHS]?.public
+          : data?.temporal?.[type]?.[SIX_MONTHS]?.public,
       },
     ]),
   )
