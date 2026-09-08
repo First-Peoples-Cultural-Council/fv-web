317f4cf6e72c0ab5fce91d0a94e55419c6f024ae
Guy McAuliffe <guy@fpcc.ca>
2026-09-08 14:44:01 -0700

    Approximate Keyboard widget styling to match App Widget

--- src/components/WidgetApps/WidgetAppsPresentation.js
+++ src/components/WidgetApps/WidgetAppsPresentation.js
@@ -15,17 +15,18 @@ function WidgetAppsPresentation() {
   return (
     <section
       id="WidgetAppsPresentation"
-      className="mx-auto bg-white py-3 sm:py-8"
+      className="mx-auto max-w-7xl w-full px-2 md:px-12"
     >
-      <div className="mx-auto max-w-4xl px-6 md:px-10 my-4">
-        <div className="isolate overflow-hidden md:flex md:items-center md:gap-x-6">
-          <div className="mx-auto text-center md:mx-0 md:flex-auto md:py-0 md:text-left">
-            <h2 className="text-balance text-blumine-700 text-3xl sm:text-4xl font-semibold tracking-tight">
-              Get the {site?.title} Language App to use on your favourite
-              devices.
-            </h2>
-            <div className="text-left mt-6">
-              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-charcoal-600 md:max-w-none">
+      <div className="rounded-lg p-6 md:p-12">
+        <div className="grid grid-cols-2 gap-8 md:gap-4 lg:gap-8">
+          <div className="col-span-2 md:col-span-1">
+            <div className="mx-auto text-center md:mx-0 md:flex-auto md:py-0 md:text-left">
+              <h2 className="text-balance text-blumine-700 text-3xl md:text-4xl font-semibold tracking-tight">
+                Get the {site?.title} Language App to use on your favourite
+                devices.
+              </h2>
+
+              <dl className="mt-10 max-w-xl space-y-4 md:space-y-8 text-left text-base/7 text-charcoal-600 md:max-w-none">
                 {appDetails?.features.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
                     <dt className="inline font-semibold text-blumine-700">
@@ -38,36 +39,43 @@ function WidgetAppsPresentation() {
                   </div>
                 ))}
               </dl>
-            </div>
-            <div className="mt-10 flex items-center justify-center gap-x-6 md:justify-start">
-              <a
-                href={appDetails?.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn-primary btn-md"
-              >
-                Go to the App
-              </a>
 
-              <Link
-                to={`/${site?.sitename}/apps`}
-                className="btn-tertiary btn-md"
-              >
-                <span>Learn more</span>
-                {getIcon('RightArrow')}
-              </Link>
+              <div className="mt-10 flex items-center justify-center gap-x-6 md:justify-start">
+                <a
+                  href={appDetails?.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn-primary btn-md"
+                >
+                  Go to the App
+                </a>
+
+                <Link
+                  to={`/${site?.sitename}/apps`}
+                  className="btn-tertiary btn-md"
+                >
+                  <span>Learn more</span>
+                  {getIcon('RightArrow')}
+                </Link>
+              </div>
             </div>
           </div>
-          <div className="h-full w-full flex items-center justify-center p-7 lg:p-12">
+
+          <div className="col-span-2 md:col-span-1">
             <div className="flex-col items-center justify-center space-y-7">
               <div className="flex justify-center w-full">
-                <div className="flex items-center justify-center rounded-3xl shadow-lg border border-charcoal-500 p-4">
+                <a
+                  href={appDetails?.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center justify-center rounded-3xl shadow-lg border border-charcoal-500 p-4"
+                >
                   <LogoPresentation
                     imgSrc={appDetails?.logoUrl}
                     altText={`${site?.title} Logo`}
-                    additionalStyling="h-32 w-32 md:h-52 md:w-52 mx-0 border"
+                    additionalStyling="h-52 w-52 lg:size-64 mx-0 border"
                   />
-                </div>
+                </a>
               </div>
               <div className="flex justify-center w-full">
                 <QrcodeCanvas url={appDetails?.url} />
